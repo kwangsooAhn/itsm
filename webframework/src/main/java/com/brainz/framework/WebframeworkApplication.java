f08756063fa59f5c0018095e304571160fbf878d
hcjung <hcjung@brainz.co.kr>
2019-07-16 19:46:54 +0900

    가이드화면을 위한 bootstrap을 제거하고 markdown 포맷으로 관리하도록 변경.

--- webframework/src/main/java/com/brainz/framework/WebframeworkApplication.java
+++ webframework/src/main/java/com/brainz/framework/WebframeworkApplication.java
@@ -2,18 +2,12 @@ package com.brainz.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class WebframeworkApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello Alice!";
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(WebframeworkApplication.class, args);
 	}
