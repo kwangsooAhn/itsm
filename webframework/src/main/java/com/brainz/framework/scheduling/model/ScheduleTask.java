252f478bcafd5afea08fff2fee1c6790962c40e5
colda <colda@192.168.10.48>
2019-08-21 18:26:27 +0900

    스케줄링 테이블명 변경

--- webframework/src/main/java/com/brainz/framework/scheduling/model/ScheduleTask.java
+++ webframework/src/main/java/com/brainz/framework/scheduling/model/ScheduleTask.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "scheduleTaskInfo")
+@Table(name = "bwfSchedTaskMst")
 public class ScheduleTask implements Serializable {
 
     private static final long serialVersionUID = -6693358225759255228L;
