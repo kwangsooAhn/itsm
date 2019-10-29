ee3f707cf7c9929e45f2cb348738e9af6743ad0a
hcjung <hcjung@brainz.co.kr>
2019-10-29 17:03:36 +0900

    배포용 jar 테스트간 수정사항 - main 클래스 삭제 (UUID), 설정파일 수정(JDK11)

--- src/main/kotlin/co/brainz/framework/util/UUIDGenerator.kt
+++ src/main/kotlin/co/brainz/framework/util/UUIDGenerator.kt
@@ -7,13 +7,4 @@ public class UUIDGenerator {
 		get() {
 			return UUID.randomUUID().toString().replace("-", "")
 		}
-
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			for (i in 0..9) {
-				println(UUID.randomUUID().toString().replace("-", ""))
-			}
-		}
-	}
 }
