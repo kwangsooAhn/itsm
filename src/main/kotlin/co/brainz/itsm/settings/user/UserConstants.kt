dbb0e419ae471ab31a581ea3dd1f1cbcc3b02001
김범호 <kbh@brainz.co.kr>
2019-11-28 18:46:57 +0900

    사용자 검색 기능 추가, 누락된 파일 추가 commit

---
+++ src/main/kotlin/co/brainz/itsm/settings/user/UserConstants.kt
@@ -0,0 +1,7 @@
+package co.brainz.itsm.settings.user
+
+enum class UserConstants(val value: String) {
+    /** 검색 selectbox 에 사용할 부모 코드 */
+    PCODE("user")
+
+}
---
+++ src/main/kotlin/co/brainz/itsm/settings/user/UserSpecification.kt
@@ -0,0 +1,39 @@
+package co.brainz.itsm.settings.user
+
+import org.springframework.data.jpa.domain.Specification
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Predicate
+import javax.persistence.criteria.Root
+
+/**
+ * 사용자 전체 검색시 조회시 사용할 필드를 정의한다
+ */
+class UserSpecification(private val userSearchDto: UserSearchDto) : Specification<UserEntity> {
+
+    enum class userKey(val code: String, val column: String) {
+        ID("user.id", "userId"),
+        NAME("user.name", "userName")
+        ;
+
+        companion object {
+            fun getUserKeyColum(code: String): String {
+                for (key in values()) {
+                    if (code == key.code) {
+                        return key.column
+                    }
+                }
+                return ""
+            }
+        }
+    }
+
+    override fun toPredicate(root: Root<UserEntity>, query: CriteriaQuery<*>, criteriaBuilder: CriteriaBuilder): Predicate? {
+        val tableColumn = userKey.getUserKeyColum(userSearchDto.searchKey)
+        if (tableColumn == "") return null
+        val predicate = mutableListOf<Predicate>()
+        predicate.add(criteriaBuilder.like(root.get<String>(tableColumn), "%" + userSearchDto.searchValue + "%"))
+        return criteriaBuilder.and(*predicate.toTypedArray())
+    }
+
+}
