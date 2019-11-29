ed2009228a0cf282023eef52a754b2f8b32dfbba
김범호 <kbh@brainz.co.kr>
2019-11-29 19:28:43 +0900

    사용자 상세출력 및 사용자 수정 역할 출력

--- src/main/kotlin/co/brainz/itsm/settings/user/UserService.kt
+++ src/main/kotlin/co/brainz/itsm/settings/user/UserService.kt
@@ -1,14 +1,18 @@
 package co.brainz.itsm.settings.user
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 /**
  * 사용자 관리 서비스
  */
 @Service
 class UserService {
-
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
     @Autowired
     lateinit var userRepository: UserRepository
 
@@ -22,8 +26,21 @@ class UserService {
     /**
      * 사용자 ID로 해당 정보를 1건 조회한다.
      */
-    fun selectUser(userId: String ): UserEntity {
+    fun selectUser(userId: String): UserEntity {
         return userRepository.findByUserId(userId)
+    }
+
+    /**
+     * 사용자 ID로 정보를 수정한다.
+     */
+    fun updateUser(user: UserUpdateDto): UserEntity {
+        val updateEntity = userRepository.findByUserId(user.userId)
+        user.userName?.let { updateEntity.userName = user.userName!! }
+        user.department?.let { logger.debug("update departmenet.") }
+
+        updateEntity.updateDate = LocalDateTime.now()
+        updateEntity.updateId = SecurityContextHolder.getContext().authentication.principal as String
+        return userRepository.save(updateEntity)
     }
 
 
