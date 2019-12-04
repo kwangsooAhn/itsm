ea81c98b4693eb17712a0a4d83207ea87c3c7eda
hg.jung <hg.jung@brainz.co.kr>
2019-12-04 11:50:09 +0900

    공지사항 CRUD 관련 REST_API 변환

--- src/main/kotlin/co/brainz/itsm/notice/controller/NoticeController.kt
+++ src/main/kotlin/co/brainz/itsm/notice/controller/NoticeController.kt
@@ -30,7 +30,7 @@ import org.springframework.ui.ModelMap
 
 
 @Controller
-@RequestMapping("/notice")
+@RequestMapping("/notices")
 public class NoticeController {
 	
 	companion object {
@@ -39,10 +39,8 @@ public class NoticeController {
 
 	fun Logging(): Unit {
 		logger.info("INFO{ }", "NoticeController")
-
 	}
-
-
+	
 	@Autowired
 	lateinit var noticeRepository: NoticeRepository
 
@@ -52,8 +50,8 @@ public class NoticeController {
 	@Autowired
 	lateinit var convertParam: ConvertParam
 
-	//공지사항 리스트 조회
-	@GetMapping("/list")
+	//공지사항 리스트 화면
+	@RequestMapping(value = ["/list"], method = [RequestMethod.GET])
 	public fun getNoticeList(request: HttpServletRequest, model: Model): String {
 
 		if (!(request.getParameter("notice_title") == "check" && request.getParameter("create_userId") == "check")) {
@@ -82,77 +80,19 @@ public class NoticeController {
 		return "notice/list"
 	}
 	
-	//공지사항 세부 조회
-	@GetMapping("/{id}")
-	public fun getNotice(@PathVariable id : String, model: Model): String {
-	
-		model.addAttribute("notice", noticeService.findNoticeByNoticeNo(id))
+	//공지사항 조회 화면
+	@RequestMapping(value = ["/notice"], method = [RequestMethod.GET])
+	public fun getNotice(request: HttpServletRequest, model: Model): String {
+		model.addAttribute("notice", noticeService.findNoticeByNoticeNo(request.getParameter("id")))
 		return "notice/detail"
 	}
 
-	//공지사항 등록 화면
-	@GetMapping("","/")
-	public fun getNoticeForm(@RequestParam(value = "noticeNo", defaultValue = "0") noticeNo: String, model: Model): String {
-		
+	//공지사항 편집 화면
+	@RequestMapping(value = ["/form"], method = [RequestMethod.GET])
+	public fun getNoticeForm(@RequestParam(value = "id", defaultValue = "0") id: String, model: Model): String {
 		var addCurrentDate = LocalDateTime.now().plusDays(1)
-		
 		model.addAttribute("addCurrentDate",addCurrentDate)
-		model.addAttribute("notice", noticeService.findNoticeByNoticeNo(noticeNo))
-		
+		model.addAttribute("notice", noticeService.findNoticeByNoticeNo(id))
 		return "notice/form"
-	}
-
-
-    //공지사항 삭제
-	@RequestMapping(value = ["/{id}"],method = [RequestMethod.DELETE])
-	public fun deleteNotice(@PathVariable id: String): String {
-		
-		noticeRepository.deleteById(id)
-		
-		return "redirect:/notice/list"
-	}
-
-	//공지사항 등록 수정.
-	@RequestMapping(value = ["/{noticeNo}","/",""],method = [RequestMethod.POST])
-	public fun insertNotice(
-		@RequestParam (required = false) popStrtDtBefore :String,
-		@RequestParam (required = false) popEndDtBefore:String,
-		@RequestParam (required = false) topNoticeStrtDtBefore:String,
-		@RequestParam (required = false) topNoticeEndDtBefore:String,
-		@RequestParam (required = false) popYn : String,
-		@RequestParam (required = false) topNoticeYn : String,
-	    @RequestParam createDtBefore:String,
-		notice: NoticeEntity): String {
-		
-		var formatter : DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-	    var createDtAfter : LocalDateTime = LocalDateTime.parse(createDtBefore,formatter)
-		
-		if(popYn == "false"){
-		    notice.popStrtDt = null
-		    notice.popEndDt = null
-		    notice.popWidth = null
-		    notice.popHeight = null
-		} else {
-		    var popStrtDtAfter : LocalDateTime? = LocalDateTime.parse(popStrtDtBefore,formatter)
-		    var popEndDtAfter : LocalDateTime? = LocalDateTime.parse(popEndDtBefore,formatter)
-	    	notice.popStrtDt = popStrtDtAfter
-	    	notice.popEndDt = popEndDtAfter
-		}
-		
-		if(topNoticeYn == "false"){
-		    notice.topNoticeStrtDt = null
-		    notice.topNoticeEndDt = null
-		} else {
-		    var topNoticeStrtDtAfter : LocalDateTime? = LocalDateTime.parse(topNoticeStrtDtBefore,formatter)
-		    var topNoticeEndDtAfter : LocalDateTime? = LocalDateTime.parse(topNoticeEndDtBefore,formatter)
-		    notice.topNoticeStrtDt = topNoticeStrtDtAfter
-		    notice.topNoticeEndDt = topNoticeEndDtAfter		
-		}	
-			
-		    notice.createDt = createDtAfter
-		
-		    noticeRepository.save(notice)
-		
-		    return "redirect:/notice/list"
 	}
 }
