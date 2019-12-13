e63a7c5b52c3ab9f7400e4701508638eef1d260d
김범호 <kbh@brainz.co.kr>
2019-12-13 17:27:02 +0900

    gradle 파일안에 code inspection warning 을 정상으로 수정했다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ group = "co.brainz"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-val developmentOnly by configurations.creating
+val developmentOnly: Configuration by configurations.creating
 configurations {
     runtimeClasspath {
         extendsFrom(developmentOnly)
