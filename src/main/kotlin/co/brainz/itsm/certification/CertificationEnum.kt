a61453a680b146ff57998c6ecabf7271bebe2ec0
lizeelf <lizeelf@brianz.co.kr>
2019-12-13 17:12:31 +0900

    #7877 가입인증 메일 전송 기능 - 띄어쓰기 수정

--- src/main/kotlin/co/brainz/itsm/certification/CertificationEnum.kt
+++ src/main/kotlin/co/brainz/itsm/certification/CertificationEnum.kt
@@ -1,5 +1,5 @@
 package co.brainz.itsm.certification
 
 enum class CertificationEnum(val code: String, val value: Int) {
-    SIGNUP("user.status.signup", 0), CERTIFIED("user.status.certified",1), OVER("user.status.over",2), ERROR("user.status.error",3);
+    SIGNUP("user.status.signup", 0), CERTIFIED("user.status.certified", 1), OVER("user.status.over", 2), ERROR("user.status.error", 3);
 }
