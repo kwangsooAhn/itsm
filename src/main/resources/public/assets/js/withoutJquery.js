d9cf7cd355e610e3ca2c553996643aa5c1f6699f
hg.jung <hg.jung@brainz.co.kr>
2019-12-18 20:01:07 +0900

    csrf 기능 수정 적용

--- src/main/resources/public/assets/js/withoutJquery.js
+++ src/main/resources/public/assets/js/withoutJquery.js
@@ -4,41 +4,43 @@
 
 function createXmlHttpRequestObject(method, url, async) {
 	// will store the reference to the XMLHttpRequest object
-	var xmlHttp;
-	var token;
-	var metas = document.getElementsByTagName('meta');
+    var xmlHttp;
+    var token;
+    var metas = document.getElementsByTagName('meta');
 
-	// if running Internet Explorer
-	if (window.ActiveXObject) {
-		try {
-			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-		} catch (e) {
-			xmlHttp = false;
-		}
-	}
-	// if running Mozilla or other browsers
-	else {
-		try {
-			xmlHttp = new XMLHttpRequest();
-		} catch (e) {
-			xmlHttp = false;
-		}
-	}
-	// return the created object or display an error message
-	if (!xmlHttp) {
-		alert("Error creating the XMLHttpRequest object.");
-	} else {
-		if (method.toUpperCase() != "GET") {
-			for (var i = 0; i < metas.length; i++) {
-				if (metas[i].getAttribute('name') === '_csrf') {
-					token = metas[i].getAttribute('content');
-				}
-			}
-			xmlHttp.open(method, url, async);
-			xmlHttp.setRequestHeader('X-CSRF-TOKEN', token);
-		} else {
-			xmlHttp.open(method, url, async);
-		}
-		return xmlHttp;
-	}
+    if( typeof async === "undefined" ){ async = true; }
+    
+    // if running Internet Explorer
+    if (window.ActiveXObject) {
+        try {
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            xmlHttp = false;
+        }
+    }
+    // if running Mozilla or other browsers
+    else {
+        try {
+            xmlHttp = new XMLHttpRequest();
+        } catch (e) {
+            xmlHttp = false;
+        }
+    }
+    // return the created object or display an error message
+    if (!xmlHttp) {
+        alert("Error creating the XMLHttpRequest object.");
+    } else {
+        if (method.toUpperCase() != "GET") {
+            for (var i = 0; i < metas.length; i++) {
+                if (metas[i].getAttribute('name') === '_csrf') {
+                    token = metas[i].getAttribute('content');
+                    }
+            }
+            xmlHttp.open(method, url, async);
+            xmlHttp.setRequestHeader('X-CSRF-TOKEN', token);
+        } else {
+            xmlHttp.open(method, url, async);
+        }
+        return xmlHttp;
+    }
 }
