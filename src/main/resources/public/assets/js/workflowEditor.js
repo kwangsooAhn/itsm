2bab7e222001c4c1db8d12366fe35eb68652b4c8
jje <jje@brainz.co.kr>
2019-12-19 16:48:32 +0900

    Merge branch 'develop.as.191206' into jje.processDesigner.as.191206.7884
    # Conflicts:
    #	src/main/resources/public/template/index2.html

--- src/main/resources/public/assets/js/workflowEditor.js
+++ src/main/resources/public/assets/js/workflowEditor.js
@@ -9,23 +9,18 @@
         e.preventDefault();
     }
 
-    function drag(e, img) {
-        e.dataTransfer.setData("text", img.src);
-    }
-    function drop(e, x, y) {
-        let data = e.dataTransfer.getData("text");
-        console.log(data)
-        let drag = d3.drag().on("drag", mover);
+    function elementDrop(img, x, y) {
+        let drag = d3.drag().on("drag", elementMover);
         d3.select('#box').append("image")
             .attr("x", x)
             .attr("y", y)
-            .attr("height", "50px")
-            .attr("width", "50px")
-            .attr("xlink:href", data)
+            .attr("width", img.clientWidth + "px")
+            .attr("height",  img.clientHeight + "px")
+            .attr("xlink:href", img.src)
             .call(drag);
     }
 
-    function mover() {
+    function elementMover() {
         d3.select(d3.event.sourceEvent.srcElement)
             .attr("x", d3.event.x - parseInt(d3.select('image').attr("width")) / 2)
             .attr("y", d3.event.y - parseInt(d3.select('image').attr("height")) / 2);
@@ -33,17 +28,15 @@
 
     function addDragEvent() {
         let elementMenu = document.getElementsByClassName('element-menu')[0];
-        elementMenu.addEventListener('dragstart', drop);
         elementMenu.addEventListener('dragover', allowDrop);
 
-        let elements = elementMenu.querySelectorAll('image');
+        let elements = elementMenu.querySelectorAll('img');
         for (let i = 0, len = elements.length; i < len; i++) {
-            elements[i].setAttribute("draggable", true);
-            elements[i].addEventListener('dragstart', function(e) { drag(e, this) });
+            elements[i].setAttribute("draggable", 'true');
+            elements[i].addEventListener('dragend', function() { elementDrop(this,(event.x-68),(event.y-48)) });
         }
 
         let drawingBoard = document.getElementsByClassName('drawing-board')[0];
-        drawingBoard.addEventListener('dragstart', function() {drop(event,(event.x-60),(event.y-44))});
         drawingBoard.addEventListener('dragover', allowDrop);
     }
 
