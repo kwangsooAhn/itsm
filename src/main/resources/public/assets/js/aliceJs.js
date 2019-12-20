053656263c7b5cd8160882b37e75f80f0faa9601
김범호 <kbh@brainz.co.kr>
2019-12-20 17:35:50 +0900

    공통 JS(aliceJs.js) 안에 serialize, serializeArray, serializeObject 를 추가했다.

--- src/main/resources/public/assets/js/aliceJs.js
+++ src/main/resources/public/assets/js/aliceJs.js
@@ -1,20 +1,24 @@
 const aliceJs = {};
 
 /**
- * XMLHttpReqeust 응답시 에러 발생하는 경우 호출
+ *  * XMLHttpReqeust 응답시 에러 발생하는 경우 호출
  *
  * @param elementId 에러를 출력할 장소 element id
  * @param text response text
- * @returns {HTMLTableElement}
  */
 aliceJs.xhrErrorResponse = function (elementId, text) {
     let elmNode = document.getElementById(elementId);
     while (elmNode.hasChildNodes()) {
         elmNode.removeChild(elmNode.firstChild);
     }
+
+    if (text === 'undefined') {
+        return;
+    }
+
     const data = JSON.parse(text);
     let messages = [
-        {key: '제목', text: data.error + '(' + data.status + ')'},
+        {key: '제목', text: data.error + ' (' + data.status + ')'},
         {key: '메시지', text: data.message},
         {key: '호출 URL', text: data.path}
     ];
@@ -25,7 +29,7 @@ aliceJs.xhrErrorResponse = function (elementId, text) {
         messages.push({key: '정의된 에러', text: data.knownError});
     }
     const table = document.createElement('table');
-    messages.forEach(function(obj) {
+    messages.forEach(function (obj) {
         const tr = table.insertRow();
         const keyTd = tr.insertCell();
         const valueTd = tr.insertCell();
@@ -33,5 +37,111 @@ aliceJs.xhrErrorResponse = function (elementId, text) {
         valueTd.innerText = obj.text;
     });
     elmNode.appendChild(table);
+};
+
+/*!
+ * Serialize all form data into a query string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   form The form to serialize
+ * @return {String}      The serialized form data
+ */
+aliceJs.serialize = function (form) {
+    // Setup our serialized data
+    const serialized = [];
+
+    // Loop through each field in the form
+    for (let i = 0; i < form.elements.length; i++) {
+
+        const field = form.elements[i];
+
+        // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+        if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+        // If a multi-select, get all selections
+        if (field.type === 'select-multiple') {
+            for (let n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[n].value));
+            }
+        }
+
+        // Convert field data to a query string
+        else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+            serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+        }
+    }
+
+    return serialized.join('&');
+
+};
+/**
+ * serialize array로 돌려준다.
+ * @param form
+ * @returns {[]}
+ */
+aliceJs.serializeArray = function (form) {
+    // Setup our serialized data
+    const serialized = [];
+
+    // Loop through each field in the form
+    for (let i = 0; i < form.elements.length; i++) {
+
+        const field = form.elements[i];
+
+        // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+        if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+        // If a multi-select, get all selections
+        if (field.type === 'select-multiple') {
+            for (let n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                serialized.push({
+                    name: field.name,
+                    value: field.options[n].value
+                });
+            }
+        }
+
+        // Convert field data to a query string
+        else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+            serialized.push({
+                name: field.name,
+                value: field.value
+            });
+        }
+    }
+    return serialized;
+};
+/**
+ * serialize object 로 돌려준다
+ * @param form
+ * @returns {{}}
+ */
+aliceJs.serializeObject = function (form) {
+
+    // Setup our serialized data
+    const serialized = {};
+    // Loop through each field in the form
+    for (let i = 0; i < form.elements.length; i++) {
+
+        const field = form.elements[i];
+
+        // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+        if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+        // If a multi-select, get all selections
+        if (field.type === 'select-multiple') {
+            for (let n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                serialized[field.name] = field.options[n].value;
+            }
+        }
+
+        // Convert field data to a query string
+        else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+            serialized[field.name] = field.value;
+        }
+    }
+    return serialized;
 };
 
