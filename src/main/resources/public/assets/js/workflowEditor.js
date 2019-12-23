1305503e54fd49402d0cbda2fba6818359e98c01
jje <jje@brainz.co.kr>
2019-12-23 13:04:32 +0900

    Merge branch 'develop.as.191206' of D:\app\workspace\alice-workspace\itsm with conflicts.

--- src/main/resources/public/assets/js/workflowEditor.js
+++ src/main/resources/public/assets/js/workflowEditor.js
@@ -5,8 +5,110 @@
 }(this, (function (exports) {
     'use strict';
 
+    function ActivityElement(x, y) {
+        const self = this;
+        const width = 80, height = 50;
+
+        self.rectData = [{ x: x, y: y }, { x: x + width, y: y + height }];
+        self.rectangleElement = d3.select('svg').append('rect')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('x', x)
+            .attr('y', y)
+            .style("fill", 'yellow')
+            .style("opacity", 0.5)
+            .style("stroke", 'black')
+            .style("stroke-width", '2px')
+            .attr('class', 'rectangle')
+            .call(d3.drag().on('drag', function() {
+                for (let i = 0; i < self.rectData.length; i++) {
+                    self.rectangleElement
+                        .attr('x', self.rectData[i].x += d3.event.dx)
+                        .attr('y', self.rectData[i].y += d3.event.dy);
+                }
+                self.rectangleElement.style('cursor', 'move');
+                updateRect();
+            }));
+
+        self.pointElement1 = d3.select('svg').append('circle').call(d3.drag().on('drag', function() {
+            d3.select(this)
+                .attr('cx', function(d) { return d.x += d3.event.dx })
+                .attr('cy', function(d) { return d.y += d3.event.dy });
+            updateRect();
+        }));
+        self.pointElement2 = d3.select('svg').append('circle').call(d3.drag().on('drag', function() {
+            d3.select(this)
+                .attr('cx', self.rectData[1].x += d3.event.dx )
+                .attr('cy', self.rectData[1].y += d3.event.dy );
+            updateRect();
+        }));
+        self.pointElement3 = d3.select('svg').append('circle').call(d3.drag().on('drag', function() {
+            d3.select(this)
+                .attr('cx', self.rectData[1].x += d3.event.dx )
+                .attr('cy', self.rectData[0].y += d3.event.dy );
+            updateRect();
+        }));
+        self.pointElement4 = d3.select('svg').append('circle').call(d3.drag().on('drag', function() {
+            d3.select(this)
+                .attr('cx', self.rectData[0].x += d3.event.dx )
+                .attr('cy', self.rectData[1].y += d3.event.dy );
+            updateRect();
+        }));
+
+        function updateRect() {
+            self.rectangleElement
+                .attr('x', self.rectData[1].x - self.rectData[0].x > 0 ? self.rectData[0].x : self.rectData[1].x)
+                .attr('y', self.rectData[1].y - self.rectData[0].y > 0 ? self.rectData[0].y :  self.rectData[1].y)
+                .attr('width', Math.abs(self.rectData[1].x - self.rectData[0].x))
+                .attr('height', Math.abs(self.rectData[1].y - self.rectData[0].y));
+
+            self.pointElement1
+                .data(self.rectData)
+                .attr('r', 5)
+                .attr('cx', self.rectData[0].x)
+                .attr('cy', self.rectData[0].y);
+            self.pointElement2
+                .data(self.rectData)
+                .attr('r', 5)
+                .attr('cx', self.rectData[1].x)
+                .attr('cy', self.rectData[1].y);
+            self.pointElement3
+                .data(self.rectData)
+                .attr('r', 5)
+                .attr('cx', self.rectData[1].x)
+                .attr('cy', self.rectData[0].y);
+            self.pointElement4
+                .data(self.rectData)
+                .attr('r', 5)
+                .attr('cx', self.rectData[0].x)
+                .attr('cy', self.rectData[1].y);
+        }
+        updateRect();
+    }
+
+    function elementDrop(x, y) {
+        new ActivityElement(x, y);
+    }
+
+    function addDragEvent() {
+        let elementMenu = document.getElementsByClassName('element-menu')[0];
+        elementMenu.addEventListener('dragover', function (e) {e.preventDefault();});
+
+        let elements = elementMenu.querySelectorAll('span');
+        for (let i = 0, len = elements.length; i < len; i++) {
+            elements[i].setAttribute('draggable', 'true');
+            elements[i].addEventListener('dragend', function(e) { elementDrop(e.x - 68, e.y - 48) });
+        }
+
+        let drawingBoard = document.getElementsByClassName('drawing-board')[0];
+        drawingBoard.addEventListener('dragover', function (e) {e.preventDefault();});
+    }
+
     function init() {
         console.info('Workflow editor initialization.');
+
+        let drawingBoard = document.getElementsByClassName('drawing-board')[0];
+        addDragEvent();
     }
 
     exports.init = init;
