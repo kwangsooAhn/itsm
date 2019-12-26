7a585036cde3cfd3c5c5e60648831018aecb2a56
woodajung <wdj@brainz.co.kr>
2019-12-26 09:28:15 +0900

    Form designer basic structure add

---
+++ src/main/kotlin/co/brainz/itsm/document/controller/DocumentController.kt
@@ -0,0 +1,31 @@
+package co.brainz.itsm.form
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.http.HttpServletRequest
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+@RequestMapping("/document")
+class DocumentController {
+    
+    @GetMapping("/list")
+    fun getDocList(request: HttpServletRequest, model: Model): String {
+        return "document/list"
+    }
+    
+    @GetMapping("/form")
+    fun getDocForm(request: HttpServletRequest, model: Model): String {
+        //To-do 지원 가능한 언어 목록 가져오기
+        
+        //To-do 템플릿 정보 가져오기
+        return "document/form"
+    }
+    
+    @GetMapping("/edit")
+    fun getDocEditor(request: HttpServletRequest, model: Model): String {
+        //To-do 컴포넌트 상세 정보 가져오기
+        return "document/edit"
+    }
+}
