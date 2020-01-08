21a5bceb72ff6282393e6dd52aef6072755cd506
hg.jung <hg.jung@brainz.co.kr>
2020-01-08 09:24:40 +0900

    패스워드 규칙 및 resource 정리 관련 커밋

--- src/main/resources/public/assets/js/util/validation-alice.js
+++ src/main/resources/public/assets/js/util/validation-alice.js
@@ -8,7 +8,6 @@ const lowerCaseReg = /^[a-z]*$/;
 const numberReg = /^[0-9]*$/;
 const specialCharReg = /^[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]*$/;
 
-
 // 패스워드 validation check
 function passwordValidation(password) {
     
@@ -20,7 +19,6 @@ function passwordValidation(password) {
     var searchNumber =  password.search(numberReg);
     var searchSpecialChar = password.search(specialCharReg);
 
-   
     /*
      * Blank Validation Check
      * [ 설명 ]
@@ -28,7 +26,7 @@ function passwordValidation(password) {
      */
     if (blankReg.test(password)) {
         alert("비밀번호에 공백을 포함할 수 없습니다.");
-        return false;    
+        return false;
     }
 
     /*
@@ -50,7 +48,8 @@ function passwordValidation(password) {
      * 2가지의 문자 구성인 경우 8자 이상, 20자 미만의 비밀번호를 설정한다.
      * 문자 구성 : 대문자, 소문자, 특수문자, 숫자
      */
-    if ((searchUpperCase < 0 && searchLowerCase < 0) || (searchLowerCase < 0 && searchSpecialChar < 0) || (searchSpecialChar < 0 && searchNumber < 0) || (searchNumber < 0 && searchUpperCase < 0)) {
+    if ((searchUpperCase < 0 && searchLowerCase < 0) || (searchLowerCase < 0 && searchSpecialChar < 0) ||
+        (searchSpecialChar < 0 && searchNumber < 0) || (searchNumber < 0 && searchUpperCase < 0)) {
         if (password.length < 8 || password.length > 20) {
             alert("2가지 문자 구성의 경우 8자~20자의 비밀번호를 설정해야 합니다.");
             return false;
@@ -80,37 +79,38 @@ function passwordValidation(password) {
     /*
      * Extension Number Inclusion Validation Check
      * [ 설명 ]
-     * 비밀번호에 사용자의 사내 번호를 포함하지 않는다.
+     * 비밀번호에 사용자의 내선 번호를 포함하지 않는다.
      */
-    if (extensionNumber.length >= 10) {
-        var middleNum = "";
-        var lastNum = "";
-        
-        if (extensionNumber.length === 10) {
-            middleNum = extensionNumber.substring(3,6);
-            lastNum = extensionNumber.substring(6,10);
-            
-            if (password.search(middleNum) > -1 || password.search(lastNum) > -1) {
-                alert("비밀번호에 사용자의 사내 번호가 포함되어 있습니다.");
-                return false;
+    if (extensionNumber != "") {
+        if (extensionNumber.length >= 10) {
+            var middleNum = "";
+            var lastNum = "";
+
+            if (extensionNumber.length === 10) {
+                middleNum = extensionNumber.substring(3, 6);
+                lastNum = extensionNumber.substring(6, 10);
+
+                if (password.search(middleNum) > -1 || password.search(lastNum) > -1) {
+                    alert("비밀번호에 사용자의 내선 번호가 포함되어 있습니다.");
+                    return false;
+                }
+            } else {
+                middleNum = extensionNumber.substring(3, 7);
+                lastNum = extensionNumber.substring(7, 11);
+
+                if (password.search(middleNum) > -1 || password.search(lastNum) > -1) {
+                    alert("비밀번호에 사용자의 내선 번호가 포함되어 있습니다.");
+                    return false;
+                }
             }
         } else {
-            middleNum = extensionNumber.substring(3,7);
-            lastNum = extensionNumber.substring(7,11);
-        
-            if (password.search(middleNum) > -1 || password.search(lastNum) > -1) {
-                alert("비밀번호에 사용자의 사내 번호가 포함되어 있습니다.");
+            if (password.search(extensionNumber) > -1) {
+                alert("비밀번호에 사용자의 내선 번호가 포함되어 있습니다.");
                 return false;
             }
         }
-    } else {
-        if (password.search(extensionNumber) > -1) {
-            alert("비밀번호에 사용자의 사내 번호가 포함되어 있습니다.");
-            return false;
-        }
+        return false;
     }
-    
-    return true;
 }
 
 // 이메일 validation check
