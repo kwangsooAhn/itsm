b08b11f5649ff63d07f939fd83221ed20b165ff4
lizeelf <lizeelf@brianz.co.kr>
2020-01-10 11:36:13 +0900

    CodeConstans object 파일로 변경

--- src/main/kotlin/co/brainz/itsm/code/constants/CodeConstants.kt
+++ src/main/kotlin/co/brainz/itsm/code/constants/CodeConstants.kt
@@ -3,13 +3,14 @@ package co.brainz.itsm.code.constants
 /**
  * 공통 상수.
  */
-class CodeConstants() {
-    companion object {
-        /**
-         * CREATE USER ID 기본값.
-         */
-        const val CREATE_USER_ID: String = "system"
-        const val USER_EXPIRED_VALUE: Long = 3
-        const val SEARCH_RANGE_VALUE: Long = 1
-    }
+object CodeConstants {
+
+    /**
+     * CREATE USER ID 기본값.
+     */
+    const val CREATE_USER_ID: String = "system"
+    const val USER_EXPIRED_VALUE: Long = 3
+    const val SEARCH_RANGE_VALUE: Long = 1
+
 }
+
