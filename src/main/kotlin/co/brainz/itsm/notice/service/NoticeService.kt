1da141bf658e34e9b668454129f8df6caafa7187
김범호 <kbh@brainz.co.kr>
2020-01-22 11:43:59 +0900

    #7954 공지사항 파일 업로드 - 공지사항 등록, 수정, 삭제시 파일업로드 및 삭제 기능 추가함 - dto 추가하고 controller entity -> dto로 변경함. - Transactional 어노테이션 추가함 - fileuploader-alice.js 버튼 1개일떄 submit 되는 오류로 인하여 type=button 추가함

--- src/main/kotlin/co/brainz/itsm/notice/service/NoticeService.kt
+++ src/main/kotlin/co/brainz/itsm/notice/service/NoticeService.kt
@@ -1,15 +1,19 @@
 package co.brainz.itsm.notice.service
 
 import co.brainz.framework.auth.dto.AliceUserDto
+import co.brainz.framework.fileTransaction.dto.FileDto
+import co.brainz.framework.fileTransaction.service.FileService
+import co.brainz.itsm.notice.dto.NoticeDto
 import co.brainz.itsm.notice.entity.NoticeEntity
 import co.brainz.itsm.notice.repository.NoticeRepository
-import java.time.LocalDateTime
-import org.springframework.stereotype.Service
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
-class NoticeService(private val noticeRepository: NoticeRepository) {
+class NoticeService(private val noticeRepository: NoticeRepository, private val fileService: FileService) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
@@ -26,14 +30,14 @@ class NoticeService(private val noticeRepository: NoticeRepository) {
     }
 
     fun findAllByWriter(keyWord: String, fromDt: LocalDateTime, toDt: LocalDateTime): MutableList<NoticeEntity> {
-        return noticeRepository.findAllByWriter(keyWord,fromDt,toDt)
+        return noticeRepository.findAllByWriter(keyWord, fromDt, toDt)
     }
 
     fun findAllCheck(keyWord: String, fromDt: LocalDateTime, toDt: LocalDateTime): MutableList<NoticeEntity> {
         return noticeRepository.findAllCheck(keyWord, fromDt, toDt)
     }
 
-    fun findNoticeByNoticeNo(noticeNo : String): NoticeEntity {
+    fun findNoticeByNoticeNo(noticeNo: String): NoticeEntity {
         return noticeRepository.findById(noticeNo).orElse(NoticeEntity())
     }
 
@@ -41,28 +45,54 @@ class NoticeService(private val noticeRepository: NoticeRepository) {
         return noticeRepository.findNoticePopUp()
     }
 
-    fun insertNotice(notice: NoticeEntity) {
+    @Transactional
+    fun insertNotice(noticeDto: NoticeDto) {
         val aliceUserDto = SecurityContextHolder.getContext().authentication.details as AliceUserDto
-        notice.createUserkey = aliceUserDto.userKey
-        noticeRepository.save(notice)
+        val noticeEntity = NoticeEntity(
+            noticeDto.noticeNo,
+            noticeDto.noticeTitle,
+            noticeDto.noticeContents,
+            noticeDto.popYn,
+            noticeDto.popStrtDt,
+            noticeDto.popEndDt,
+            noticeDto.popWidth,
+            noticeDto.popHeight,
+            noticeDto.topNoticeYn,
+            noticeDto.topNoticeStrtDt,
+            noticeDto.topNoticeEndDt,
+            noticeDto.createDt,
+            aliceUserDto.userKey,
+            null,
+            null
+        )
+        val resltNoticeEntity = noticeRepository.save(noticeEntity)
+        fileService.upload(FileDto(resltNoticeEntity.noticeNo, noticeDto.fileSeq))
     }
 
-    fun updateNotice(notice: NoticeEntity) {
+    @Transactional
+    fun updateNotice(noticeDto: NoticeDto) {
         val aliceUserDto = SecurityContextHolder.getContext().authentication.details as AliceUserDto
-        val noticeEntity = noticeRepository.findByNoticeNo(notice.noticeNo)
+        val noticeEntity = noticeRepository.findByNoticeNo(noticeDto.noticeNo)
         noticeEntity.updateDt = LocalDateTime.now()
         noticeEntity.updateUserkey = aliceUserDto.userKey
-        noticeEntity.noticeTitle = notice.noticeTitle
-        noticeEntity.noticeContents = notice.noticeContents
-        noticeEntity.popStrtDt = notice.popStrtDt
-        noticeEntity.popEndDt = notice.popEndDt
-        noticeEntity.popWidth = notice.popWidth
-        noticeEntity.popHeight = notice.popHeight
-        noticeEntity.popYn = notice.popYn
-        noticeEntity.topNoticeStrtDt = notice.topNoticeStrtDt
-        noticeEntity.topNoticeEndDt = notice.topNoticeEndDt
-        noticeEntity.topNoticeYn = notice.topNoticeYn
+        noticeEntity.noticeTitle = noticeDto.noticeTitle
+        noticeEntity.noticeContents = noticeDto.noticeContents
+        noticeEntity.popStrtDt = noticeDto.popStrtDt
+        noticeEntity.popEndDt = noticeDto.popEndDt
+        noticeEntity.popWidth = noticeDto.popWidth
+        noticeEntity.popHeight = noticeDto.popHeight
+        noticeEntity.popYn = noticeDto.popYn
+        noticeEntity.topNoticeStrtDt = noticeDto.topNoticeStrtDt
+        noticeEntity.topNoticeEndDt = noticeDto.topNoticeEndDt
+        noticeEntity.topNoticeYn = noticeDto.topNoticeYn
         noticeRepository.save(noticeEntity)
+        fileService.upload(FileDto(noticeEntity.noticeNo, noticeDto.fileSeq))
+    }
+
+    @Transactional
+    fun delete(noticeNo: String) {
+        noticeRepository.deleteById(noticeNo)
+        fileService.delete(noticeNo)
     }
 
 }
