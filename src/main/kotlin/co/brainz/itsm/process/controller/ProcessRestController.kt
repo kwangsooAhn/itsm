62cc9b6f611d706857b530df65134c09218d8333
jje <jje@brainz.co.kr>
2020-01-23 18:08:23 +0900

    #7959 프로세스 디자이너 - 프로세스 저장/불러오기

--- src/main/kotlin/co/brainz/itsm/process/controller/ProcessRestController.kt
+++ src/main/kotlin/co/brainz/itsm/process/controller/ProcessRestController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 
@@ -55,5 +56,34 @@ class ProcessRestController(private val processService: ProcessService) {
     @DeleteMapping("/{processId}")
     fun deleteProcess(@PathVariable processId: String) {
         processService.deleteProcess(processId)
+    }
+
+    /**
+     * 프로세스 불러오기.
+     */
+    @GetMapping("/data/{processId}")
+    fun getProcessData(@PathVariable processId: String): String {
+        // 테스트용 프로세스 데이터
+        return """
+               {"process": {"id": "$processId", "name": "서비스데스크", "description": "서비스데스크입니다."},
+                "elements": [{
+                              "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a4",
+                              "category": "event",
+                              "type": "start",
+                              "display": {"width": 38, "height": 50, "position-x": 10, "position-y": 10},
+                              "data": {"name": "시작"}
+                             }]
+               }
+               """
+    }
+
+    /**
+     * 프로세스 저장.
+     */
+    @PostMapping("/data")
+    fun saveProcessData(@RequestParam data: String): String {
+        // 테스트용 데이터
+        println(data)
+        return "1"
     }
 }
