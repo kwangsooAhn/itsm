480e9b54c4fcd1e3e21f5a47831026ee1ed7903a
이소현 <sh-lee@brainz.co.kr>
2020-01-30 17:38:14 +0900

    충돌 수정

--- src/test/kotlin/co/brainz/itsm/user/UserJpaTest.kt
+++ src/test/kotlin/co/brainz/itsm/user/UserJpaTest.kt
@@ -38,16 +38,14 @@ class UserJpaTest {
         userEntity = AliceUserEntity(
                 UUID.randomUUID().toString(), "kbh", "itsm123", "kbh", "kbh@brainz.co.kr", true,
                 0, "과장", "ITSM팀", "02-6416-8324", "admin",
-                "admin", "status", "code", UserConstants.Platform.ALICE.code,
-                LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(), emptySet(), "",  TimeZone.getDefault().id, "en"
-                "status", "code", UserConstants.Platform.ALICE.code, LocalDateTime.now(), emptySet(), TimeZone
-                .getDefault().id, "en"
+                "status", "code", UserConstants.Platform.ALICE.code,
+                LocalDateTime.now(), emptySet(), "", TimeZone.getDefault().id, "en"
         )
 
         userUpdateDto = UserUpdateDto(
-                "kbh", "beom", "kbh@brainz.co.kr", "대리", "ITSM팀",
+                "testKey", "kbh", "beom", "itsm123", "kbh@brainz.co.kr", "대리", "ITSM팀",
                 "02-6416-8324", "010-0000-1234", emptySet(), "code",
-                "status"
+                "status", TimeZone.getDefault().id, "en"
         )
 
     }
