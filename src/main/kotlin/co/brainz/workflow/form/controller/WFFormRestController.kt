4b9b3386752fb009ef04dfec4399b882a87e2967
jje <jje@brainz.co.kr>
2020-01-30 16:26:47 +0900

    #7960 폼 디자이너 - 저장/불러오기
    - URL 변경

--- src/main/kotlin/co/brainz/workflow/form/controller/WFFormRestController.kt
+++ src/main/kotlin/co/brainz/workflow/form/controller/WFFormRestController.kt
@@ -43,4 +43,29 @@ class WFFormRestController(private val formRepository: FormRepository) {
     fun deleteForm(@PathVariable formId: String) {
         return WFEngine().form(formRepository).deleteForm(formId)
     }
+
+    @GetMapping("/data/{formId}")
+    fun getFormData(@PathVariable formId: String): String {
+        // 테스트용 문서양식 데이터
+        return """
+               {
+                "form": {"id": "$formId", "name": "장애신고", "description": "장애신고 신청서 문서양식입니다."},
+                "components": [{
+                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a4",
+                    "type": "text",
+                    "label": {"position": "left", "column": 2, "size": 12, "color": "#ffffff", "bold": "Y", "italic": "N", "underline": "N", "text": "제목", "align": "left"},
+                    "display": {"placeholder": "제목을 입력하세요.", "column": 10, "outline-width": 1, "outline-color": "#000000", order: 1},
+                    "validate": {"required": "N", "regexp": "", "regexp-msg": "", "length-min": 0, "length-max": 30}
+                }]
+               }
+               """
+    }
+
+    @PostMapping("/data")
+    fun saveFormData(@RequestBody formData: String): String {
+        // 테스트용 데이터
+        println(formData)
+        return "1"
+    }
+
 }
