9e05870e9d241479a16e3199c24e17d02c47670d
lizeelf <lizeelf@brianz.co.kr>
2020-01-30 16:46:03 +0900

    Querydsl을 기본 쿼리로 변경

--- src/main/kotlin/co/brainz/workflow/form/repository/FormRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/workflow/form/repository/FormRepositoryImpl.kt
@@ -4,15 +4,16 @@ import co.brainz.workflow.form.entity.FormEntity
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
 
-@Repository
-class FormRepositoryImpl: QuerydslRepositorySupport(FormEntity::class.java), FormRepositoryCustom {
+//@Repository
+class FormRepositoryImpl {
+//class FormRepositoryImpl: QuerydslRepositorySupport(FormEntity::class.java), FormRepositoryCustom {
 
-    override fun findFormEntityList(formName: String, formDesc: String): List<FormEntity> {
+    /*override fun findFormEntityList(formName: String, formDesc: String): List<FormEntity> {
         return emptyList()
-        /*val table = QFormEntity.formEntity
+        *//*val table = QFormEntity.formEntity
         return from(table)
                 .where(table.formName.containsIgnoreCase(formName).or(table.formDesc.containsIgnoreCase(formDesc)))
-                .fetch()*/
-    }
+                .fetch()*//*
+    }*/
 
 }
