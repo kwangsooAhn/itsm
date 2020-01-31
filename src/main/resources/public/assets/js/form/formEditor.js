b267af33d4463f029e84da33766394a057a7eadf
woodajung <wdj@brainz.co.kr>
2020-01-31 09:58:00 +0900

    충돌 해결

--- src/main/resources/public/assets/js/form/formEditor.js
+++ src/main/resources/public/assets/js/form/formEditor.js
@@ -1,3 +1,10 @@
+/**
+* @projectDescription Form Desigener Library
+*
+* @author woodajung
+* @version 1.0
+* @sdoc js/form/FormComponent.js
+*/
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
         typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -6,34 +13,35 @@
     'use strict';
     
     const _contextCompProperties = [
-            {"type": "text", "name": "텍스트", "icon": ""},
-            {"type": "textarea", "name": "문단", "icon": ""},
-            {"type": "list", "name": "리스트 선택", "icon": ""},
-            {"type": "radio", "name": "라디오 버튼", "icon": ""},
-            {"type": "checkbox", "name": "체크박스", "icon": ""},
-            {"type": "label", "name": "라벨", "icon": ""},
-            {"type": "image", "name": "이미지", "icon": ""},
-            {"type": "line", "name": "라인", "icon": ""},
-            {"type": "date", "name": "날짜", "icon": ""},
-            {"type": "time", "name": "시간", "icon": ""},
-            {"type": "datetime", "name": "일시", "icon": ""},
-            {"type": "fileupload", "name": "파일업로드", "icon": ""}
-        ],
-        _contextMenuProperties = [
-            {"type": "delete", "name": "삭제", "icon": ""},
-            {"type": "copy", "name": "복사", "icon": ""},
-        ],
-        _KEYCODE = { ARROW_UP: 38, ARROW_DOWN: 40, ENTER: 13 };
+              {'type': 'text', 'name': '텍스트', 'icon': ''},
+              {'type': 'textarea', 'name': '문단', 'icon': ''},
+              {'type': 'list', 'name': '리스트 선택', 'icon': ''},
+              {'type': 'radio', 'name': '라디오 버튼', 'icon': ''},
+              {'type': 'checkbox', 'name': '체크박스', 'icon': ''},
+              {'type': 'label', 'name': '라벨', 'icon': ''},
+              {'type': 'image', 'name': '이미지', 'icon': ''},
+              {'type': 'line', 'name': '라인', 'icon': ''},
+              {'type': 'date', 'name': '날짜', 'icon': ''},
+              {'type': 'time', 'name': '시간', 'icon': ''},
+              {'type': 'datetime', 'name': '일시', 'icon': ''},
+              {'type': 'fileupload', 'name': '파일업로드', 'icon': ''}
+          ],
+          _contextMenuProperties = [
+              {'type': 'delete', 'name': '삭제', 'icon': ''},
+             {'type': 'copy', 'name': '복사', 'icon': ''},
+          ],
+          _KEYCODE = { ARROW_UP: 38, ARROW_DOWN: 40, ENTER: 13 };
+    
     let _context = null,
         data = {},
         eventHandler = {
-        onBoxKeyDownHandler: function(e) {
+        onEditboxKeyDownHandler: function(e) {
             if (_context.childLength === 0) return;
             let keyCode = e.keyCode ? e.keyCode : e.which;
             switch (keyCode) {
                 case _KEYCODE.ARROW_UP:
                     if (_context.selected !== null) {
-                        _context.selected.classList.remove("active");
+                        _context.selected.classList.remove('active');
                         if (_context.selectedIndex > 0) {
                             _context.selected = _context.control.childNodes[_context.selectedIndex - 1];
                             _context.selectedIndex--;
@@ -41,16 +49,16 @@
                             _context.selected = _context.control.childNodes[_context.childLength];
                             _context.selectedIndex = _context.childLength;
                         }
-                        _context.selected.classList.add("active");
+                        _context.selected.classList.add('active');
                     } else {
                         _context.selected = _context.control.childNodes[_context.childLength];
                         _context.selectedIndex = _context.childLength;
-                        _context.selected.classList.add("active");
+                        _context.selected.classList.add('active');
                     }
                     break;
                 case _KEYCODE.ARROW_DOWN:
                     if (_context.selected !== null) {
-                       _context.selected.classList.remove("active");
+                       _context.selected.classList.remove('active');
                         if (_context.selectedIndex < _context.childLength) {
                            _context.selected =_context.control.childNodes[_context.selectedIndex + 1];
                            _context.selectedIndex++;
@@ -58,11 +66,11 @@
                            _context.selected = _context.control.childNodes[0];
                            _context.selectedIndex = 0;
                         }
-                       _context.selected.classList.add("active");
+                       _context.selected.classList.add('active');
                     } else {
                        _context.selected = _context.control.childNodes[0];
                        _context.selectedIndex = 0;
-                       _context.selected.classList.add("active");
+                       _context.selected.classList.add('active');
                     }
                     break;
                 case _KEYCODE.ENTER:
@@ -78,20 +86,20 @@
                 _context.resize();
             }
         },
-        onBoxKeyPressHandler: function(e) {
+        onEditboxKeyPressHandler: function(e) {
             let keyCode = e.keyCode ? e.keyCode : e.which;
             if (keyCode === _KEYCODE.ENTER) {
                 if (this.textContent.length === 0) {
                     e.preventDefault();
                     if (_context.selected === null) {
-                        addBox(true);
+                        addEditBox(true);
                     }
-                } else if (this.textContent.length > 0 && this.textContent.charAt(0) === "/") {
+                } else if (this.textContent.length > 0 && this.textContent.charAt(0) === '/') {
                     e.preventDefault();
                 }
             }
         },
-        onBoxKeyUpHandler: function(e) {
+        onEditboxKeyUpHandler: function(e) {
             let keyCode = e.keyCode ? e.keyCode : e.which;
             if (_context.control === null) return;
             if (_context.selected && (keyCode === _KEYCODE.ARROW_UP || keyCode === _KEYCODE.ARROW_DOWN)) return;
@@ -103,7 +111,7 @@
             }
             let _this = this;
             let text = _this.textContent;
-            if (text.length > 0 && text.charAt(0) !== "/") return;
+            if (text.length > 0 && text.charAt(0) !== '/') return;
             
             let rect = _this.getBoundingClientRect();
             let searchTexts = [];
@@ -118,13 +126,13 @@
         },
         onFormClickHandler: function(e) {
             _context.hide();
-            if (e.target.classList.contains('component') && e.target.dataset.type !== "box" &&
+            if (e.target.classList.contains('component') && e.target.dataset.type !== 'editbox' &&
                 Component.getSelectedComponentId() !== e.target.id) {
                 Component.setSelectedComponentId(e.target.id);
                 Component.hidePropertyPanel();
                 Component.showPropertyPanel(e.target.id);
             } else {
-                Component.setSelectedComponentId("");
+                Component.setSelectedComponentId('');
                 Component.hidePropertyPanel();
             }
         },
@@ -141,11 +149,14 @@
         }
     };
     /**
-     * 컨텍스트 메뉴 
+     * 폼 디자이너 컨텍스트 메뉴 객체
+     *
+     * @method ContextMenu
+     * @access private
      */
     function ContextMenu() {
-        this.control = document.getElementById("component-context");
-        if (this.control !== null) { this.control.innerHTML = ""; };
+        this.control = document.getElementById('context-menu');
+        if (this.control !== null) { this.control.innerHTML = ''; };
         
         this.top = 0;
         this.left = 0;
@@ -156,26 +167,26 @@
     Object.assign( ContextMenu.prototype, {
         constructor: ContextMenu,
         hide: function() {
-            if (this.control.style.display === "block") {
-                this.control.innerHTML = "";
-                this.control.style.display = "none";
+            if (this.control.style.display === 'block') {
+                this.control.innerHTML = '';
+                this.control.style.display = 'none';
                 this.childLength = 0;
             }
         },
         show: function(items, eventElem) {
             for (let i = 0, len = items.length; i < len; i++) {
                 let item = items[i];
-                let el = document.createElement("li");
-                el.className = "context-item";
+                let el = document.createElement('li');
+                el.className = 'context-item';
                 el.setAttribute('data-type', item.type);
                 el.setAttribute('data-name', item.name);
                 el.textContent = item.name;
                 el.onclick = function (e) {
                     _context.hide();
-                    if (e.target.dataset.type === "delete") {
-                        Component.remove(eventElem.id);
-                    } else if (e.target.dataset.type === "copy") {
-                        Component.copy(eventElem);
+                    if (e.target.dataset.type === 'delete') {
+                        //Component.remove(eventElem.id);
+                    } else if (e.target.dataset.type === 'copy') {
+                        //Component.copy(eventElem);
                     } else {
                         addComponent(this.dataset.type, this.dataset.name, eventElem);
                     }
@@ -184,9 +195,9 @@
                 };
                 this.control.appendChild(el);
             }
-            this.control.style.top = (this.top + 30) + "px";
-            this.control.style.left = (this.left + 30) + "px";
-            this.control.style.display = "block";
+            this.control.style.top = (this.top + 30) + 'px';
+            this.control.style.left = (this.left + 30) + 'px';
+            this.control.style.display = 'block';
             this.childLength = items.length -1;
         },
         resize: function() {
@@ -201,11 +212,11 @@
         },
         getContextItems: function (items, search) {
             let result = [];
-            search = search || "/";
-            let text = search.replace("/", ""); //검색어
+            search = search || '/';
+            let text = search.replace('/', ''); //검색어
             for (let i = 0, len = items.length; i < len; i++) {
                 let item = items[i];
-                if (search === "/") { //'/'를 입력한 경우 모두 검색
+                if (search === '/') { //'/'를 입력한 경우 모두 검색
                     result.push(item);
                 } else {
                     if (text === item.name.slice(0, text.length)) {
@@ -216,6 +227,7 @@
             return result;
         }
     });
+    
     /**
      * 컴포넌트 신규 추가
      *
@@ -228,56 +240,28 @@
     function addComponent(type, name, targetElement) {
         let comp = Component.add({type: type, name: name, attrs: {}, isFocus: true, targetElement: targetElement});
         comp.addEventListener( 'contextmenu', eventHandler.onCompRightClickHandler, false);
-        let box = document.querySelectorAll("[contenteditable=true]");
-        if (box.length === 0 || Component.getLastComponentId() === comp.id) { addBox(false);}
+        let box = document.querySelectorAll('[contenteditable=true]');
+        if (box.length === 0 || Component.getLastComponentId() === comp.id) { addEditBox(false);}
     }
-    function addBox(isFocus) {
-        let box = Component.add({type: 'box', name: '', isFocus: isFocus});
-        box.addEventListener( 'keydown', eventHandler.onBoxKeyDownHandler, false);
-        box.addEventListener( 'keypress', eventHandler.onBoxKeyPressHandler, false);
-        box.addEventListener( 'keyup', eventHandler.onBoxKeyUpHandler, false);
-        box.addEventListener( 'contextmenu', eventHandler.onCompRightClickHandler, false);
-    }
-    function init(form) {
-        console.info('form editor initialization. [FORM ID: ' + form.formId + ']');
-        Component.init();
-        _context = new ContextMenu();
-        
-        let formDesigner = document.getElementById("form-designer");
-        formDesigner.addEventListener( 'click', eventHandler.onFormClickHandler, false);
-        addBox(true);
- 
-        // add save button event handler.
-        document.querySelector('#btnSave').addEventListener('click', saveForm);
-
-        // load form data.
-        const xhr = createXmlHttpRequestObject('GET', '/rest/forms/data/' + form.formId);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    data = JSON.parse(xhr.responseText);
-                    setComponents();
-                } else if (xhr.status === 400) {
-                    alert('There was an error 400');
-                } else {
-                    console.log(xhr);
-                    alert('something else other than 200 was returned. ' + xhr.status);
-                }
-            }
-        };
-        xhr.send();
-    }
-
     /**
-     * 조회된 문서양식 데이터로 component 를 추가한다.
+     * 편집가능한 영역 추가
+     *
+     * @method addEditBox
+     * @param isFocus 포커스 여부
+     * @access private
      */
-    function setComponents() {
-        //TODO: add component.
-        console.debug(data);
+    function addEditBox(isFocus) {
+        let editbox = Component.add({type: 'editbox', name: '', isFocus: isFocus});
+        editbox.addEventListener( 'keydown', eventHandler.onEditboxKeyDownHandler, false);
+        editbox.addEventListener( 'keypress', eventHandler.onEditboxKeyPressHandler, false);
+        editbox.addEventListener( 'keyup', eventHandler.onEditboxKeyUpHandler, false);
+        editbox.addEventListener( 'contextmenu', eventHandler.onCompRightClickHandler, false);
     }
-
     /**
-     * 문서양식 저장.
+     * 폼 디자이너 저장
+     *
+     * @method saveForm
+     * @access public
      */
     function saveForm() {
         const xhr = createXmlHttpRequestObject('POST', '/rest/forms/data');
@@ -300,7 +284,104 @@
         xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
         xhr.send(JSON.stringify(data));
     }
+    /**
+     * 작업 취소
+     *
+     * @method undoForm
+     * @access public
+     */
+    function undoForm() {
+        //TODO: 작업 취소
+    }
+    /**
+     * 작업 재실행
+     *
+     * @method redoForm
+     * @access public
+     */
+    function redoForm() {
+        //TODO: 작업 재실행
+    }
+    /**
+     * 미리보기
+     *
+     * @method previewForm
+     * @access public
+     */
+    function previewForm() {
+        //TODO: 미리보기
+    }
+    /**
+     * export
+     *
+     * @method exportForm
+     * @access public
+     */
+    function exportForm() {
+        //TODO: export
+    }
+    /**
+     * import
+     *
+     * @method importForm
+     * @access public
+     */
+    function importForm() {
+        //TODO: import
+    }
+    /**
+     * 조회된 문서양식 데이터로 component 를 추가한다.
+     * 
+     * @method setComponents
+     * @access private
+     */
+    function setComponents() {
+        //TODO: add component.
+        console.debug(data);
+    }
+    /**
+     * 폼 디자이너 편집 화면 초기화
+     *
+     * @function init
+     * @param type 컴포넌트 타입
+     * @param name 컴포넌트 명
+     * @param targetElement 컴포넌트를 추가할 위치를 나타내는 element
+     * @access public
+     */
+    function init(data) {
+        console.info('form editor initialization. [FORM ID: ' + form.formId + ']');
+        Component.init();
+        _context = new ContextMenu();
+        
+        let formDesigner = document.getElementById('form-designer');
+        formDesigner.addEventListener( 'click', eventHandler.onFormClickHandler, false);
+        addBox(true);
+        
+        // load form data.
+        const xhr = createXmlHttpRequestObject('GET', '/rest/forms/data/' + form.formId);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    data = JSON.parse(xhr.responseText);
+                    setComponents();
+                } else if (xhr.status === 400) {
+                    alert('There was an error 400');
+                } else {
+                    console.log(xhr);
+                    alert('something else other than 200 was returned. ' + xhr.status);
+                }
+            }
+        };
+        xhr.send();
+    }
     
     exports.init = init;
+    exports.save = saveForm;
+    exports.undo = undoForm;
+    exports.redo = redoForm;
+    exports.preview = previewForm;
+    exports.exportform = exportForm;
+    exports.importform = exportForm;
+    
     Object.defineProperty(exports, '__esModule', { value: true });
 })));
