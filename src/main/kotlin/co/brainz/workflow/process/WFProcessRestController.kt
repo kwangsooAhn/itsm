dd2f24b457c2c5ba87be772b27312ea2616b4755
김범호 <kbh@brainz.co.kr>
2020-02-03 10:37:18 +0900

    workflow 용 /rest/wf/processes 를 위한 컨트롤러 추가함.

--- src/main/kotlin/co/brainz/workflow/process/WFProcessRestController.kt
+++ src/main/kotlin/co/brainz/workflow/process/WFProcessRestController.kt
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/rest/processes")
-class ProcessRestController(private val wfEngine: WFEngine) {
+@RequestMapping("/rest/wf/processes")
+class WFProcessRestController(private val wfEngine: WFEngine) {
 
     /**
      * 프로세스 데이터 조회.
@@ -54,32 +54,4 @@ class ProcessRestController(private val wfEngine: WFEngine) {
         wfEngine.process().deleteProcess(processId)
     }
 
-    /**
-     * 프로세스 불러오기.
-     */
-    @GetMapping("/data/{processId}")
-    fun getProcessData(@PathVariable processId: String): String {
-        // 테스트용 프로세스 데이터
-        return """
-               {"process": {"id": "$processId", "name": "서비스데스크", "description": "서비스데스크입니다."},
-                "elements": [{
-                              "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a4",
-                              "category": "event",
-                              "type": "start",
-                              "display": {"width": 38, "height": 50, "position-x": 10, "position-y": 10},
-                              "data": {"name": "시작"}
-                             }]
-               }
-               """
-    }
-
-    /**
-     * 프로세스 저장.
-     */
-    @PostMapping("/data")
-    fun saveProcessData(@RequestBody processData: String): String {
-        // 테스트용 데이터
-        println(processData)
-        return "1"
-    }
 }
