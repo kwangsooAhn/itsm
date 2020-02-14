b674f782cada3788cf32b3fd5232411461ca3bdf
lizeelf <lizeelf@brianz.co.kr>
2020-02-14 10:22:40 +0900

    1. 폼 저장, 조회 구현 (양방향구조)

---
+++ src/main/kotlin/co/brainz/workflow/component/entity/ComponentDataEntity.kt
@@ -0,0 +1,35 @@
+package co.brainz.workflow.component.entity
+
+import org.hibernate.annotations.GenericGenerator
+import java.io.Serializable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "wf_comp_data")
+data class ComponentDataEntity(
+        @Id @GeneratedValue(generator = "system-uuid")
+        @GenericGenerator(name = "system-uuid", strategy = "uuid")
+        @Column(name = "id")
+        val id: String,
+
+        @Column(name = "attr_id")
+        val attrId: String,
+
+        @Column(name = "attr_value")
+        val attrValue: String,
+
+        @Column(name = "attr_order")
+        val attrOrder: Int,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "comp_id")
+        val attributes: ComponentMstEntity
+
+) : Serializable
