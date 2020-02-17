887e8a352ca947d45d1c8b31a72d009af4438a5f
hcjung <hcjung@brainz.co.kr>
2020-02-17 13:21:48 +0900

    공지사항 DTO 적용
    - Metadata에 AliceUserEntity 적용을 위한 사전작업
    - MapStruct 적용

---
+++ src/main/kotlin/co/brainz/itsm/notice/mapper/NoticeMapper.kt
@@ -0,0 +1,20 @@
+package co.brainz.itsm.notice.mapper
+
+import co.brainz.itsm.notice.dto.NoticeDto
+import co.brainz.itsm.notice.dto.NoticeListDto
+import co.brainz.itsm.notice.dto.NoticePopupDto
+import co.brainz.itsm.notice.dto.NoticePopupListDto
+import co.brainz.itsm.notice.entity.NoticeEntity
+import org.mapstruct.Mapper
+import org.springframework.web.bind.annotation.Mapping
+
+@Mapper
+public interface NoticeMapper {
+    fun toNoticePopupDto(noticeEntity: NoticeEntity): NoticePopupDto
+
+    fun toNoticePopupListDto(noticeEntity: NoticeEntity): NoticePopupListDto
+
+    fun toNoticeDto(noticeEntity: NoticeEntity): NoticeDto
+
+    fun toNoticeListDto(noticeEntity: NoticeEntity): NoticeListDto
+}
