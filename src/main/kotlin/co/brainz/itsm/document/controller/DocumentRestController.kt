d88981d5d55a8d42288646a297e8d250d3fea630
이소현 <sh-lee@brainz.co.kr>
2020-02-21 17:45:10 +0900

    #8085 신청서 화면 출력 - checkbox, label, image, line, date, time, datetime, file

--- src/main/kotlin/co/brainz/itsm/document/controller/DocumentRestController.kt
+++ src/main/kotlin/co/brainz/itsm/document/controller/DocumentRestController.kt
@@ -13,6 +13,8 @@ class DocumentRestController {
     /**
      * 신청서의 문서 데이터 조회.
      *
+     * text, textarea, select, radio, checkbox, label, image, line, date, time, datetime, file => 12개
+     *
      * @param documentId
      * */
     @GetMapping("/data/{documentId}")
@@ -20,100 +22,148 @@ class DocumentRestController {
         // 테스트용 문서양식 데이터
         return """
                {
-                "document": {"id": "$documentId", "name": "장애신고~", "description": "장애신고 신청서 문서양식입니다."},
+                "document": {"id": "$documentId", "name": "회원가입~", "description": "회원가입 문서양식입니다."},
                 "components": [{
-                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a4",
-                    "type": "text",
-                    "label": {"position": "left", "column": 2, "size": 16, "color": "#000000", "bold": "N",
-                    "italic": "Y", "underline": "Y", "text": "문자", "align": "center"},
-                    "display": {"placeholder": "문자를 입력하세요.", "column": 10, "outline-width": 1,
-                    "outline-color": "#000000", "order": 4},
-                    "validate": {"required": "Y", "regexp": "char", "regexp-msg": "문자를 입력해주세요.",
-                    "length-min": 5, "length-max": 30}
-                },
-                {
-                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a5",
-                    "type": "text",
-                    "label": {"position": "top", "column": 2, "size": 20, "color": "#000000", "bold": "Y",
-                    "italic": "N", "underline": "N", "text": "숫자 :", "align": "left"},
-                    "display": {"placeholder": "숫자를 입력하세요.", "column": 10, "outline-width": 2,
-                    "outline-color": "#111111", "order": 6},
-                    "validate": {"required": "N", "regexp": "num", "regexp-msg": "숫자를 입력해주세요.",
-                    "length-min": 0, "length-max": 30}
-                },
-                {
-                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a6",
-                    "type": "text",
-                    "label": {"position": "left", "column": 4, "size": 15, "color": "#0f3fef", "bold": "Y",
-                    "italic": "N", "underline": "N", "text": "숫자 + 문자 :", "align": "left"},
-                    "display": {"placeholder": "숫자 + 문자를 입력하세요.", "column": 8, "outline-width": 1,
-                    "outline-color": "#ff0000", "order": 3},
-                    "validate": {"required": "N", "regexp": "numchar", "regexp-msg": "숫자 + 문자를 입력해주세요.",
-                     "length-min": 0, "length-max": 30}
-                },
-                {
-                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a7",
-                    "type": "text",
-                    "label": {"position": "hidden", "column": 2, "size": 15, "color": "#000000", "bold": "N",
-                    "italic": "N", "underline": "N", "text": "phone(hidden) :", "align": "left"},
-                    "display": {"placeholder": "phone(hidden)를 입력하세요.", "column": 10, "outline-width": 1,
-                    "outline-color": "#111111", "order": 7},
-                    "validate": {"required": "N", "regexp": "phone", "regexp-msg": "phone을 입력해주세요.",
-                    "length-min":0, "length-max": 30}
-                },
-                {
-                    "id": "4a417b48-be2e-4ebe-82bf-8f80a63622a8",
-                    "type": "text",
-                    "label": {"position": "left", "column": 2, "size": 15, "color": "#000000", "bold": "N",
-                    "italic": "N", "underline": "N", "text": "email(hidden) :", "align": "right"},
-                    "display": {"placeholder": "email(hidden)를 입력하세요.", "column": 10, "outline-width": 1,
-                    "outline-color": "#ff0000", "order": 9},
-                    "validate": {"required": "N", "regexp": "email", "regexp-msg": "email을 입력해주세요.",
-                    "length-min":0, "length-max": 30}
-                },
-                {
-                    "id": "00e3b10a-a9b1-4f68-9ae0-113f34532c29",
-                    "type": "textarea",
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703201",
+                    "type": "label",
                     "common": {"mapping-id": ""},
-                    "label": {"position": "left", "column": 2, "size": 12, "color": "#ff0000", "bold": "Y", "italic":
-                     "N", "underline": "N", "text": "설명~~", "align": "left"},
-                    "display": {"rows": 3, "placeholder": "~~~", "column": 10, "outline-width": 1, "outline-color":
-                    "#000000", "order": 8},
-                    "validate": {"required": "Y", "length-min": 0, "length-max": 100}
+                    "display": {
+                        "size": 30, "color": "#000000", "bold": "Y", "italic": "Y", "underline": "N", 
+                        "text": "회원가입 문서양식", "align": "center", "order": 1}
                 },
                 {
-                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d37032fe",
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703202",
+                    "type": "line",
+                    "common": {"mapping-id": ""},
+                    "display": {"width": 2, "color": "#0066ff", "type": "dashed", "order": 2}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703203",
+                    "type": "text",
+                    "label": {
+                        "position": "left", "column": 2, "size": 16, "color": "#000000", "bold": "N", "italic": "Y", 
+                        "underline": "N", "text": "아이디", "align": "left"},
+                    "display": {
+                        "placeholder": "아이디를 입력하세요.", "column": 10, "outline-width": 1, "outline-color": "#000000", 
+                        "order": 3},
+                    "validate": {
+                        "required": "Y", "regexp": "char", "regexp-msg": "문자를 입력해주세요.", "length-min": 5, 
+                        "length-max": 30}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703204",
+                    "type": "text",
+                    "label": {
+                        "position": "left", "column": 2, "size": 16, "color": "#000000", "bold": "Y", "italic": "N", 
+                        "underline": "Y", "text": "이름", "align": "left"},
+                    "display": {
+                        "placeholder": "이름을 입력하세요.", "column": 10, "outline-width": 1, "outline-color": "#000000", 
+                        "order": 4},
+                    "validate": {
+                        "required": "Y", "regexp": "char", "regexp-msg": "문자를 입력해주세요.", "length-min": 5, 
+                        "length-max": 30}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d37032105",
+                    "type": "textarea",
+                    "label": {
+                        "position": "hidden", "column": 2, "size": 16, "color": "#000000", "bold": "Y", "italic": "N", 
+                        "underline": "Y", "text": "기타", "align": "left"},
+                    "display": {
+                        "placeholder": "기타사항을 입력하세요.", "column": 10, "outline-width": 1, "outline-color": "#cc66ff", 
+                        "order": 5},
+                    "validate": {"required": "N", "regexp": "none", "regexp-msg": "", "length-min": 5, "length-max": 30}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703205",
                     "type": "select",
                     "common": {"mapping-id": ""},
-                    "label": {"position": "left", "column": 2, "size": 12, "color": "#000000", "bold": "Y", "italic":
-                    "N", "underline": "N", "text": "부서", "align": "left"},
-                    "display": {"column": 10, "order": 1},
-                    "validate": {"required": "N"},
-                    "option": [ {"seq": 1, "name": "ITSM팀", "value": "itsm"},
-                                {"seq": 3, "name": "인프라웹팀", "value": "infraweb"},
-                                {"seq": 2, "name": "UX팀", "value": "ux"} ]
+                    "label": {
+                        "position": "left", "column": 2, "size": 12, "color": "#66ccff", "bold": "Y", "italic": "N", 
+                        "underline": "N", "text": "날짜 유형", "align": "left"},
+                    "display": {"column": 10, "order": 5},
+                    "validate": {"required": "Y"},
+                    "option": [ {"seq": 4, "name": "dd-MM-yyyy", "value": "ddmmyyyy"},
+                                {"seq": 1, "name": "yyyy-MM-dd", "value": "yyyymmdd"},
+                                {"seq": 3, "name": "MM-dd-yyyy", "value": "mmddyyyy"},
+                                {"seq": 2, "name": "yyyy-dd-MM", "value": "yyyyddmm"} ]
                 },
                 {
-                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d37032ff",
-                    "type": "radio",
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703206",
+                    "type": "select",
                     "common": {"mapping-id": ""},
-                    "label": {"position": "top", "column": 2, "size": 12, "color": "#000000", "bold": "Y", "italic":
-                    "N", "underline": "N", "text": "프로젝트", "align": "left"},
-                    "display": {"column": 10, "direction": "horizontal", "position": "left","order": 2},
-                    "validate": {"required": "N"},
-                    "option": [ {"seq": 1, "name": "ITSM", "value": "itsm"},
-                                {"seq": 2, "name": "대시보드", "value": "dashboard"} ]
+                    "label": {
+                        "position": "left", "column": 2, "size": 12, "color": "#ff3399", "bold": "N", "italic": "N", 
+                        "underline": "N", "text": "언어", "align": "left"},
+                    "display": {"column": 10, "order": 6},
+                    "validate": {"required": "Y"},
+                    "option": [ {"seq": 2, "name": "한국어", "value": "ko"},
+                                {"seq": 1, "name": "영어", "value": "en"} ]
                 },
                 {
-                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d37032ff",
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703207",
                     "type": "radio",
                     "common": {"mapping-id": ""},
-                    "label": {"position": "left", "column": 2, "size": 15, "color": "#000000", "bold": "Y", "italic":
-                    "N", "underline": "N", "text": "프로젝트~~", "align": "left"},
-                    "display": {"column": 10, "direction": "vertical", "position": "right","order": 5},
+                    "label": {
+                        "position": "top", "column": 2, "size": 12, "color": "#000000", "bold": "Y", "italic": "N", 
+                        "underline": "N", "text": "라디오 버튼", "align": "left"},
+                    "display": {"column": 10, "direction": "horizontal", "position": "left","order": 7},
                     "validate": {"required": "N"},
-                    "option": [{"seq": 2, "name": "ITSM", "value": "itsm2"},
-                               {"seq": 1, "name": "대시보드", "value": "dashboard2"}]
+                    "option": [ {"seq": 2, "name": "radio button2", "value": "radio2"},
+                                {"seq": 1, "name": "radio button1", "value": "radio1"} ]
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703208",
+                    "type": "checkbox",
+                    "common": {"mapping-id": ""},
+                    "label": {
+                        "position": "left", "column": 2, "size": 15, "color": "#ff0000", "bold": "Y", "italic": "N", 
+                        "underline": "N", "text": "Check Box", "align": "left"},
+                    "display": {"column": 10, "direction": "vertical", "position": "right","order": 8},
+                    "validate": {"required": "N"},
+                    "option": [{"seq": 2, "name": "제목", "value": "title"},
+                               {"seq": 1, "name": "작성자", "value": "name"}]
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703208",
+                    "type": "image",
+                    "common": {"mapping-id": ""},
+                    "display": {
+                        "path": "https://placehold.it/700X50", "width": 700, "height": 50, "align": "center", 
+                        "order": 8}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703209",
+                    "type": "date",
+                    "common": {"mapping-id": ""},
+                    "label": {"position": "left", "column": 4, "size": 16, "color": "#33ccff", "bold": "N",
+                    "italic": "Y", "underline": "Y", "text": "날짜(yyyy-MM-dd)", "align": "center"},
+                    "display": {"column": 8, "format": "yyyy-MM-dd", "default": "today", "order": 9},
+                    "validate": {"required": "Y", "date-min": "1900-01-01", "date-max": "2999-12-31"}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d37032010",
+                    "type": "time",
+                    "common": {"mapping-id": ""},
+                    "label": {"position": "left", "column": 4, "size": 16, "color": "#ff3399", "bold": "Y",
+                    "italic": "N", "underline": "N", "text": "시간(HH:mm)", "align": "center"},
+                    "display": {"column": 8, "format": "HH:mm", "default": "17:16", "order": 10},
+                    "validate": {"required": "Y"}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703211",
+                    "type": "datetime",
+                    "common": {"mapping-id": ""},
+                    "label": {"position": "left", "column": 4, "size": 16, "color": "#009933", "bold": "Y",
+                    "italic": "N", "underline": "Y", "text": "날짜시간(yyyy-MM-dd HH:mm)", "align": "center"},
+                    "display": {"column": 8, "format": "yyyy-MM-dd HH:mm", "default": "now", "order": 11},
+                    "validate": {"required": "Y", "date-min": "1900-01-01", "date-max": "2999-12-31"}
+                },
+                {
+                    "id": "53f7b7f9-c7de-4ffc-a819-0be1d3703212",
+                    "type": "fileupload",
+                    "common": {"mapping-id": ""},
+                    "display": {"order": 12}
                 }]
                 }
                 """
--- src/main/resources/public/assets/js/document/documentEditor.js
+++ src/main/resources/public/assets/js/document/documentEditor.js
@@ -9,6 +9,28 @@
     const defaultColWidth = 8.33; //폼 패널을 12등분하였을때, 1개의 너비
 
     /**
+     * 현재 시간 추출 2020-02-19 13:30 형식
+     */
+    function getTimeStamp() {
+        let today = new Date();
+        return parseZero(today.getFullYear(), 4) + '-' +
+               parseZero(today.getMonth() + 1, 2) + '-' +
+               parseZero(today.getDate(), 2) + ' ' +
+               parseZero(today.getHours(), 2) + ':' +
+               parseZero(today.getMinutes(), 2);
+    }
+
+    function parseZero(n, digits) {
+        let zero = '';
+        n = n.toString();
+        if (n.length < digits) {
+            for (let i = 0; i < (digits - n.length); i++)
+                zero += '0';
+        }
+        return zero + n;
+    }
+
+    /**
      * add Component.
      *
      * @param compData 컴포넌트.
@@ -28,7 +50,7 @@
         const displayData = compData.display;
         const validateData = compData.validate;
 
-        if (lblData !== 'undefined' && lblData.position !== 'hidden') {
+        if (typeof lblData !== 'undefined' && lblData.position !== 'hidden') {
             const lblEle = document.createElement('div');
             lblEle.className = 'label';
             lblEle.style.fontSize = lblData.size + 'px';
@@ -45,13 +67,15 @@
                 fieldLastEle.style.flexBasis = (defaultColWidth * Number(displayData.column)) + '%';
             }
             fieldFirstEle.appendChild(lblEle);
+            comp.appendChild(fieldFirstEle);
         }
+        comp.appendChild(fieldLastEle);
+        documentContainer.appendChild(comp);
 
         //구현 완료 시 삭제.
-        console.log('=================================================== ' + compData.type + ' ===================================================');
-        console.log(JSON.stringify(displayData));
-        console.log(JSON.stringify(validateData));
-
+        //console.log('=================================================== ' + compData.type + '===================================================');
+        //console.log(JSON.stringify(displayData));
+        //console.log(JSON.stringify(validateData));
         //TODO: common[mapping-id] 구현 필요.
         //TODO: validate 처리 필요.
         switch (compData.type) {
@@ -98,7 +122,7 @@
                     if (displayData.direction === 'horizontal') { divEle.style.display = 'inline-block'}
                     const radioEle = document.createElement('input');
                     radioEle.type = 'radio';
-                    radioEle.name = 'radio-' + displayData.order; //TODO: 임시. 별도의 속성 필요.
+                    radioEle.name = 'radio-' + compData.id;
                     radioEle.id = radioOptData[i].value;
                     radioEle.value = radioOptData[i].value;
                     radioEle.checked = (i === 0);
@@ -117,16 +141,110 @@
                 }
                 break;
             case 'checkbox':
-                //TODO: checkbox
+                const checkOptData = compData.option;
+                checkOptData.sort(function(a, b) {
+                    return a.seq - b.seq;
+                });
+                for (let i = 0; i < checkOptData.length; i++) {
+                    const divEle = document.createElement('div');
+                    if (displayData.direction === 'horizontal') { divEle.style.display = 'inline-block'}
+                    const checkEle = document.createElement('input');
+                    checkEle.type = 'checkbox';
+                    checkEle.name = 'check-' + compData.id;
+                    checkEle.id = checkOptData[i].value;
+                    checkEle.value = checkOptData[i].value;
+                    checkEle.checked = (i === 0);
 
+                    const lblEle = document.createElement('label');
+                    lblEle.innerText = checkOptData[i].name;
+                    lblEle.setAttribute('for', checkOptData[i].value);
+                    if (displayData.position === 'left') {
+                        divEle.appendChild(checkEle);
+                        divEle.appendChild(lblEle);
+                    } else {
+                        divEle.appendChild(lblEle);
+                        divEle.appendChild(checkEle);
+                    }
+                    fieldLastEle.appendChild(divEle);
+                }
+                break;
+            case 'label':
+                const lblEle = document.createElement('div');
+                lblEle.className = 'label';
+                lblEle.style.fontSize = displayData.size + 'px';
+                lblEle.style.color = displayData.color;
+                lblEle.style.fontWeight = (displayData.bold === 'Y') ? 'bold' : '';
+                lblEle.style.fontStyle = (displayData.italic === 'Y') ? 'italic' : '';
+                lblEle.style.textDecoration = (displayData.underline === 'Y') ? 'underline' : '';
+                lblEle.style.textAlign = displayData.align;
+                lblEle.innerHTML = displayData.text;
+                fieldLastEle.appendChild(lblEle);
+                break;
+            case 'image':
+                const imgEle = document.createElement('img');
+                imgEle.src = displayData.path;
+                imgEle.width = displayData.width;
+                imgEle.height = displayData.height;
+                fieldLastEle.style.textAlign = displayData.align;
+                fieldLastEle.appendChild(imgEle);
+                break;
+            case 'line':
+                const lineEle = document.createElement('hr');
+                lineEle.style.borderWidth = displayData.width + 'px';
+                lineEle.style.borderStyle = displayData.type;
+                lineEle.style.borderColor = displayData.color;
+                fieldLastEle.appendChild(lineEle);
+                break;
+            case 'date':
+                let dateDefault = displayData.default;
+                if (dateDefault === 'today') {
+                    dateDefault = getTimeStamp();
+                    dateDefault = dateDefault.split(' ')[0];
+                }
+                const dateEle = document.createElement('input');
+                dateEle.id = 'date-' + compData.id;
+                dateEle.type = 'text';
+                dateEle.placeholder = displayData.format;
+                dateEle.value = dateDefault; //TODO: 수정 필요
+                fieldLastEle.appendChild(dateEle);
+                dateTimePicker.initDatePicker('date-' + compData.id, displayData.format.toUpperCase());
+                break;
+            case 'time':
+                let timeDefault = displayData.default;
+                if (timeDefault === 'now') {
+                    timeDefault = getTimeStamp();
+                    timeDefault = timeDefault.split(' ')[1];
+                }
+                const timeEle = document.createElement('input');
+                timeEle.id = 'time-' + compData.id;
+                timeEle.type = 'text';
+                timeEle.placeholder = displayData.format;
+                timeEle.value = timeDefault; //TODO: 수정 필요
+                fieldLastEle.appendChild(timeEle);
+                dateTimePicker.initTimePicker('time-' + compData.id, displayData.format.toUpperCase());
+                break;
+            case 'datetime':
+                let datetimeDefault = displayData.default;
+                if (datetimeDefault === 'now') {
+                    datetimeDefault = getTimeStamp();
+                }
+                const datetimeEle = document.createElement('input');
+                datetimeEle.id = 'datetime-' + compData.id;
+                datetimeEle.type = 'text';
+                datetimeEle.placeholder = displayData.format;
+                datetimeEle.value = datetimeDefault; //TODO: 수정 필요
+                fieldLastEle.appendChild(datetimeEle);
+                dateTimePicker.initDateTimePicker('datetime-' + compData.id, displayData.format.toUpperCase());
+                break;
+            case 'fileupload':
+                const fileEle = document.createElement('input');
+                fileEle.type = 'file';
+                fileEle.multiple = true;
+                fieldLastEle.appendChild(fileEle);
                 break;
             default :
                 break;
         }
-
-        comp.appendChild(fieldFirstEle);
-        comp.appendChild(fieldLastEle);
-        documentContainer.appendChild(comp);
     }
 
     /**
