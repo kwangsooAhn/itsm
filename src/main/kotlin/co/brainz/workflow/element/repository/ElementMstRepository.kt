c52dc6d5ce68d2848e7ddf8ae0308302502ed500
hcjung <hcjung@brainz.co.kr>
2020-02-27 13:50:56 +0900

    task 완료 처리 (#8100~#8102) - 미사용 import 삭제

--- src/main/kotlin/co/brainz/workflow/element/repository/ElementMstRepository.kt
+++ src/main/kotlin/co/brainz/workflow/element/repository/ElementMstRepository.kt
@@ -5,7 +5,6 @@ import co.brainz.workflow.element.entity.ElementMstEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface ElementMstRepository: JpaRepository<ElementMstEntity, String> {
