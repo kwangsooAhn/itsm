6ee6a417642d929699083aff2eb2b743ece1d1b1
hg.jung <hg.jung@brainz.co.kr>
2020-03-03 18:26:14 +0900

    validation 공통 모듈 함수

--- src/main/resources/public/assets/js/util/validation-alice.js
+++ src/main/resources/public/assets/js/util/validation-alice.js
@@ -8,11 +8,144 @@ const lowerCaseReg = /^[a-z]*$/;
 const numberReg = /^[0-9]*$/;
 const specialCharReg = /^[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]*$/;
 
-// 패스워드 validation check
-function passwordValidation(password) {
-    
+/** @brief 해당 엘리먼트 아이디의 값이 null인 경우를 판별한다.
+ *  @date 2020-03-03
+ */
+function isNull(elementId, message) {
+    if (document.getElementById(elementId).value === null) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 해당 엘리먼트 아이디의 값이 null이 아닌 경우를 판별한다.
+ *  @date 2020-03-03
+ */
+function isNotNull(elementId, message) {
+    if (document.getElementById(elementId).value !== null) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 해당 엘리먼트 아이디의 값이 ''인 경우를 판별한다.
+ *  @date 2020-03-03
+ */
+function isEmpty(elementId, message) {
+    if (document.getElementById(elementId).value === '') {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 해당 엘리먼트 아이디의 값이 ''이 아닌 경우를 판별한다.
+ *  @date 2020-03-03
+ */
+function isNotEmpty(elementId, message) {
+    if (document.getElementById(elementId).value !== '') {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 해당 엘리먼트 아이디의 값이 ''이면서 null 경우를 판별한다.
+ *  @date 2020-03-03
+ */
+function isNullAndEmpty(elementId, message) {
+    if (document.getElementById(elementId).value === null && document.getElementById(elementId).value === '') {
+        if(message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 서로 다른 엘리먼트의 값이 동일한지 판별한다.
+ *  @date 2020-03-03
+ */
+function isEquals(elementId, elementId1, message) {
+    if (document.getElementById(elementId).value === document.getElementById(elementId1).value) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 서로 다른 엘리먼트의 값이 상이한지 판별한다.
+ *  @date 2020-03-03
+ */
+function isNotEquals(elementId, elementId1, message) {
+    if (document.getElementById(elementId).value !== document.getElementById(elementId1).value) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 엘리먼트의 값이 범위 내에 있는지 판별한다.
+ *  @date 2020-03-03
+ */
+function isWithinScope(elementId, minValue, maxValue, message) {
+    if (document.getElementById(elementId).value < minValue || document.getElementById(elementId).value > maxValue) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 논리 연산자 (AND) 판별.
+ *  @date 2020-03-03
+ */
+function isAndOperator(condition, condition1, message) {
+    if (condition && condition1) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 논리 연산자 (OR) 판별.
+ *  @date 2020-03-03
+ */
+function isOrOperator(condition, condition1, message) {
+    if (condition || condition1) {
+        if (message != null) {
+            alert(i18n.get(message));
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief 패스워드 관련 유효성 검사(공백, 문자 구성, 개인정보 포함 여부)
+ *  @date 2020-03-03
+ */
+function validPassword(id) {
+
     var userId =  document.getElementById("userId").value;
     var emailId = document.getElementById("email").value.split('@');
+    var password = document.getElementById(id).value;
     var mobileNumber = document.getElementById("mobileNumber").value;
     var searchUpperCase = password.search(upperCaseReg);
     var searchLowerCase = password.search(lowerCaseReg);
@@ -41,10 +174,10 @@ function passwordValidation(password) {
             return false;
         }
     }
-    
+
     /*
      * Password Validation Check
-     * [ 설명 ] 
+     * [ 설명 ]
      * 2가지의 문자 구성인 경우 8자 이상, 20자 미만의 비밀번호를 설정한다.
      * 문자 구성 : 대문자, 소문자, 특수문자, 숫자
      */
@@ -55,17 +188,17 @@ function passwordValidation(password) {
             return false;
         }
     }
-  
+
     /*
      * Id Inclusion Validation Check
      * [ 설명 ]
-     * 비밀번호에 사용자의 ID를 포함하지 않는다. 
+     * 비밀번호에 사용자의 ID를 포함하지 않는다.
      */
     if (password.search(userId) > -1) {
         alert(i18n.get('validation.msg.pwContainsId'));
         return false;
     }
-    
+
     /*
      * Email Inclusion Validation Check
      * [ 설명 ]
@@ -75,7 +208,7 @@ function passwordValidation(password) {
         alert(i18n.get('validation.msg.pwContainsEmail'));
         return false;
     }
-    
+
     /*
      * Mobile Number Inclusion Validation Check
      * [ 설명 ]
@@ -113,53 +246,16 @@ function passwordValidation(password) {
     return true;
 }
 
+/** @brief 이메일 관련 유효성 검사(이메일 형식)
+ *  @date 2020-03-03
+ */
 // 이메일 validation check
-function emailValidation(email) {
+function validEmail(id) {
+    var email = document.getElementById(id).value;
+
     if (!emailReg.test(email)) {
         alert(i18n.get('validation.msg.checkEmailFormat'));
         return false;
     }
     return true;
 }
-
-/*
- * element 요소의 null validation을 검사한다.
- *
- */
-function validNull(id, message) {
-    if (document.getElementById(id).value === '') {
-        if (message != null) {
-            alert(i18n.get(message));
-            return true;
-        } else {
-            alert(i18n.get('validation.msg.checkNull'));
-            return true;
-        }
-    }
-    return false;
-}
-/*
- * element 요소의 value가 min과 max 사이 범위에 있는지 검사한다.
- *
- */
-function validRange(id, min, max, message) {
-    if (document.getElementById(id).value < min || document.getElementById(id).value > max) {
-        alert(i18n.get(message));
-        return true;
-    }
-    return false;
-}
-
-/*
- * 두 element 요소의 value가 일치여부 검사.
- *
- */
-function validEquals(id, anotherId, message) {
-    if (document.getElementById(id).value === document.getElementById(anotherId).value) {
-        alert(i18n.get(message));
-        return true;
-    }
-    return false;
-}
-
-
