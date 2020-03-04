6300926330d371f576a26cc1651e92983f40b803
김범호 <kbh@brainz.co.kr>
2020-03-04 10:44:55 +0900

    #8061 프로세스 저장 및 불러오기  - ProcessDto.kt 에 코드 포매터 적용함

--- src/main/kotlin/co/brainz/workflow/process/dto/ProcessDto.kt
+++ src/main/kotlin/co/brainz/workflow/process/dto/ProcessDto.kt
@@ -5,17 +5,17 @@ import java.io.Serializable
 import java.time.LocalDateTime
 
 data class ProcessDto(
-        val processId: String= "",
-        val processName: String= "",
-        val processDesc: String? = null,
-        var processStatus: String,
-        val formId: String? = null,
-        val formName: String? = null,
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        var createDt: LocalDateTime?,
-        val createUserKey: String?,
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        var updateDt: LocalDateTime? = null,
-        val updateUserKey: String? = null,
-        val enabled: Boolean? = false
+    val processId: String = "",
+    val processName: String = "",
+    val processDesc: String? = null,
+    var processStatus: String,
+    val formId: String? = null,
+    val formName: String? = null,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    var createDt: LocalDateTime?,
+    val createUserKey: String?,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    var updateDt: LocalDateTime? = null,
+    val updateUserKey: String? = null,
+    val enabled: Boolean? = false
 ) : Serializable
