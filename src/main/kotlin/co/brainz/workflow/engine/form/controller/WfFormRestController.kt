cbfe130baa3bc4ab10218443c255ca80806409d8
jy.lim <jy.lim@brainz.co.kr>
2020-03-18 16:49:24 +0900

    Merge branch 'woo.previewError.as.2000306' of gitlab.com:brainz.itsm/itsm into jylim.reqDocumentList.200306.8203

--- src/main/kotlin/co/brainz/workflow/engine/form/controller/WfFormRestController.kt
+++ src/main/kotlin/co/brainz/workflow/engine/form/controller/WfFormRestController.kt
@@ -4,14 +4,7 @@ import co.brainz.workflow.engine.WfEngine
 import co.brainz.workflow.engine.form.dto.WfFormComponentSaveDto
 import co.brainz.workflow.engine.form.dto.WfFormComponentViewDto
 import co.brainz.workflow.engine.form.dto.WfFormDto
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.transaction.Transactional
 
 @RestController
