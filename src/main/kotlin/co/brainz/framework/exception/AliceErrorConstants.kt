87304e2936f4ab246a6566c7ce28fc6e5e458bee
김범호 <kbh@brainz.co.kr>
2020-03-24 15:47:28 +0900

    사용할 http 상태코드 AliceHttpStatusConstants.kt를 정의하고 에러 상수 클래스 AliceErrorConstants.kt에 반영

--- src/main/kotlin/co/brainz/framework/exception/AliceErrorConstants.kt
+++ src/main/kotlin/co/brainz/framework/exception/AliceErrorConstants.kt
@@ -1,9 +1,18 @@
 package co.brainz.framework.exception
 
-enum class AliceErrorConstants(val code: String, val detail: String) {
-    ERR("ERR", "Default error"),
-    ERR_00001("ERR-00001", "Wrong id or passwd"),
-    ERR_00002("ERR-00002", "Session expired"),
-    ERR_00003("ERR-00003", "Unauthorized page."),
-    ERR_00004("ERR-00004", "The file extension is not allowed")
+/**
+ * 에러 코드 정의.
+ *
+ */
+enum class AliceErrorConstants(
+    val httpStatus: Int = AliceHttpStatusConstants.INTERNAL_SERVER_ERROR.code,
+    val code: String = "DEFAULT",
+    val detailMessage: String
+) {
+    ERR(AliceHttpStatusConstants.NOT_ACCEPTABLE.code, "ERR-DEFAULT", "Default error"),
+    ERR_00001(AliceHttpStatusConstants.BAD_REQUEST.code, "ERR-00001", "Wrong id or passwd"),
+    ERR_00002(AliceHttpStatusConstants.NOT_FOUND.code, "ERR-00002", "Session expired"),
+    ERR_00003(AliceHttpStatusConstants.UNAUTHORIZED.code, "ERR-00003", "Unauthorized page."),
+    ERR_00004(AliceHttpStatusConstants.BAD_REQUEST.code, "ERR-00004", "The file extension is not allowed"),
+    ERR_00005(AliceHttpStatusConstants.NOT_ACCEPTABLE.code, "ERR-00005", "Not Acceptable")
 }
