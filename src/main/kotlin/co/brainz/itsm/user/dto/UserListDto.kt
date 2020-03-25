762c5eea6eec6660348acf8ad1cdcfc89005eb3c
jje <jje@brainz.co.kr>
2020-03-25 14:14:06 +0900

    #8282 user task의 assignee type에 따라 assignee 입력 형태 변경

--- src/main/kotlin/co/brainz/itsm/user/dto/UserListDto.kt
+++ src/main/kotlin/co/brainz/itsm/user/dto/UserListDto.kt
@@ -1,7 +1,9 @@
 package co.brainz.itsm.user.dto
 
+import java.io.Serializable
+
 data class UserListDto(
         var userKey: String,
         var userId: String,
         var userName: String
-)
+) : Serializable
