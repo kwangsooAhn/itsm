bddedd0065cf1c368858c174a4ba58c655965cc4
hg.jung <hg.jung@brainz.co.kr>
2020-03-25 16:44:31 +0900

    AliceIpVerification 형식으로 파일명 변경

--- src/main/kotlin/co/brainz/framework/auth/entity/AliceIpVerificationEntity.kt
+++ src/main/kotlin/co/brainz/framework/auth/entity/AliceIpVerificationEntity.kt
@@ -8,8 +8,8 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
-@Table(name = "awf_access_allow")
-data class AliceAccessAllowEntity(
+@Table(name = "awf_ip_verification")
+data class AliceIpVerificationEntity(
         @Id
         @Column(name = "ip_addr", length = 128)
         var ipAddr: String,
--- src/main/kotlin/co/brainz/framework/auth/repository/AliceIpVerificationRepository.kt
+++ src/main/kotlin/co/brainz/framework/auth/repository/AliceIpVerificationRepository.kt
@@ -1,9 +1,9 @@
 package co.brainz.framework.auth.repository
 
-import co.brainz.framework.auth.entity.AliceAccessAllowEntity
+import co.brainz.framework.auth.entity.AliceIpVerificationEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AliceAccessAllowRepository: JpaRepository<AliceAccessAllowEntity, String> {
+interface AliceIpVerificationRepository: JpaRepository<AliceIpVerificationEntity, String> {
 }
