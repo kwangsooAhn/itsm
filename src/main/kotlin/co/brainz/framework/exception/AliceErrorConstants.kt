b0b1d22ddbc30456b3eb928e08501baf2dcc1595
김범호 <kbh@brainz.co.kr>
2020-03-27 09:52:26 +0900

    메시지 용어 변경함

--- src/main/kotlin/co/brainz/framework/exception/AliceErrorConstants.kt
+++ src/main/kotlin/co/brainz/framework/exception/AliceErrorConstants.kt
@@ -10,7 +10,7 @@ enum class AliceErrorConstants(
     val message: String
 ) {
     ERR(AliceHttpStatusConstants.NOT_ACCEPTABLE.status, "ERR-DEFAULT", "Default error"),
-    ERR_00001(AliceHttpStatusConstants.BAD_REQUEST.status, "ERR-00001", "Wrong id or passwd"),
+    ERR_00001(AliceHttpStatusConstants.BAD_REQUEST.status, "ERR-00001", "Wrong id or password"),
     ERR_00002(AliceHttpStatusConstants.NOT_FOUND.status, "ERR-00002", "Session expired"),
     ERR_00003(AliceHttpStatusConstants.UNAUTHORIZED.status, "ERR-00003", "Unauthorized page."),
     ERR_00004(AliceHttpStatusConstants.BAD_REQUEST.status, "ERR-00004", "The file extension is not allowed")
