c9f5e04d59b4b7f5ae4e2cd8a3057eed7b7d7760
hg.jung <hg.jung@brainz.co.kr>
2020-04-02 10:43:27 +0900

    fetch inner join 적용

--- src/main/kotlin/co/brainz/framework/fileTransaction/repository/AliceFileOwnMapRepository.kt
+++ src/main/kotlin/co/brainz/framework/fileTransaction/repository/AliceFileOwnMapRepository.kt
@@ -4,11 +4,13 @@ import co.brainz.framework.fileTransaction.entity.AliceFileLocEntity
 import co.brainz.framework.fileTransaction.entity.AliceFileOwnMapEntity
 import co.brainz.framework.fileTransaction.entity.idclass.AliceFileCompositeKey
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface AliceFileOwnMapRepository : JpaRepository<AliceFileOwnMapEntity, AliceFileCompositeKey> {
     fun findAllByOwnId(ownId: String): List<AliceFileOwnMapEntity>
+    @Query("select f from AliceFileOwnMapEntity f inner join fetch f.fileLocEntity where f.ownId = :ownId and f.fileLocEntity.uploaded = :fileLocEntityUploaded")
     fun findAllByOwnIdAndFileLocEntityUploaded(ownId: String, fileLocEntityUploaded: Boolean): List<AliceFileOwnMapEntity>
     fun findByFileLocEntityFileSeq(fileSeq: Long): AliceFileOwnMapEntity
     fun deleteByFileLocEntity(aliceFileLocEntity: AliceFileLocEntity)
