8973d44ceb5f232542b7c2c8ea2063e45addf3dd
lizeelf <lizeelf@brianz.co.kr>
2020-04-07 18:15:53 +0900

    1. 불필요한 import 삭제

--- src/main/kotlin/co/brainz/workflow/engine/process/service/WfProcessService.kt
+++ src/main/kotlin/co/brainz/workflow/engine/process/service/WfProcessService.kt
@@ -19,7 +19,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
-import kotlin.collections.LinkedHashMap
 
 @Service
 @Transactional
