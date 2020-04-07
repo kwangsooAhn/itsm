b008264665cf9e9b67a44ef47ec0364849ad0478
hg.jung <hg.jung@brainz.co.kr>
2020-04-07 11:42:14 +0900

    파일 확장자 제한 강화 관련 커밋 진행

---
+++ src/main/kotlin/co/brainz/framework/fileTransaction/repository/AliceFileNameExtensionRepository.kt
@@ -0,0 +1,9 @@
+package co.brainz.framework.fileTransaction.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import co.brainz.framework.fileTransaction.entity.AliceFileNameExtensionEntity
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AliceFileNameExtensionRepository:JpaRepository<AliceFileNameExtensionEntity, String> {
+}
