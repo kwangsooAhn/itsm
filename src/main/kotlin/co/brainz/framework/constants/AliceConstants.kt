8a4b3ce4f6cafd4220c380921c3a1072671cc590
hg.jung <hg.jung@brainz.co.kr>
2020-04-07 11:40:57 +0900

    파일 확장자 제한 강화 관련 커밋 진행

--- src/main/kotlin/co/brainz/framework/constants/AliceConstants.kt
+++ src/main/kotlin/co/brainz/framework/constants/AliceConstants.kt
@@ -45,7 +45,8 @@ object AliceConstants {
         PATTEN97("/filelist"),
         PATTEN98("/filedel"),
         PATTEN99("/fileSubmit"),
-        PATTEN100("/i18n/**")
+        PATTEN100("/fileNameExtensionList"),
+        PATTEN101("/i18n/**")
         ;
 
         companion object {
