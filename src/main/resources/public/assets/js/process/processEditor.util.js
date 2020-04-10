75b6b9218721f869ef819ce00aaf5ab77b00a6a2
jje <jje@brainz.co.kr>
2020-04-10 17:04:00 +0900

    MR 응대 수정

--- src/main/resources/public/assets/js/process/processEditor.util.js
+++ src/main/resources/public/assets/js/process/processEditor.util.js
@@ -23,7 +23,7 @@
         if (isEdited) {
             event.returnValue = '';
         } else {
-            delete event['returnValue'];
+            delete event.returnValue;
         }
     });
 
