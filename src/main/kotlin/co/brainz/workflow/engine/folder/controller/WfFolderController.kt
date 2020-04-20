26218db22d6b6d29f096d67ff99fc5880825ab60
hcjung <hcjung@brainz.co.kr>
2020-04-20 17:42:06 +0900

    동일한 folder_id 유지하도록 수정. 자동생성을 하지 않고 직접 생성.

--- src/main/kotlin/co/brainz/workflow/engine/folder/controller/WfFolderController.kt
+++ src/main/kotlin/co/brainz/workflow/engine/folder/controller/WfFolderController.kt
@@ -2,7 +2,10 @@ package co.brainz.workflow.engine.folder.controller
 
 import co.brainz.workflow.engine.folder.dto.WfFolderDto
 import co.brainz.workflow.engine.folder.service.WfFolderService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/rest/wf/folders")
--- src/main/kotlin/co/brainz/workflow/engine/folder/service/WfFolderService.kt
+++ src/main/kotlin/co/brainz/workflow/engine/folder/service/WfFolderService.kt
@@ -6,6 +6,7 @@ import co.brainz.workflow.engine.folder.entity.WfFolderEntity
 import co.brainz.workflow.engine.folder.repository.WfFolderRepository
 import co.brainz.workflow.engine.instance.entity.WfInstanceEntity
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class WfFolderService(
@@ -14,6 +15,7 @@ class WfFolderService(
     fun createFolder(instance: WfInstanceEntity) {
         wfFolderRepository.save(
             WfFolderEntity(
+                folderId = UUID.randomUUID().toString().replace("-", ""),
                 instance = instance,
                 relatedType = WfFolderConstants.RelatedType.ORIGIN.code
             )
