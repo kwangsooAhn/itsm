e826f51ba81690ef15af3d48d8cf4e6945c2baaa
hcjung <hcjung@brainz.co.kr>
2020-04-21 17:14:08 +0900

    dto들 들여쓰기 수정

--- src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateFormDto.kt
+++ src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateFormDto.kt
@@ -4,14 +4,13 @@ import java.io.Serializable
 import java.time.LocalDateTime
 
 data class RestTemplateFormDto(
-        var id: String = "",
-        var name: String = "",
-        var status: String = "",
-        var desc: String? = null,
-        var editable: Boolean = false,
-        var createUserKey: String? = null,
-        var createDt: LocalDateTime? = null,
-        val updateUserKey: String? = null,
-        var updateDt: LocalDateTime? = null
+    var id: String = "",
+    var name: String = "",
+    var status: String = "",
+    var desc: String? = null,
+    var editable: Boolean = false,
+    var createUserKey: String? = null,
+    var createDt: LocalDateTime? = null,
+    val updateUserKey: String? = null,
+    var updateDt: LocalDateTime? = null
 ) : Serializable
-
--- src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateInstanceCountDto.kt
+++ src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateInstanceCountDto.kt
@@ -3,6 +3,6 @@ package co.brainz.workflow.provider.dto
 import java.io.Serializable
 
 data class RestTemplateInstanceCountDto(
-        val instanceStatus: String,
-        val instanceCount: Int? = 0
+    val instanceStatus: String,
+    val instanceCount: Int? = 0
 ) : Serializable
