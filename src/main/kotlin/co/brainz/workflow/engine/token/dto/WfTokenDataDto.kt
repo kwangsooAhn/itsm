ebfa3957e182069447aba0cef524ea2b86aa6e68
김범호 <kbh@brainz.co.kr>
2020-04-24 18:59:33 +0900

    버그 유발 dto 필드값 제거함.

--- src/main/kotlin/co/brainz/workflow/engine/token/dto/WfTokenDataDto.kt
+++ src/main/kotlin/co/brainz/workflow/engine/token/dto/WfTokenDataDto.kt
@@ -3,7 +3,6 @@ package co.brainz.workflow.engine.token.dto
 import java.io.Serializable
 
 data class WfTokenDataDto(
-    var tokenId: String = "",
     var componentId: String = "",
     var value: String = ""
 ) : Serializable
