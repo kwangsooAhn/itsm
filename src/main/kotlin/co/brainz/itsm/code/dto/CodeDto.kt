bb62f3757a166091b8c5a5bc24b2cbe74431a577
hg.jung <hg.jung@brainz.co.kr>
2020-04-24 21:15:16 +0900

    CodeDto 수정

--- src/main/kotlin/co/brainz/itsm/code/dto/CodeDto.kt
+++ src/main/kotlin/co/brainz/itsm/code/dto/CodeDto.kt
@@ -1,11 +1,12 @@
 package co.brainz.itsm.code.dto
 
+import co.brainz.itsm.code.entity.CodeEntity
 import java.io.Serializable
 import java.time.LocalDateTime
 
 data class CodeDto (
     var code: String = "",
-    var pCode: String? = null,
+    var pCode: CodeEntity? = null,
     var codeValue: String? = null,
     var editable: Boolean? = true,
     var createDt: LocalDateTime? = null,
