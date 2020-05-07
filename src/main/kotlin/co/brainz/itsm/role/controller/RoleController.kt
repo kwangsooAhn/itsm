74782e9a74687a629be60b9a9b84c48142b8583c
hcjung <hcjung@brainz.co.kr>
2020-05-07 10:59:42 +0900

    URL Refactoring - Role

--- src/main/kotlin/co/brainz/itsm/role/controller/RoleController.kt
+++ src/main/kotlin/co/brainz/itsm/role/controller/RoleController.kt
@@ -1,12 +1,12 @@
 package co.brainz.itsm.role.controller
 
 import co.brainz.itsm.role.service.RoleService
+import javax.servlet.http.HttpServletRequest
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import javax.servlet.http.HttpServletRequest
 
 @RequestMapping("/roles")
 @Controller
@@ -19,10 +19,10 @@ public class RoleController(private val roleService: RoleService) {
      * 역할  설정 뷰를 호출한다.
      */
     @GetMapping("/edit", "")
-    fun getRolelist(request: HttpServletRequest, model: Model): String {
+    fun getRoleList(request: HttpServletRequest, model: Model): String {
 
-        var roleAllList = roleService.selectRoleList()
-        var authAllList = roleService.selectAuthList()
+        val roleAllList = roleService.selectRoleList()
+        val authAllList = roleService.selectAuthList()
         model.addAttribute("authList", authAllList)
         model.addAttribute("roleList", roleAllList)
 
