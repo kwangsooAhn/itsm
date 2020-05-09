862120be1760868ff7cba0c32e523f4c288562fd
hcjung <hcjung@brainz.co.kr>
2020-05-09 12:04:39 +0900

    URL Refactoring - Token

--- src/main/kotlin/co/brainz/itsm/token/controller/TokenController.kt
+++ src/main/kotlin/co/brainz/itsm/token/controller/TokenController.kt
@@ -7,6 +7,7 @@ import co.brainz.itsm.folder.service.FolderService
 import co.brainz.itsm.instance.service.InstanceService
 import co.brainz.itsm.token.service.TokenService
 import co.brainz.itsm.user.service.UserService
+import javax.servlet.http.HttpServletRequest
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import javax.servlet.http.HttpServletRequest
 
 @Controller
 @RequestMapping("/tokens")
