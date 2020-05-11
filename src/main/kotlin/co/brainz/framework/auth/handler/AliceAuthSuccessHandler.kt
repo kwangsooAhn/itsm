f8f263b3b356b859da2d58939c00d003dc0266ae
lizeelf <lizeelf@brianz.co.kr>
2020-05-11 14:30:10 +0900

    1. 로그아웃시 팝업에서 로그인 시도 관련 문제 수정

--- src/main/kotlin/co/brainz/framework/auth/handler/AliceAuthSuccessHandler.kt
+++ src/main/kotlin/co/brainz/framework/auth/handler/AliceAuthSuccessHandler.kt
@@ -8,7 +8,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.LocaleResolver
 
@@ -21,7 +21,7 @@ import org.springframework.web.servlet.LocaleResolver
 @Component
 class AliceAuthSuccessHandler(
     private val localeResolver: LocaleResolver
-) : SavedRequestAwareAuthenticationSuccessHandler() {
+) : SimpleUrlAuthenticationSuccessHandler() {
 
     private val thisLogger: Logger = LoggerFactory.getLogger(this::class.java)
 
