e9d1904861b237dac69108340587daba1ca5d96e
jje <jje@brainz.co.kr>
2020-05-14 10:17:23 +0900

    #8093 [Import/Export]

--- src/main/resources/public/assets/js/process/process.util.js
+++ src/main/resources/public/assets/js/process/process.util.js
@@ -9,12 +9,13 @@
      * import process.
      */
     function importProcess() {
-        const saveAsProcessData = {};
+        console.log('import');
+        /*const saveAsProcessData = {};
 
         const elements = saveAsProcessData.elements;
         elements.forEach(function(element) {
-            //const category = getElementCategory(element.type);
-            //element['required'] = getAttributeRequired(category, element.type);
+            // const category = getElementCategory(element.type);
+            // element['required'] = getAttributeRequired(category, element.type);
         });
 
         aliceJs.sendXhr({
@@ -29,7 +30,7 @@
             },
             contentType: 'application/json; charset=utf-8',
             params: JSON.stringify(saveAsProcessData)
-        });
+        });*/
     }
 
     /**
@@ -38,13 +39,54 @@
      * @param processId 프로세스ID
      */
     function exportProcess(processId) {
+        let attributeData = {};
+        /**
+         * load element attribute data.
+         */
+        const loadElementData = function() {
+            aliceJs.sendXhr({
+                method: 'GET',
+                async: false,
+                url: '/assets/js/process/elementAttribute.json',
+                callbackFunc: function(xhr) {
+                    attributeData = JSON.parse(xhr.responseText);
+                },
+                contentType: 'application/json; charset=utf-8',
+                showProgressbar: false
+            });
+        };
+
+        /**
+         * load process data.
+         */
         aliceJs.sendXhr({
             method: 'GET',
             url: '/rest/processes/' + processId + '/data',
             callbackFunc: function(xhr) {
-                JSON.parse(xhr.responseText);
+                loadElementData();
+                const processData = JSON.parse(xhr.responseText);
+                console.debug(attributeData);
+                console.debug(processData);
+
+                const xmlDoc = document.implementation.createDocument('', '', null);
+                let definitions = xmlDoc.createElement('definitions', );
+                let process = xmlDoc.createElement('process');
+                process.setAttribute('id', processData.process.id);
+                process.setAttribute('name', processData.process.name);
+                process.setAttribute('description', processData.process.description);
+                definitions.append(process);
+
+                let diagram = xmlDoc.createElement('diagram');
+                definitions.append(diagram);
+
+                xmlDoc.append(definitions);
+
+                let serializer = new XMLSerializer();
+                let xmlString = serializer.serializeToString(xmlDoc);
+                console.log(xmlString);
             },
-            contentType: 'application/json; charset=utf-8'
+            contentType: 'application/json; charset=utf-8',
+            showProgressbar: false
         });
     }
 
