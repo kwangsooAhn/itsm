cf9c30bf5302e0df28d30fde2923c191e1e79953
hg.jung <hg.jung@brainz.co.kr>
2020-05-17 12:53:46 +0900

    로직 수정 진행

--- src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateInstanceListDto.kt
+++ src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateInstanceListDto.kt
@@ -9,5 +9,5 @@ data class RestTemplateInstanceListDto(
     var documentNo: String?,
     var instanceStartDt: LocalDateTime?,
     var instanceEndDt: LocalDateTime?,
-    var instanceCreateUserName: String?
+    var instanceCreateUserKey: String?
 ) : Serializable
