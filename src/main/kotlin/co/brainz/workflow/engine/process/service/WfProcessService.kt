1f4c36933f316eb4b943fda80729c676c01f51d5
김범호 <kbh@brainz.co.kr>
2020-05-19 09:41:35 +0900

    import 순서 변경

--- src/main/kotlin/co/brainz/workflow/engine/process/service/WfProcessService.kt
+++ src/main/kotlin/co/brainz/workflow/engine/process/service/WfProcessService.kt
@@ -17,11 +17,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
+import java.util.UUID
 import org.mapstruct.factory.Mappers
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 @Transactional
