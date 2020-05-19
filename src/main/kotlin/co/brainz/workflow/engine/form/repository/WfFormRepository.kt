cd22bd0124847694d7ec3da9ddb92f573d642bc2
lizeelf <lizeelf@brianz.co.kr>
2020-05-19 16:07:41 +0900

    1. 불필요한 내용 제거

--- src/main/kotlin/co/brainz/workflow/engine/form/repository/WfFormRepository.kt
+++ src/main/kotlin/co/brainz/workflow/engine/form/repository/WfFormRepository.kt
@@ -1,26 +1,11 @@
 package co.brainz.workflow.engine.form.repository
 
 import co.brainz.workflow.engine.form.entity.WfFormEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import java.util.Optional
+import org.springframework.data.jpa.repository.JpaRepository
 
 interface WfFormRepository : JpaRepository<WfFormEntity, String>, WfFormRepositoryCustom {
 
-    @Query(
-        "SELECT m " +
-                "FROM WfFormEntity m " +
-                "WHERE (lower(m.formName) like lower(concat('%', :value, '%')) or lower(m.formDesc) like lower(concat('%', :value, '%')) or :value is null or :value = '') " +
-                "ORDER BY " +
-                "CASE " +
-                "WHEN m.formStatus = 'form.status.edit' THEN 1 " +
-                "WHEN m.formStatus = 'form.status.publish' THEN 2 " +
-                "WHEN m.formStatus = 'form.status.use' THEN 3 " +
-                "WHEN m.formStatus = 'form.status.destroy' THEN 4 " +
-                "END, COALESCE(m.updateDt, m.createDt) DESC"
-    )
-    fun findFormListOrFormSearchList(value: String?): List<WfFormEntity>
     fun findWfFormEntityByFormId(formId: String): Optional<WfFormEntity>
-    fun findByFormStatusInOrderByFormName(formStatus: List<String>): List<WfFormEntity>
     fun removeWfFormEntityByFormId(formId: String)
 }
