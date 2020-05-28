4244604d875c485e388dbef82b6836aacd0b3ba8
이소현 <sh-lee@brainz.co.kr>
2020-05-28 14:59:14 +0900

    #8270 알림 기능 구현 (MR 수정사항 반영)

--- src/main/kotlin/co/brainz/framework/notification/entity/NotificationEntity.kt
+++ src/main/kotlin/co/brainz/framework/notification/entity/NotificationEntity.kt
@@ -2,7 +2,6 @@ package co.brainz.framework.notification.entity
 
 import co.brainz.framework.auditor.AliceMetaEntity
 import co.brainz.framework.auth.entity.AliceUserEntity
-import org.hibernate.annotations.GenericGenerator
 import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -12,26 +11,27 @@ import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.OneToOne
 import javax.persistence.Table
+import org.hibernate.annotations.GenericGenerator
 
 @Entity
 @Table(name = "awf_notification")
 data class NotificationEntity(
     @Id @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(name = "notification_id")
+    @Column(name = "notification_id", length = 128)
     var notificationId: String = "",
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "received_user")
     var receivedUser: AliceUserEntity = AliceUserEntity(),
 
-    @Column(name = "title")
+    @Column(name = "title", length = 128)
     var title: String = "",
 
-    @Column(name = "message")
+    @Column(name = "message", length = 1024)
     var message: String? = null,
 
-    @Column(name = "instance_id")
+    @Column(name = "instance_id", length = 128)
     var instanceId: String? = null,
 
     @Column(name = "confirm_yn")
