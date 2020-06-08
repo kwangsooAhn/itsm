a908cb1e58a5bc13da898354c1c931ddc815152a
hg.jung <hg.jung@brainz.co.kr>
2020-06-08 17:25:05 +0900

    불필요한 import 제거

--- src/main/kotlin/co/brainz/workflow/engine/process/repository/WfProcessRepository.kt
+++ src/main/kotlin/co/brainz/workflow/engine/process/repository/WfProcessRepository.kt
@@ -2,7 +2,6 @@ package co.brainz.workflow.engine.process.repository
 
 import co.brainz.workflow.engine.process.entity.WfProcessEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
