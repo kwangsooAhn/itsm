9c6b63b05960400cc7919a72419054cf1d1db463
lizeelf <lizeelf@brianz.co.kr>
2020-06-09 11:22:36 +0900

    1. Ktlint 수정 - 1차

--- src/main/kotlin/co/brainz/itsm/code/controller/CodeController.kt
+++ src/main/kotlin/co/brainz/itsm/code/controller/CodeController.kt
@@ -1,11 +1,11 @@
 package co.brainz.itsm.code.controller
 
 import co.brainz.itsm.code.service.CodeService
+import javax.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import javax.servlet.http.HttpServletRequest
 
 @Controller
 @RequestMapping("/codes")
