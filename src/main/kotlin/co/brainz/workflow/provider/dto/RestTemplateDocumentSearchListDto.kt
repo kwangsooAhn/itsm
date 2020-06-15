ccd158fa5ef67e9bd816df0e0006e95dd642dbaa
woodajung <wdj@brainz.co.kr>
2020-06-15 16:57:36 +0900

    #8268 신청서 양식 그룹핑 기능

--- src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateDocumentSearchListDto.kt
+++ src/main/kotlin/co/brainz/workflow/provider/dto/RestTemplateDocumentSearchListDto.kt
@@ -6,6 +6,7 @@ import java.io.Serializable
  * 신청서를 검색할 때 사용하는 DTO 클래스
  */
 data class RestTemplateDocumentSearchListDto(
+    var searchGroupName: String? = "",
     var searchDocuments: String? = "",
     var searchDocumentType: String? = "",
     var searchDocumentStatus: String? = "",
