449577831d5b6d91e64236dcd7e913615a480e1a
hcjung <hcjung@brainz.co.kr>
2020-06-16 17:38:38 +0900

    검색조건에 따른 인스턴스 상태값 그룹 enum 생성, waiting 상태로 변경하는 함수 작성 (#8637)

--- src/main/kotlin/co/brainz/workflow/instance/constants/WfInstanceConstants.kt
+++ src/main/kotlin/co/brainz/workflow/instance/constants/WfInstanceConstants.kt
@@ -10,4 +10,23 @@ class WfInstanceConstants {
         WAITING("waiting"),
         FINISH("finish")
     }
+
+    enum class SearchType(val code: String) {
+        TODO("token.type.todo"),
+        REQUESTED("token.type.requested"),
+        PROGRESS("token.type.progress"),
+        COMPLETED("token.type.completed")
+    }
+
+    companion object {
+        fun getTargetStatusGroup(searchType: Status): Array<String>? {
+            return when (searchType) {
+                SearchType.TODO -> arrayOf(Status.RUNNING.code)
+                SearchType.REQUESTED -> null
+                SearchType.PROGRESS -> arrayOf(Status.RUNNING.code, Status.WAITING.code)
+                SearchType.COMPLETED -> arrayOf(Status.FINISH.code)
+                else -> null
+            }
+        }
+    }
 }
