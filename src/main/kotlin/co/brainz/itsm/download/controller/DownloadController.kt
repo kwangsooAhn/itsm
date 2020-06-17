a0ee94cf242ded7a49c5a92fbad078a77a23cd5b
hg.jung <hg.jung@brainz.co.kr>
2020-06-17 14:12:32 +0900

    MR응대

--- src/main/kotlin/co/brainz/itsm/download/controller/DownloadController.kt
+++ src/main/kotlin/co/brainz/itsm/download/controller/DownloadController.kt
@@ -4,8 +4,8 @@ import co.brainz.itsm.code.service.CodeService
 import co.brainz.itsm.download.constants.DownloadConstants
 import co.brainz.itsm.download.dto.DownloadSearchDto
 import co.brainz.itsm.download.service.DownloadService
-import org.springframework.ui.Model
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
