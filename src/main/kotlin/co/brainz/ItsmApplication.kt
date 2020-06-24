1897244f85ef0ced0ef06159fa3368e23c610a5a
hg.jung <hg.jung@brainz.co.kr>
2020-06-24 10:06:10 +0900

    부분 수정 진행

--- src/main/kotlin/co/brainz/ItsmApplication.kt
+++ src/main/kotlin/co/brainz/ItsmApplication.kt
@@ -4,11 +4,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.bind.annotation.RestController
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @RestController
 @EnableScheduling
 @SpringBootApplication
-open class ItsmApplication
+open class ItsmApplication {
+
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ItsmApplication>(*args)
