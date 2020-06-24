97dd1c6b279716d2cddfae13bb178b9f5045d96f
hg.jung <hg.jung@brainz.co.kr>
2020-06-24 13:55:48 +0900

    WfDocumentRepositoryImpl 수정 진행

--- src/main/kotlin/co/brainz/ItsmApplication.kt
+++ src/main/kotlin/co/brainz/ItsmApplication.kt
@@ -1,6 +1,5 @@
 package co.brainz
 
-import co.brainz.framework.constants.AliceConstants
 import java.util.TimeZone
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -13,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController
 open class ItsmApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     runApplication<ItsmApplication>(*args)
-    TimeZone.setDefault(TimeZone.getTimeZone(AliceConstants.DEFAULT_TIMEZONE))
 }
