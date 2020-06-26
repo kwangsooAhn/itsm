5e91fb223834d93c6773fb1cd824b267a6ec6fd5
code-Blanc <codeblanx@gmail.com>
2020-06-26 18:24:12 +0900

    #8446 관련문서 목록에 문서번호 추가.
    - 문서번호 클릭시 팝업 생성

--- src/main/kotlin/co/brainz/workflow/folder/repository/WfFolderRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/workflow/folder/repository/WfFolderRepositoryImpl.kt
@@ -13,42 +13,50 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class WfFolderRepositoryImpl : QuerydslRepositorySupport(WfFolderEntity::class.java),
-    WfFolderRepositoryCustom {
+	WfFolderRepositoryCustom {
 
-    override fun findRelatedDocumentListByTokenId(tokenId: String): List<RestTemplateFolderDto> {
-        val folder = QWfFolderEntity.wfFolderEntity
-        val user = QAliceUserEntity.aliceUserEntity
-        val token = QWfTokenEntity.wfTokenEntity
-        val queryTokenId = JPAExpressions.select(folder.folderId)
-            .from(folder, token)
-            .where(
-                folder.instance.eq(token.instance).and(token.tokenId.eq(tokenId)).and(folder.relatedType.eq("origin"))
-            )
-        return from(folder)
-            .select(
-                Projections.constructor(
-                    RestTemplateFolderDto::class.java,
-                    folder.folderId,
-                    folder.instance.instanceId,
-                    folder.relatedType,
-                    ExpressionUtils.`as`(folder.instance.documentNo,"tokenId"),
-                    folder.instance.documentNo,
-                    folder.instance.document.documentName,
-                    folder.createUserKey,
-                    folder.createDt,
-                    folder.instance.instanceStartDt,
-                    folder.instance.instanceEndDt,
-                    user.userKey,
-                    user.userName
-                )
-            )
-            .leftJoin(user).on(folder.instance.instanceCreateUser.userKey.eq(user.userKey))
-            .leftJoin(token).on(folder.instance.instanceId.eq(token.instance.instanceId))
-            .where(
-                folder.folderId.eq(queryTokenId)
-            )
-            .where(folder.relatedType.eq("reference").or(folder.relatedType.eq("related")))
-            .orderBy(folder.instance.instanceStartDt.asc())
-            .fetch()
-    }
+	override fun findRelatedDocumentListByTokenId(tokenId: String): List<RestTemplateFolderDto> {
+		val folder = QWfFolderEntity.wfFolderEntity
+		val user = QAliceUserEntity.aliceUserEntity
+		val token = QWfTokenEntity.wfTokenEntity
+		val queryTokenId = JPAExpressions.select(folder.folderId)
+			.from(folder, token)
+			.where(
+				folder.instance.eq(token.instance).and(token.tokenId.eq(tokenId)).and(
+					folder.relatedType.eq(
+						"origin"
+					)
+				)
+			)
+		return from(folder)
+			.select(
+				Projections.constructor(
+					RestTemplateFolderDto::class.java,
+					folder.folderId,
+					folder.instance.instanceId,
+					folder.relatedType,
+					ExpressionUtils.`as`(
+						JPAExpressions.select(token.tokenId.max())
+							.from(token)
+							.where(folder.instance.instanceId.eq(token.instance.instanceId))
+						, "tokenId"
+					),
+					folder.instance.documentNo,
+					folder.instance.document.documentName,
+					folder.createUserKey,
+					folder.createDt,
+					folder.instance.instanceStartDt,
+					folder.instance.instanceEndDt,
+					user.userKey,
+					user.userName
+				)
+			)
+			.leftJoin(user).on(folder.instance.instanceCreateUser.userKey.eq(user.userKey))
+			.where(
+				folder.folderId.eq(queryTokenId)
+			)
+			.where(folder.relatedType.eq("reference").or(folder.relatedType.eq("related")))
+			.orderBy(folder.instance.instanceStartDt.asc())
+			.fetch()
+	}
 }
