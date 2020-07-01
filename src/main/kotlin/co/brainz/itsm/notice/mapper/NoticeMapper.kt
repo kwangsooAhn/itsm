a052bd6d265001a45f9f295f7e7d60c10e29f3df
hcjung <hcjung@brainz.co.kr>
2020-07-01 09:57:31 +0900

    kapt 경고 수정 #1 - mapping 안하는 필드 ignore 속성 추가

--- src/main/kotlin/co/brainz/itsm/notice/mapper/NoticeMapper.kt
+++ src/main/kotlin/co/brainz/itsm/notice/mapper/NoticeMapper.kt
@@ -6,9 +6,14 @@ import co.brainz.itsm.notice.dto.NoticePopupDto
 import co.brainz.itsm.notice.dto.NoticePopupListDto
 import co.brainz.itsm.notice.entity.NoticeEntity
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 
 @Mapper
 interface NoticeMapper {
+    @Mappings(
+        Mapping(target = "createUserKey", ignore = true)
+    )
     fun toNoticePopupDto(noticeEntity: NoticeEntity): NoticePopupDto
 
     fun toNoticePopupListDto(noticeEntity: NoticeEntity): NoticePopupListDto
