c2bbacdf357e54ec3e76acc2c8009ee56e4ec7f4
김범호 <kbh@brainz.co.kr>
2020-07-20 17:37:26 +0900

    erd 업데이트 - portal_faq.faq_title 오타 수정

--- src/main/erd/sql/1. execute_create_all_tables.sql
+++ src/main/erd/sql/1. execute_create_all_tables.sql
@@ -1936,7 +1936,7 @@ CREATE TABLE portal_faq
 (
 	faq_id varchar(128) NOT NULL,
 	faq_group varchar(100) NOT NULL,
-	faq_titile varchar(512) NOT NULL,
+    faq_title varchar(512) NOT NULL,
 	faq_content text NOT NULL,
 	create_user_key varchar(128) NOT NULL,
 	create_dt timestamp NOT NULL,
@@ -1948,7 +1948,7 @@ CREATE TABLE portal_faq
 COMMENT ON TABLE portal_faq IS 'FAQ정보';
 COMMENT ON COLUMN portal_faq.faq_id IS 'FAQ아이디';
 COMMENT ON COLUMN portal_faq.faq_group IS 'FAQ 그룹';
-COMMENT ON COLUMN portal_faq.faq_titile IS 'FAQ 제목';
+COMMENT ON COLUMN portal_faq.faq_title IS 'FAQ 제목';
 COMMENT ON COLUMN portal_faq.faq_content IS 'FAQ 내용';
 COMMENT ON COLUMN portal_faq.create_user_key IS '등록자';
 COMMENT ON COLUMN portal_faq.create_dt IS '등록일';
