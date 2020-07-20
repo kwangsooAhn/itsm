8bc670c746b2c1e490f7a9f1e75801c888098986
김범호 <kbh@brainz.co.kr>
2020-07-20 15:25:43 +0900

    허용 IP 목록에 대한 entity 를 바로 리턴해서 발생하는 오류를 수정함

---
+++ src/main/kotlin/co/brainz/framework/auth/dto/AliceIpVerificationDto.kt
@@ -0,0 +1,8 @@
+package co.brainz.framework.auth.dto
+
+import java.io.Serializable
+
+data class AliceIpVerificationDto(
+    var ipAddr: String = "",
+    var ipExplain: String? = ""
+) : Serializable
