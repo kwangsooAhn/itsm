23c1e45dabb04f12693d448b6333389503dbf7a7
김범호 <kbh@brainz.co.kr>
2020-07-21 17:19:57 +0900

    test 디렉토리 아래 통합 sql 만드는 소스 업데이트

---
+++ src/test/kotlin/erd/CreateSqlWithErd.kt
@@ -0,0 +1,17 @@
+import java.io.FileFilter
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+
+fun main() {
+    val executePath = Paths.get("src/main/erd/sql/execute/execute_all_tables.sql")
+    val executeSql = executePath.toFile()
+    if (executeSql.canRead()) {
+        executeSql.delete()
+    }
+    executeSql.createNewFile()
+    val sqlFiles = Paths.get("src/main/erd/sql").toFile().listFiles(FileFilter { it.isFile })
+    for (sql in sqlFiles!!) {
+        Files.write(executePath, sql.readBytes(), StandardOpenOption.APPEND)
+    }
+}
