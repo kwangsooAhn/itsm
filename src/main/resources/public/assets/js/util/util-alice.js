b17215f3d1b5060468b5cea6e487d82807969d8e
lizeelf <lizeelf@brianz.co.kr>
2020-08-18 15:55:14 +0900

    메뉴 슬라이드 애니메이션 추가

--- src/main/resources/public/assets/js/util/util-alice.js
+++ src/main/resources/public/assets/js/util/util-alice.js
@@ -754,3 +754,86 @@ aliceJs.isHexCode = function(value) {
 aliceJs.isRgba = function(value) {
     return rgbaReg.test(value);
 }
+
+/**
+ * Slide Toggle (메뉴 등).
+ *
+ * @param {HTMLElement} target 대상
+ * @param {Number} duration 기간 (기본 500ms)
+ */
+aliceJs.slideToggle = (target, duration = 500) => {
+    if (window.getComputedStyle(target).display === 'none') {
+        return aliceJs.slideDown(target, duration);
+    } else {
+        return aliceJs.slideUp(target, duration);
+    }
+}
+
+/**
+ * Slide Up.
+ *
+ * @param {HTMLElement} target
+ * @param {Number} duration
+ */
+aliceJs.slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = '0';
+    target.style.paddingTop = '0';
+    target.style.paddingBottom = '0';
+    target.style.marginTop = '0';
+    target.style.marginBottom = '0';
+    window.setTimeout( () => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        //alert("!");
+    }, duration);
+}
+
+/**
+ * Slide Down.
+ *
+ * @param {HTMLElement} target
+ * @param {Number} duration
+ */
+aliceJs.slideDown = (target, duration = 500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+
+    if (display === 'none') display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = '0';
+    target.style.paddingTop = '0';
+    target.style.paddingBottom = '0';
+    target.style.marginTop = '0';
+    target.style.marginBottom = '0';
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout( () => {
+        target.style.removeProperty('height');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+    }, duration);
+}
