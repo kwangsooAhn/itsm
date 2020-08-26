129c8ccc0c0c8a6b242b8ee3f3a6aef8ff6b9f68
lizeelf <lizeelf@brianz.co.kr>
2020-08-26 17:42:42 +0900

    #8873 스크롤페이징 - [사용자]

--- src/main/kotlin/co/brainz/itsm/user/repository/UserRepositoryCustom.kt
+++ src/main/kotlin/co/brainz/itsm/user/repository/UserRepositoryCustom.kt
@@ -1,7 +1,8 @@
 package co.brainz.itsm.user.repository
 
 import co.brainz.framework.auth.entity.AliceUserEntity
+import com.querydsl.core.QueryResults
 
 interface UserRepositoryCustom {
-    fun findAliceUserEntityList(search: String, category: String): List<AliceUserEntity>
+    fun findAliceUserEntityList(search: String, category: String, offset: Long): QueryResults<AliceUserEntity>
 }
--- src/main/kotlin/co/brainz/itsm/user/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/itsm/user/repository/UserRepositoryImpl.kt
@@ -2,12 +2,18 @@ package co.brainz.itsm.user.repository
 
 import co.brainz.framework.auth.entity.AliceUserEntity
 import co.brainz.framework.auth.entity.QAliceUserEntity
+import co.brainz.itsm.constants.ItsmConstants
+import com.querydsl.core.QueryResults
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 class UserRepositoryImpl : QuerydslRepositorySupport(AliceUserEntity::class.java),
     UserRepositoryCustom {
 
-    override fun findAliceUserEntityList(search: String, category: String): List<AliceUserEntity> {
+    override fun findAliceUserEntityList(
+        search: String,
+        category: String,
+        offset: Long
+    ): QueryResults<AliceUserEntity> {
         val user = QAliceUserEntity.aliceUserEntity
         val query = from(user)
             .where(user.userName.containsIgnoreCase(search).or(user.userId.containsIgnoreCase(search)))
@@ -15,7 +21,9 @@ class UserRepositoryImpl : QuerydslRepositorySupport(AliceUserEntity::class.java
             query.where(user.platform.eq(category))
         }
         query.orderBy(user.userName.asc())
+            .limit(ItsmConstants.SEARCH_DATA_COUNT)
+            .offset(offset)
 
-        return query.fetch()
+        return query.fetchResults()
     }
 }
