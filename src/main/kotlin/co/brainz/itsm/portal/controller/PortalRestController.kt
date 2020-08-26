3ef3e9b9a722ca48e4ec8433cd74a3c838946c3c
ksm00 <ksm00@brainz.co.kr>
2020-08-26 13:50:23 +0900

    #8779 디자인 적용 - 페이징 제거
    - 포탈

--- src/main/kotlin/co/brainz/itsm/portal/controller/PortalRestController.kt
+++ src/main/kotlin/co/brainz/itsm/portal/controller/PortalRestController.kt
@@ -1,5 +1,7 @@
 package co.brainz.itsm.portal.controller
 
+import co.brainz.itsm.portal.dto.PortalDto
+import co.brainz.itsm.portal.dto.PortalSearchDto
 import co.brainz.itsm.portal.service.PortalService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController
 class PortalRestController(
     private val portalService: PortalService
 ) {
+
+    /**
+     * 포탈 검색 리스트 호출 처리
+     */
+    @GetMapping("")
+    fun getPortalList(portalSearchDto: PortalSearchDto): MutableList<PortalDto> {
+        return portalService.findPortalListOrSearchList(portalSearchDto)
+    }
 
     @GetMapping("/top")
     fun getTopList(@RequestParam(value = "limit") limit: Long): LinkedHashMap<String, Any> {
