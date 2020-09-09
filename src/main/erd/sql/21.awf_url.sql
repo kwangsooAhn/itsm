39344202ca267ed0fe6f80099554aaee186b4a41
hcjung <hcjung@brainz.co.kr>
2020-09-10 08:45:59 +0900

    DB 구축용 sql - 포탈화면 URL 누락된거 추가.

--- src/main/erd/sql/21.awf_url.sql
+++ src/main/erd/sql/21.awf_url.sql
@@ -218,3 +218,4 @@ insert into awf_url values ('/users/search', 'get', '사용자 검색, 목록 �
 insert into awf_url values ('/users/{userkey}/edit', 'get', '사용자 정보 수정 화면', 'TRUE');
 insert into awf_url values ('/users/{userkey}/editSelf', 'get', '사용자 자기 정보 수정 화면', 'FALSE');
 insert into awf_url values ('/users/department/view-pop', 'get', '부서관리팝업화면', 'TRUE');
+insert into awf_url values ('/rest/portal/top', 'get', '포탈 데이터 리스트 조회', 'FALSE');
