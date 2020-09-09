c5f7828ea68bbb543c7f12fc1f3cf7cf9d8eaa50
hcjung <hcjung@brainz.co.kr>
2020-09-09 14:09:26 +0900

    DB 구축용 sql - 사용자화면 부서팝업 접근 권한 추가

--- src/main/erd/sql/21.awf_url.sql
+++ src/main/erd/sql/21.awf_url.sql
@@ -217,3 +217,4 @@ insert into awf_url values ('/users/new', 'get', '사용자 등록 화면', 'TRU
 insert into awf_url values ('/users/search', 'get', '사용자 검색, 목록 등 메인이 되는 조회 화면', 'TRUE');
 insert into awf_url values ('/users/{userkey}/edit', 'get', '사용자 정보 수정 화면', 'TRUE');
 insert into awf_url values ('/users/{userkey}/editSelf', 'get', '사용자 자기 정보 수정 화면', 'FALSE');
+insert into awf_url values ('/users/department/view-pop', 'get', '부서관리팝업화면', 'TRUE');
