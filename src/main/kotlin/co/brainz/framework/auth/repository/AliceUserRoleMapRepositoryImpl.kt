bc083c30455f00c9eddcb572ee7ecdae68699b1e
lizeelf <lizeelf@brianz.co.kr>
2020-11-02 16:20:38 +0900

    #9426 [리팩토링] 로그인시 데이터 조회 1. 권한수만큼 반복 쿼리 실행 수정

--- src/main/kotlin/co/brainz/framework/auth/repository/AliceUserRoleMapRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/framework/auth/repository/AliceUserRoleMapRepositoryImpl.kt
@@ -1,20 +1,45 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
 package co.brainz.framework.auth.repository
 
-import co.brainz.framework.auth.entity.AliceRoleEntity
 import co.brainz.framework.auth.entity.AliceUserRoleMapEntity
 import co.brainz.framework.auth.entity.QAliceRoleEntity
+import co.brainz.framework.auth.entity.QAliceUserEntity
 import co.brainz.framework.auth.entity.QAliceUserRoleMapEntity
+import co.brainz.itsm.role.dto.RoleListDto
+import com.querydsl.core.types.Projections
+import com.querydsl.jpa.JPAExpressions
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 class AliceUserRoleMapRepositoryImpl : QuerydslRepositorySupport(AliceUserRoleMapEntity::class.java),
     AliceUserRoleMapRepositoryCustom {
-    override fun findUserRoleByUserKey(userKey: String): MutableList<AliceRoleEntity> {
-        val roleMap = QAliceUserRoleMapEntity.aliceUserRoleMapEntity
+
+    override fun findUserRoleByUserKey(userKey: String): MutableList<RoleListDto> {
         val role = QAliceRoleEntity.aliceRoleEntity
+        val roleMap = QAliceUserRoleMapEntity.aliceUserRoleMapEntity
+        val user = QAliceUserEntity.aliceUserEntity
         return from(role)
-            .innerJoin(roleMap).on(role.eq(roleMap.role))
-            .fetchJoin()
-            .where(roleMap.user.userKey.eq(userKey))
+            .select(
+                Projections.constructor(
+                    RoleListDto::class.java,
+                    role.roleId,
+                    role.roleName,
+                    role.roleDesc
+                )
+            )
+            .innerJoin(roleMap).on(
+                role.eq(roleMap.role).and(
+                    roleMap.user.eq(
+                        JPAExpressions.select(user)
+                            .from(user)
+                            .where(user.userKey.eq(userKey))
+                    )
+                )
+            )
+            .orderBy(role.roleId.asc())
             .fetch()
     }
 
