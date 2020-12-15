70843eebb9f40851e6c8b451061d362388268d20
hg.jung <hg.jung@brainz.co.kr>
2020-12-16 06:44:40 +0900

    ktlint 오류 수정

--- src/main/kotlin/co/brainz/itsm/numberingPattern/dto/NumberingPatternListDto.kt
+++ src/main/kotlin/co/brainz/itsm/numberingPattern/dto/NumberingPatternListDto.kt
@@ -2,7 +2,7 @@ package co.brainz.itsm.numberingPattern.dto
 
 import java.io.Serializable
 
-data class NumberingPatternListDto (
+data class NumberingPatternListDto(
     var patternId: String,
     var patternName: String,
     var patternType: String,
