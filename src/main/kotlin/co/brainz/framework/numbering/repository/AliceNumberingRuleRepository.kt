3df64fb63f82a955817c91438f6c46bd0a44ee44
hg.jung <hg.jung@brainz.co.kr>
2021-01-05 03:21:06 +0900

    framework numbering package delete

--- src/main/kotlin/co/brainz/framework/numbering/repository/AliceNumberingRuleRepository.kt
+++
@@ -1,8 +0,0 @@
-package co.brainz.framework.numbering.repository
-
-import co.brainz.framework.numbering.entity.AliceNumberingRuleEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface AliceNumberingRuleRepository : JpaRepository<AliceNumberingRuleEntity, String>
