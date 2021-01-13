cd91c8688227bf4660c0a20a8ac49229fb44e884
jylim <jy.lim@brainz.co.kr>
2021-01-13 17:07:46 +0900

    #9885 CI Type 화면에 Type 트리 출력
    - DB 스크립트 업데이트

--- src/main/kotlin/co/brainz/cmdb/type/entity/CmdbTypeEntity.kt
+++ src/main/kotlin/co/brainz/cmdb/type/entity/CmdbTypeEntity.kt
@@ -12,7 +12,14 @@ import org.hibernate.annotations.GenericGenerator
 import org.hibernate.annotations.NotFound
 import org.hibernate.annotations.NotFoundAction
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
 
 @Entity
 @Table(name = "cmdb_type")
