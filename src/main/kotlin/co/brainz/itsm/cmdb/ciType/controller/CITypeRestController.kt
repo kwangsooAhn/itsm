174ceca18448010d716d802c2f56b469cb971a96
jylim <jy.lim@brainz.co.kr>
2021-01-18 12:26:10 +0900

    #9884 CI Type 상세정보 등록, 수정 및 삭제 구현
    - MR feedback

--- src/main/kotlin/co/brainz/itsm/cmdb/ciType/controller/CITypeRestController.kt
+++ src/main/kotlin/co/brainz/itsm/cmdb/ciType/controller/CITypeRestController.kt
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/rest/cmdb/types")
 class CITypeRestController(private val ciTypeService: CITypeService) {
