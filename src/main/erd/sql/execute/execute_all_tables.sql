7d2f0b0b15ab9cb6b8f27d5922f9d34952e101bb
woodajung <wdj@brainz.co.kr>
2021-01-22 17:17:57 +0900

    #9775 CI 컴포넌트 - 화면

--- src/main/erd/sql/execute/execute_all_tables.sql
+++ src/main/erd/sql/execute/execute_all_tables.sql
@@ -2976,17 +2976,17 @@ COMMENT ON COLUMN awf_label.update_dt IS '수정일시';
 /**
  * CI 컴포넌트 - CI 세부 속성 임시 테이블
  */
-DROP TABLE IF EXISTS wf_ci_component_temporary_data;
+DROP TABLE IF EXISTS wf_component_ci_data;
 
-CREATE TABLE wf_ci_component_temporary_data
+CREATE TABLE wf_component_ci_data
 (
     component_id varchar(128) NOT NULL,
     ci_id character varying(128) NOT NULL,
     values text,
-    CONSTRAINT wf_ci_component_temporary_data_pk PRIMARY KEY (component_id, ci_id)
+    CONSTRAINT wf_component_ci_data_pk PRIMARY KEY (component_id, ci_id)
 );
 
-COMMENT ON TABLE wf_ci_component_temporary_data IS 'ci 컴포넌트 임시 데이터';
-COMMENT ON COLUMN wf_ci_component_temporary_data.component_id IS '컴포넌트아이디';
-COMMENT ON COLUMN wf_ci_component_temporary_data.ci_id IS 'CI아이디';
-COMMENT ON COLUMN wf_ci_component_temporary_data.values IS '세부속성 데이터';
+COMMENT ON TABLE wf_component_ci_data IS 'CI 컴포넌트 CI 상세 데이터';
+COMMENT ON COLUMN wf_component_ci_data.component_id IS '컴포넌트아이디';
+COMMENT ON COLUMN wf_component_ci_data.ci_id IS 'CI아이디';
+COMMENT ON COLUMN wf_component_ci_data.values IS '세부속성 데이터';
