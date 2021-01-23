bc4df21c7a9768e4de2639abdf88d62c2a083235
hcjung <hcjung@brainz.co.kr>
2021-01-23 15:34:52 +0900

    테스트코드에 "jenkinsTest" 프로필 추가

--- src/test/kotlin/co/brainz/framework/label/AliceLabelControllerTest.kt
+++ src/test/kotlin/co/brainz/framework/label/AliceLabelControllerTest.kt
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
@@ -54,6 +55,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ActiveProfiles("jenkinsTest")
 class AliceLabelControllerTest {
     @Autowired
     private lateinit var mvc: MockMvc
