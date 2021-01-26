ea4f92e9e639db1fd80655bc8aa58f2b41c3da0d
hg.jung <hg.jung@brainz.co.kr>
2021-01-26 17:17:04 +0900

    #9883 MR 응대

--- src/main/kotlin/co/brainz/itsm/cmdb/ci/controller/CIController.kt
+++ src/main/kotlin/co/brainz/itsm/cmdb/ci/controller/CIController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/cmdb/ci")
+@RequestMapping("/cmdb/cis")
 class CIController(private val ciService: CIService) {
 
     private val ciSearchPage: String = "cmdb/ci/ciSearch"
