d141da20c394b921bac8776252ceb9ffe4dd504c
hg.jung <hg.jung@brainz.co.kr>
2021-01-29 04:08:30 +0900

    #10055 통계 차트 메뉴 조회 구현

--- src/main/erd/sql/execute/execute_all_tables.sql
+++ src/main/erd/sql/execute/execute_all_tables.sql
@@ -3037,3 +3037,30 @@ COMMENT ON COLUMN wf_component_ci_data.component_id IS '컴포넌트아이디';
 COMMENT ON COLUMN wf_component_ci_data.ci_id IS 'CI아이디';
 COMMENT ON COLUMN wf_component_ci_data.values IS '세부속성 데이터';
 COMMENT ON COLUMN wf_component_ci_data.instance_id IS '인스턴스아이디';
+
+/**
+ * 차트설정
+ */
+DROP TABLE IF EXISTS awf_chart cascade;
+
+CREATE TABLE awf_chart
+(
+    chart_id character varying(128) NOT NULL,
+    chart_type character varying(128) NOT NULL,
+    chart_name character varying(256) NOT NULL,
+    chart_desc text NOT NULL,
+    create_user_key character varying(128),
+    create_dt timestamp without time zone,
+    update_user_key character varying(128),
+    update_dt timestamp without time zone
+);
+
+COMMENT ON TABLE awf_chart IS '차트설정';
+COMMENT ON COLUMN awf_chart.chart_id IS  '차트아이디';
+COMMENT ON COLUMN awf_chart.chart_type IS '차트타입';
+COMMENT ON COLUMN awf_chart.chart_name IS '차트이름';
+COMMENT ON COLUMN awf_chart.chart_desc IS '차트설명';
+COMMENT ON COLUMN awf_chart.create_user_key IS '등록자';
+COMMENT ON COLUMN awf_chart.create_dt IS '등록일시';
+COMMENT ON COLUMN awf_chart.update_user_key IS '수정자';
+COMMENT ON COLUMN awf_chart.update_dt IS '수정일시';
