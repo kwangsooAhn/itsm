5311bb2c692e86c62dfa4f632208a5c57cf09758
hg.jung <hg.jung@brainz.co.kr>
2021-01-29 15:10:58 +0900

    #10083 chart crud 구현

--- src/main/erd/sql/execute/execute_all_tables.sql
+++ src/main/erd/sql/execute/execute_all_tables.sql
@@ -3500,7 +3500,8 @@ CREATE TABLE awf_chart
     chart_id character varying(128) NOT NULL,
     chart_type character varying(128) NOT NULL,
     chart_name character varying(256) NOT NULL,
-    chart_desc text NOT NULL,
+    chart_desc text,
+    chart_config text NOT NULL,
     create_user_key character varying(128),
     create_dt timestamp,
     update_user_key character varying(128),
@@ -3512,6 +3513,7 @@ COMMENT ON COLUMN awf_chart.chart_id IS  '차트아이디';
 COMMENT ON COLUMN awf_chart.chart_type IS '차트타입';
 COMMENT ON COLUMN awf_chart.chart_name IS '차트이름';
 COMMENT ON COLUMN awf_chart.chart_desc IS '차트설명';
+COMMENT ON COLUMN awf_chart.chart_config IS '차트설정';
 COMMENT ON COLUMN awf_chart.create_user_key IS '등록자';
 COMMENT ON COLUMN awf_chart.create_dt IS '등록일시';
 COMMENT ON COLUMN awf_chart.update_user_key IS '수정자';
