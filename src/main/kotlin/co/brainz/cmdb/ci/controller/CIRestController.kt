40858cb9f25b515f0fd63948a42132636508f086
woodajung <wdj@brainz.co.kr>
2021-02-02 17:05:05 +0900

    #10077 CI 컴포넌트 - 서버 : 리뷰 후 수정 사항 반영

--- src/main/kotlin/co/brainz/cmdb/ci/controller/CIRestController.kt
+++ src/main/kotlin/co/brainz/cmdb/ci/controller/CIRestController.kt
@@ -12,7 +12,16 @@ import co.brainz.cmdb.provider.dto.CIDto
 import co.brainz.cmdb.provider.dto.CIListDto
 import co.brainz.cmdb.provider.dto.RestTemplateReturnDto
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/rest/cmdb/eg/cis")
