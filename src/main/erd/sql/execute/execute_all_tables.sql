114e09421ae9738205c69e3a7422f90718eaa84f
jylim <jy.lim@brainz.co.kr>
2021-02-03 13:09:22 +0900

    CMDB Type 관련 sql 추가

--- src/main/erd/sql/execute/execute_all_tables.sql
+++ src/main/erd/sql/execute/execute_all_tables.sql
@@ -1707,10 +1707,17 @@ insert into awf_url_auth_map values ('/rest/cmdb/classes/{id}', 'get', 'cmdb.cla
 insert into awf_url_auth_map values ('/rest/cmdb/classes/{id}', 'put', 'cmdb.class.update');
 insert into awf_url_auth_map values ('/rest/cmdb/classes/{id}', 'delete', 'cmdb.class.delete');
 insert into awf_url_auth_map values ('/rest/cmdb/types', 'get', 'cmdb.type.read');
+insert into awf_url_auth_map values ('/rest/cmdb/types', 'get', 'cmdb.type.create');
+insert into awf_url_auth_map values ('/rest/cmdb/types', 'get', 'cmdb.type.update');
+insert into awf_url_auth_map values ('/rest/cmdb/types', 'get', 'cmdb.type.delete');
+insert into awf_url_auth_map values ('/rest/cmdb/types', 'post', 'cmdb.type.create');
 insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'get', 'cmdb.type.read');
 insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'get', 'cmdb.type.create');
 insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'get', 'cmdb.type.update');
 insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'get', 'cmdb.type.delete');
+insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'post', 'cmdb.type.create');
+insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'put', 'cmdb.type.update');
+insert into awf_url_auth_map values ('/rest/cmdb/types/{id}', 'delete', 'cmdb.type.delete');
 insert into awf_url_auth_map values ('/rest/codes', 'get', 'code.read');
 insert into awf_url_auth_map values ('/rest/codes', 'post', 'code.create');
 insert into awf_url_auth_map values ('/rest/codes', 'get', 'code.delete');
