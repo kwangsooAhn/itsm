733834849380fb996ed9f7f840d1f2384ef32a04
LIM JIYOUNG <jy.lim@brainz.co.kr>
2021-03-04 09:47:35 +0900

    #10024 업무흐름에서 신청서 아이콘이 뜨지 않는다.
    - TEST

--- src/main/kotlin/co/brainz/framework/fileTransaction/service/AliceFileService.kt
+++ src/main/kotlin/co/brainz/framework/fileTransaction/service/AliceFileService.kt
@@ -39,7 +39,6 @@ import org.apache.tika.metadata.Metadata
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
-import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.InputStreamResource
 import org.springframework.data.repository.findByIdOrNull
@@ -260,34 +259,27 @@ class AliceFileService(
      * 워크플로우 이미지 파일 로드.
      */
     fun getImageFileList(type: String, searchValue: String): List<AliceImageFileDto> {
-        logger.info("dir test1 = {}", ClassPathResource(docIconRootDirectory))
-        logger.info("dir test2 = {}", ClassPathResource(docIconRootDirectory).uri)
-        logger.info("dir test3 = {}", Paths.get(ClassPathResource(docIconRootDirectory).uri))
-
-        logger.info("dir test4 = {}", javaClass.classLoader.getResource(this.documentIconRootDirectory))
-        logger.info("dir test5 = {}", javaClass.classLoader.getResource(this.documentIconRootDirectory).toURI())
-        logger.info("dir test6 = {}", Paths.get(javaClass.classLoader.getResource(this.documentIconRootDirectory).toURI()))
-        // 내부 경로 이미지 ( icon )은 jar에서도 경로를 읽어올 수 있게 처리해야한다.
+        // todo: 내부 경로 이미지 icon을 jar에서도 경로를 읽어올 수 있게 처리
         val dir = when (type) {
             AliceConstants.FileType.ICON.code -> Paths.get(javaClass.classLoader.getResource(this.documentIconRootDirectory).toURI())
             AliceConstants.FileType.ICON_TYPE.code -> Paths.get(javaClass.classLoader.getResource(this.typeIconRootDirectory).toURI())
             else -> super.getWorkflowDir(this.imagesRootDirectory)
         }
 
+        logger.info("dir test1 = {}", javaClass.classLoader.getResource(this.documentIconRootDirectory))
+        logger.info("dir test2 = {}", javaClass.classLoader.getResource(this.documentIconRootDirectory).toURI())
+        logger.info("dir test3 = {}", Paths.get(javaClass.classLoader.getResource(this.documentIconRootDirectory).toURI()))
         logger.info("DIR = {}", dir)
         logger.info(">>>> Available DIR? = {}", Files.isDirectory(dir))
 
 //        logger.debug(">>>> WORKFLOW IMAGE URI = {}", Paths.get(ClassPathResource(this.documentIconRootDirectory).uri))
 
-        // 경로에 존재하는 이미지 목록을 구성
         val fileList = mutableListOf<Path>()
         if (Files.isDirectory(dir)) {
             val fileDirMap = Files.list(dir).collect(Collectors.partitioningBy { Files.isDirectory(it) })
             fileDirMap[false]?.forEach { filePath ->
                 val file = filePath.toFile()
-                // 허용 확장자인지 체크
                 if (allowedImageExtensions.indexOf(file.extension.toLowerCase()) > -1) {
-                    // 검색결과에 해당하는 파일 정보를 추가
                     when (searchValue) {
                         "" -> fileList.add(filePath)
                         else -> {
