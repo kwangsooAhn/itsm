1cf8341083353c6321cb5d4761ef1adeac9f0a1d
lizeelf <lizeelf@brianz.co.kr>
2021-03-09 14:28:26 +0900

    1. 변수명 대문자 시작 수정 2. JUnit5 테스트 코드 작성

---
+++ src/test/kotlin/co/brainz/cmdb/CIServiceTest.kt
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ *
+ */
+
+package co.brainz.cmdb
+
+import co.brainz.cmdb.ci.service.CIService
+import co.brainz.cmdb.provider.constants.RestTemplateConstants
+import co.brainz.cmdb.provider.dto.CIDto
+import co.brainz.framework.util.AliceUtil
+import javax.transaction.Transactional
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assumptions.assumingThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@DisplayName("CI 호출 테스트")
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@Transactional
+class CIServiceTest {
+
+    @Autowired
+    private lateinit var ciService: CIService
+
+    lateinit var ciName: String
+    lateinit var typeId: String
+
+    @BeforeEach
+    fun init() {
+        this.ciName = "Test CI 1"
+        this.typeId = "f18c064040304e493f4dc7385595601f" // 네트워크
+    }
+
+    @Test
+    @DisplayName("CI 전체 갯수 조회")
+    @Order(1)
+    fun getAllCICountCheck() {
+        val params = LinkedHashMap<String, Any>()
+        val ciDtoList = ciService.getCIs(params)
+        assumingThat(
+            ciDtoList.isNotEmpty()
+        ) {
+            assertTrue(ciDtoList[0].totalCount > 0)
+        }
+    }
+
+    @Test
+    @DisplayName("CI 생성")
+    @Order(2)
+    fun createCI() {
+        val ciDto = CIDto(
+            ciId = AliceUtil().getUUID(),
+            ciNo = "",
+            ciName = this.ciName,
+            classId = "",
+            automatic = false,
+            ciStatus = RestTemplateConstants.CIStatus.STATUS_USE.code,
+            ciDesc = "",
+            typeId = this.typeId,
+            ciIcon = "",
+            ciDataList = mutableListOf(),
+            ciRelations = mutableListOf(),
+            ciTags = mutableListOf()
+        )
+        val returnDto = ciService.createCI(ciDto)
+        assertEquals(returnDto.code, "0")
+        assertTrue(returnDto.status)
+    }
+
+    @Test
+    @DisplayName("CI Name 검색어 조회")
+    @Order(3)
+    fun getCISearch() {
+        val searchValue = this.ciName
+        val params = LinkedHashMap<String, Any>()
+        params["search"] = searchValue
+        val ciDtoList = ciService.getCIs(params)
+        assumingThat(
+            ciDtoList.isNotEmpty()
+        ) {
+            assertEquals(ciDtoList[0].ciName, searchValue)
+        }
+    }
+
+    @Test
+    @DisplayName("CI 단일 조회")
+    @Order(4)
+    fun getCI() {
+        var ciId = ""
+        val params = LinkedHashMap<String, Any>()
+        params["search"] = this.ciName
+        val ciDtoList = ciService.getCIs(params)
+        if (!ciDtoList.isNullOrEmpty()) {
+            ciId = ciDtoList[0].ciId.toString()
+        }
+        assumingThat(
+            ciId.isNotEmpty()
+        ) {
+            val ciDto = ciService.getCI(ciId)
+            assertEquals(ciDto.ciNo, ciDtoList[0].ciNo)
+            assertEquals(ciDto.ciName, ciDtoList[0].ciName)
+            assertEquals(ciDto.ciDesc, ciDtoList[0].ciDesc)
+            assertEquals(ciDto.typeId, ciDtoList[0].typeId)
+            assertEquals(ciDto.ciStatus, ciDtoList[0].ciStatus)
+        }
+    }
+
+    @Test
+    @DisplayName("CI 수정")
+    @Order(5)
+    fun updateCI() {
+        val params = LinkedHashMap<String, Any>()
+        params["search"] = this.ciName
+        val ciDtoList = ciService.getCIs(params)
+        assumingThat(
+            ciDtoList.isNotEmpty()
+        ) {
+            for (ciDto in ciDtoList) {
+                if (ciDto.ciId != null) {
+                    val updateCiDto = CIDto(
+                        ciId = ciDto.ciId.toString(),
+                        ciNo = ciDto.ciNo,
+                        ciName = ciDto.ciName,
+                        ciDesc = "Update Test 1",
+                        typeId = ciDto.typeId.toString(),
+                        classId = ciDto.classId,
+                        ciTags = ciDto.tags,
+                        ciIcon = ciDto.ciIcon
+                    )
+                    assertTrue(ciService.updateCI(ciDto.ciId.toString(), updateCiDto).status)
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("CI 삭제")
+    @Order(6)
+    fun deleteCI() {
+        val params = LinkedHashMap<String, Any>()
+        params["search"] = this.ciName
+        val ciDtoList = ciService.getCIs(params)
+        assumingThat(
+            ciDtoList.isNotEmpty()
+        ) {
+            for (ciDto in ciDtoList) {
+                assertTrue(ciService.deleteCI(ciDto.ciId.toString()).status)
+            }
+        }
+    }
+}
