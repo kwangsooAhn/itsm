d15b9789dd139c3049d17d52de8860c278e5e308
lizeelf <lizeelf@brianz.co.kr>
2021-03-09 15:42:12 +0900

    1. 기존 CMDB URL 선언부 삭제

--- src/main/kotlin/co/brainz/cmdb/provider/constants/RestTemplateConstants.kt
+++ src/main/kotlin/co/brainz/cmdb/provider/constants/RestTemplateConstants.kt
@@ -24,64 +24,6 @@ object RestTemplateConstants {
     }
 
     /**
-     * Attribute Url.
-     *
-     * @param url
-     * @param method
-     */
-    enum class Attribute(val url: String, val method: String) {
-        GET_ATTRIBUTES("/rest/cmdb/eg/attributes", "GET"),
-        GET_ATTRIBUTE("/rest/cmdb/eg/attributes/{attributeId}", "GET"),
-        POST_ATTRIBUTE("/rest/cmdb/eg/attributes", "POST"),
-        PUT_ATTRIBUTE("/rest/cmdb/eg/attributes/{attributeId}", "PUT"),
-        DELETE_ATTRIBUTE("/rest/cmdb/eg/attributes/{attributeId}", "DELETE")
-    }
-
-    /**
-     * Class Url.
-     *
-     * @Param url
-     * @Param method
-     */
-    enum class Class(val url: String, val method: String) {
-        GET_CLASSES("/rest/cmdb/eg/classes", "GET"),
-        GET_CLASS("/rest/cmdb/eg/classes/{classId}", "GET"),
-        POST_CLASS("/rest/cmdb/eg/classes", "POST"),
-        PUT_CLASS("/rest/cmdb/eg/classes/{classId}", "PUT"),
-        DELETE_CLASS("/rest/cmdb/eg/classes/{classId}", "DELETE"),
-        GET_CLASS_ATTRIBUTE("/rest/cmdb/eg/classes/{classId}/attributes", "GET")
-    }
-
-    /**
-     * Type Url.
-     *
-     * @param url
-     * @param method
-     */
-    enum class Type(val url: String, val method: String) {
-        GET_TYPES("/rest/cmdb/eg/types", "GET"),
-        GET_TYPE("/rest/cmdb/eg/types/{typeId}", "GET"),
-        POST_TYPE("/rest/cmdb/eg/types", "POST"),
-        PUT_TYPE("/rest/cmdb/eg/types/{typeId}", "PUT"),
-        DELETE_TYPE("/rest/cmdb/eg/types/{typeId}", "DELETE")
-    }
-
-    /**
-     * Ci Url.
-     *
-     * @param url
-     * @param method
-     */
-    enum class CI(val url: String, val method: String) {
-        GET_CIS("/rest/cmdb/eg/cis", "GET"),
-        GET_CI("/rest/cmdb/eg/cis/{ciId}", "GET"),
-        POST_CI("/rest/cmdb/eg/cis", "POST"),
-        PUT_CI("/rest/cmdb/eg/cis/{ciId}", "PUT"),
-        PUT_CI_COMPONENT("/rest/cmdb/eg/cis/{ciId}", "PUT"),
-        DELETE_CI("/rest/cmdb/eg/cis/{ciId}", "DELETE")
-    }
-
-    /**
      * CI Status.
      */
     enum class CIStatus(val code: String) {
