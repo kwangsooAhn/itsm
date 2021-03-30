2fc8df64c2e9eea4b9733339a331e4f09a187250
hg.jung <hg.jung@brainz.co.kr>
2021-03-31 07:26:36 +0900

    #10230 CI 컴포넌트 구분 값 오류

--- src/main/resources/public/assets/js/form/form.component.ci.js
+++ src/main/resources/public/assets/js/form/form.component.ci.js
@@ -118,7 +118,7 @@
      * @param {Object} comp 컴포넌트
      * @param {Function} callbackFunc callback 함수
      */
-    function saveCIComponentData(actionType, comp, modal, callbackFunc) {
+    function saveCIComponentData(actionType, realActionType, comp, modal, callbackFunc) {
         if (isValidRequiredAll(modal) && hasErrorClass()) {
             const saveCIData = {};
             Object.keys(CIData).forEach(function(key) {
@@ -131,7 +131,7 @@
                         elemKey = 'typeIconData';
                         break;
                     default:
-                        elemKey = keyl;
+                        elemKey = key;
                 }
                 const elem = document.getElementById(elemKey);
                 if (elem !== null) {
@@ -145,13 +145,13 @@
             if (actionType === ACTION_TYPE_REGISTER) { // 신규 추가
                 saveCIData.ciId = workflowUtil.generateUUID();
                 saveCIData.actionType = ACTION_TYPE_REGISTER;
-                addRow(comp, saveCIData);
+                addRow(comp, saveCIData, undefined, actionType);
                 componentData.value.push(saveCIData);
             } else { // 수정
                 saveCIData.actionType = ACTION_TYPE_MODIFY;
                 const ciId = document.getElementById('ciId').value;
                 const ciIdx = componentData.value.findIndex(function (ci) { return ci.ciId === ciId; });
-                addRow(comp, saveCIData, ciIdx);
+                addRow(comp, saveCIData, ciIdx, realActionType);
                 componentData.value[ciIdx] = saveCIData;
             }
 
@@ -240,7 +240,7 @@
                     bindKey: false,
                     callback: function (modal) {
                         // 세부 속성 저장
-                        saveCIComponentData(ACTION_TYPE_REGISTER, ciComponent, ciRegisterModal, function() {
+                        saveCIComponentData(ACTION_TYPE_REGISTER, undefined, ciComponent, ciRegisterModal, function() {
                             modal.hide();
                         });
                     }
@@ -286,7 +286,7 @@
     /**
      * 기존 CI 변경 모달
      */
-    function openUpdateModal(componentId, ciId, elem) {
+    function openUpdateModal(componentId, ciId, elem, realActionType) {
         const compIdx = aliceDocument.getComponentIndex(componentId);
         const componentData = aliceDocument.data.form.components[compIdx];
         const ciIdx = componentData.value.findIndex(function (ci) { return ci.ciId === ciId; });
@@ -307,7 +307,7 @@
                     bindKey: false,
                     callback: function (modal) {
                         // 세부 속성 저장
-                        saveCIComponentData(ACTION_TYPE_MODIFY, document.getElementById(componentId), ciUpdateModal, function() {
+                        saveCIComponentData(ACTION_TYPE_MODIFY, realActionType, document.getElementById(componentId), ciUpdateModal, function() {
                             modal.hide();
                         });
                     }
@@ -446,7 +446,7 @@
                                     ci[cell.id] = (cell.id === 'ciId') ? chkElem.value : cell.textContent;
                                 }
                             });
-                            addRow(ciComponent, ci);
+                            addRow(ciComponent, ci, undefined, actionType);
                             componentData.value.push(ci);
                         }
                     });
@@ -504,7 +504,7 @@
      * @param {Object} data 데이터
      * @param {Number} idx 인덱스
      */
-    function addRow(comp, data, idx) {
+    function addRow(comp, data, idx, realActionType) {
         const ciTb = comp.querySelector('.ci-table-body');
         const row = document.createElement('tr');
         const rowBorderColor = ciTb.getAttribute('data-border');
@@ -527,7 +527,7 @@
                     tdTemplate += (typeof data[opt.id] !== 'undefined') ? `${aliceJs.filterXSS(data[opt.id])}` : '';
                     break;
                 case 'readonly':
-                    tdTemplate += `${i18n.msg('cmdb.ci.actionType.' + data.actionType)}`;
+                    tdTemplate += `${i18n.msg('cmdb.ci.actionType.' + realActionType)}`;
                     break;
                 case 'image':
                     tdTemplate += `<img src="${data[opt.id]}" width="20" height="20"/>`;
@@ -536,7 +536,7 @@
                     if (actionType === ACTION_TYPE_DELETE || comp.getAttribute('data-displaytype') === 'readonly') {
                         tdTemplate += `<button type="button" class="icon-search-area" onclick="javascript:CI.openViewModal('${comp.id}', '${data.ciId}', this);"><span class="icon icon-search"></span></button>`;
                     } else {
-                        tdTemplate += `<button type="button" onclick="javascript:CI.openUpdateModal('${comp.id}', '${data.ciId}', this);"><span class="icon icon-edit"></span></button>`;
+                        tdTemplate += `<button type="button" onclick="javascript:CI.openUpdateModal('${comp.id}', '${data.ciId}', this, '${realActionType}');"><span class="icon icon-edit"></span></button>`;
                     }
                     break;
                 case 'icon-search': // CI 상세 조회
