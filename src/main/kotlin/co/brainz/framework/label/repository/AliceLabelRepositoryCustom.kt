056b4b246a26766fac9febe47257630aebe32afa
hg.jung <hg.jung@brainz.co.kr>
2021-04-07 00:49:23 +0900

    #10346 "동적차트"에서 대상 라벨리스트(태그)가 단순 스트링으로 처리되고 있음. -> 일감 분석 및 수정 진행

--- src/main/kotlin/co/brainz/framework/label/repository/AliceLabelRepositoryCustom.kt
+++ src/main/kotlin/co/brainz/framework/label/repository/AliceLabelRepositoryCustom.kt
@@ -5,5 +5,5 @@ import co.brainz.framework.querydsl.AliceRepositoryCustom
 
 interface AliceLabelRepositoryCustom : AliceRepositoryCustom {
     fun findLabels(labelTarget: String, targetId: String, labelKey: String?): MutableList<AliceLabelEntity>
-    fun findLabelByKey(labelKey: String): MutableList<AliceLabelEntity>
+    fun findLabelByKeyIn(labelKey:ArrayList<String>): MutableList<AliceLabelEntity>
 }
