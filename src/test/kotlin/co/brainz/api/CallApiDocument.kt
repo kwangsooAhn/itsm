ff5d4f3d9a4edcd9e6b0d87c324725ee06613761
lizeelf <lizeelf@brianz.co.kr>
2021-04-22 12:17:15 +0900

    1. 소스 정리

--- src/test/kotlin/co/brainz/api/CallApiDocument.kt
+++ src/test/kotlin/co/brainz/api/CallApiDocument.kt
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.type.TypeFactory
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
@@ -66,7 +65,6 @@ class CallApiDocument {
 
     @DisplayName("문서의 데이터 타입")
     @Order(1)
-    @Disabled
     @Test
     fun getDocumentDataStructure() {
         val headers = HttpHeaders()
