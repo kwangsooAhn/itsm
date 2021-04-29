64cef8efbf901de6000dd79868d19794b8f3b882
hcjung <hcjung@brainz.co.kr>
2021-04-29 18:46:32 +0900

    태그 관련 SQL 정리 및 ERD 정리

--- src/main/erd/sql/execute/execute_all_tables.sql
+++ src/main/erd/sql/execute/execute_all_tables.sql
@@ -9671,33 +9671,6 @@ insert into cmdb_class_attribute_map values ('39dbe77aa58b778064a0f4a10dd06b05',
 insert into cmdb_class_attribute_map values ('f88ee1c29fdf9d847ba6002abc5bbf1b', 'df0e88d216ace73e0164f3dbf7ade131', 1);
 
 /**
- * Label 마스터 클래스
- */
-DROP TABLE IF EXISTS awf_label cascade;
-
-CREATE TABLE awf_label
-(
-    label_target character varying(128),
-    label_target_id character varying(128),
-    label_key character varying(128),
-    label_value character varying(512),
-    create_user_key character varying(128),
-    create_dt timestamp,
-    update_user_key character varying(128),
-    update_dt timestamp,
-    CONSTRAINT awf_label_pk UNIQUE (label_target, label_target_id, label_key)
-);
-
-COMMENT ON TABLE awf_label IS '라벨마스터';
-COMMENT ON COLUMN awf_label.label_target IS '라벨링 대상 타입';
-COMMENT ON COLUMN awf_label.label_target_id IS '라벨링 대상';
-COMMENT ON COLUMN awf_label.label_key IS '라벨링 키값';
-COMMENT ON COLUMN awf_label.label_value IS '라벨링 value 값';
-COMMENT ON COLUMN awf_label.create_user_key IS '등록자';
-COMMENT ON COLUMN awf_label.create_dt IS '등록일시';
-COMMENT ON COLUMN awf_label.update_user_key IS '수정자';
-COMMENT ON COLUMN awf_label.update_dt IS '수정일시';
-/**
  * CI 컴포넌트 - CI 세부 속성 임시 테이블
  */
 DROP TABLE IF EXISTS wf_component_ci_data;
