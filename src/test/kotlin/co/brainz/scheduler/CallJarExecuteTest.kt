642663d033b2ecd3630772868a8c229780bfb6b7
ksm00 <ksm00@brainz.co.kr>
2021-05-27 09:30:08 +0900

    WfInstanceServiceTest 파일 삭제

--- src/test/kotlin/co/brainz/scheduler/CallJarExecuteTest.kt
+++ src/test/kotlin/co/brainz/scheduler/CallJarExecuteTest.kt
@@ -1,20 +1,19 @@
 /*
  * Copyright 2020 Brainzcompany Co., Ltd.
  * https://www.brainz.co.kr
- *
  */
 
 package co.brainz.scheduler
 
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
 
 @SpringBootTest
 @AutoConfigureMockMvc
