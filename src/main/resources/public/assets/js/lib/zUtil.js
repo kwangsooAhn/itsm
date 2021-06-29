79d6ac5a7fc5d6e9e99fa4770e4a923c171ca4fa
jy.lim <jy.lim@brainz.co.kr>
2021-06-29 13:08:36 +0900

    #10861 Input Box 디자인 적용 - Clear Text
    - MR feedback

--- src/main/resources/public/assets/js/lib/zUtil.js
+++ src/main/resources/public/assets/js/lib/zUtil.js
@@ -1200,7 +1200,7 @@ aliceJs.importMixin = function (target, source) {
  * @param rexg
  * @param flag
  */
-function removeChar(event, rexg = integerReg, flag = 'g') {
+aliceJs.removeChar = function (event, rexg = integerReg, flag = 'g') {
     event = event || window.event;
     let regToString = '[' + rexg.toString()
         .replaceAll('/', '')
@@ -1218,7 +1218,7 @@ function removeChar(event, rexg = integerReg, flag = 'g') {
  * input 엘리먼트 내부의 X 버튼을 클릭했을 때, 같은 레벨의 input value를 clear한다.
  * @param req
  */
-function clearText(req) {
+aliceJs.clearText = function (req) {
     let target = req.parentElement.getElementsByTagName('input');
     for (let i = 0; i < target.length; i++) {
         target[i].value = '';
