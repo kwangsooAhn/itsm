a7714c7741812554175372f7f713343a241918f0
hcjung <hcjung@brainz.co.kr>
2021-08-05 14:20:19 +0900

    #10923 페이징 처리 - 디자인 적용

--- src/main/resources/public/assets/js/lib/zPaging.js
+++ src/main/resources/public/assets/js/lib/zPaging.js
@@ -26,6 +26,9 @@ class ZPaging {
      * @return {boolean}
      */
     update() {
+        let selectedNumClass = 'active'
+        let pageNumsSelector = 'div.z-pagingNumbers a'
+        let selectedPage = 'selectedPage'
 
         // 1) 전체 검색건수, 저장된 데이터 전체 건수, 정렬 순서등을 출력
         document.querySelector('#spanTotalCount').innerText = i18n.msg('common.label.count', document.querySelector('#totalCount').value);
@@ -39,23 +42,28 @@ class ZPaging {
         // 2) 페이지 번호 목록 처리
         // 2-1) 현재 출력되어 있는 페이징 번호 목록
         let currentPageList = [];
-        document.querySelectorAll('div.PagingNumber span').forEach((span) => {
-            currentPageList.push(span.innerText);
+        document.querySelectorAll(pageNumsSelector).forEach((element) => {
+            currentPageList.push(element.innerText);
         })
 
         // 2-2) 현재 페이지 번호, 전체 페이지 수
-        let currentPageNum = document.querySelector('#currentPageNum').value;
-        let totalPageNum = document.querySelector('#totalPageNum').value;
+        let currentPageNum = Number(document.querySelector('#currentPageNum').value);
+        let totalPageNum = Number(document.querySelector('#totalPageNum').value);
         document.querySelector('#spanCurrentPageNum').innerText = i18n.msg('common.label.totalPageNum', totalPageNum);
 
-        // 2-3) 현재 페이지번호가 이미 있는 경우 -> class 적용만.
+        // 2-3) 현재 페이지번호가 이미 있는 경우 -> class 적용만하고 끝냄.
         if (currentPageList.includes(currentPageNum)) {
-            // class add
+            document.querySelectorAll(pageNumsSelector).forEach((span) => {
+                span.className = '';
+                if (span.innerText === currentPageNum) {
+                    span.classList.add(selectedPage);
+                }
+            })
             return true;
         }
 
         // 2-4) 현재 출력되어 있는 페이지 번호 목록 삭제.
-        document.querySelectorAll('div.PagingNumber span').forEach((span) => {
+        document.querySelectorAll(pageNumsSelector).forEach((span) => {
             span.remove();
         });
 
@@ -67,46 +75,47 @@ class ZPaging {
             let newAnchor = document.createElement('a');
             newAnchor.setAttribute('href', 'javascript:getList(' + i + ')')
             newAnchor.innerText = '' + i;
-            // span tag
-            let newSpan = document.createElement('span');
-            newSpan.appendChild(newAnchor);
-            document.querySelector('div.PagingNumber').appendChild(newSpan)
+            document.querySelector('div.z-pagingNumbers').appendChild(newAnchor)
+            // 현재 페이지면 클래스 추가
+            if (currentPageNum === i) {
+                newAnchor.classList.add(selectedPage);
+            }
         }
 
         // 3) 화살표 처리
         // 3-1) Start 화살표 처리 (제일 앞에 있는 '페이지 목록' 이동)
         let pagingStartArrow = document.querySelector('#pagingStartArrow');
-        pagingStartArrow.className = '';
+        pagingStartArrow.classList.remove(selectedNumClass);
         pagingStartArrow.removeAttribute('href');
         if (endPageNum > NUM_OF_PAGE_NUMS) {
-            pagingStartArrow.classList.add('active');
+            pagingStartArrow.classList.add(selectedNumClass);
             pagingStartArrow.setAttribute('href', 'javascript:getList(1)');
         }
 
         // 3-2) Prev 화살표 처리 (바로 앞에 있는 '페이지 목록' 이동)
         let pagingPrevArrow = document.querySelector('#pagingPrevArrow');
-        pagingPrevArrow.className = '';
+        pagingPrevArrow.classList.remove(selectedNumClass);
         pagingPrevArrow.removeAttribute('href');
         if (endPageNum > NUM_OF_PAGE_NUMS) {
-            pagingPrevArrow.classList.add('active');
+            pagingPrevArrow.classList.add(selectedNumClass);
             pagingPrevArrow.setAttribute('href', 'javascript:getList(' + (startPageNum - 1) + ')');
         }
 
         // 3-3) End 화살표 처리 (제일 마지막 '페이지 목록' 이동)
         let pagingEndArrow = document.querySelector('#pagingEndArrow');
-        pagingEndArrow.className = '';
+        pagingEndArrow.classList.remove(selectedNumClass);
         pagingEndArrow.removeAttribute('href');
         if (totalPageNum > endPageNum) {
-            pagingEndArrow.classList.add('active');
+            pagingEndArrow.classList.add(selectedNumClass);
             pagingEndArrow.setAttribute('href', 'javascript:getList(' + (totalPageNum) + ')');
         }
 
         // 3-4) Next 화살표 처리 (바로 다음 '페이지 목록' 이동)
         let pagingNextArrow = document.querySelector('#pagingNextArrow');
-        pagingNextArrow.className = '';
+        pagingNextArrow.classList.remove(selectedNumClass);
         pagingNextArrow.removeAttribute('href');
         if (totalPageNum > endPageNum) {
-            pagingNextArrow.classList.add('active');
+            pagingNextArrow.classList.add(selectedNumClass);
             pagingNextArrow.setAttribute('href', 'javascript:getList(' + (endPageNum + 1) + ')');
         }
     }
