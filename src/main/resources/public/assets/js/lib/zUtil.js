c6176963423231f132c02d2b198fd6a3393d5864
woodajung <wdj@brainz.co.kr>
2021-08-11 17:24:12 +0900

    로컬파일 미존재시 동작이 멈추는 버그 수정(ajax > fetch 사용으로 변경)

--- src/main/resources/public/assets/js/lib/zUtil.js
+++ src/main/resources/public/assets/js/lib/zUtil.js
@@ -1082,6 +1082,23 @@ aliceJs.fetchText = function(url, option) {
 };
 
 /**
+ * 비동기 통신 후 Promise 형태로 Blob 데이터를 반환하는 함수
+ * @param url url
+ * @param option 옵션
+ * @returns Promise 객체 반환값
+ */
+aliceJs.fetchBlob = function (url, option) {
+    return aliceJs.doFetch(url, option)
+        .then(response => {
+            if (response.ok) {
+                return response.blob();
+            } else {
+                return false;
+            }
+        });
+};
+
+/**
  * 믹스인을 추가하는 함수
  *
  * @param target 믹스인을 추가할 대상 객체의 prototype
