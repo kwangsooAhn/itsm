2b088a800ce36d0343d3d3804b5aaaa04a57676d
lizeelf <lizeelf@brianz.co.kr>
2021-09-10 15:59:44 +0900

    #10852 [보고서] 구현 - 보고서용 테이블 4개 SQL 추가

---
+++ erd/sql/67.awf_report_template.sql
@@ -0,0 +1,23 @@
+/**
+ * 보고서 설정 테이블 (템플릿)
+ */
+create table awf_report_template (
+    template_id varchar(128) NOT NULL,
+    template_name varchar(128) NOT NULL,
+    template_desc varchar(512),
+    automatic boolean DEFAULT false,
+    create_user_key varchar(128),
+    create_dt timestamp,
+    update_user_key varchar(128),
+    update_dt timestamp,
+    CONSTRAINT awf_report_template_pk PRIMARY KEY (template_id)
+);
+
+COMMENT ON TABLE awf_report_template IS '보고서 템플릿 정보';
+COMMENT ON COLUMN awf_report_template.template_id IS '템플릿아이디';
+COMMENT ON COLUMN awf_report_template.template_name IS '템플릿명';
+COMMENT ON COLUMN awf_report_template.template_desc IS '템플릿설명';
+COMMENT ON COLUMN awf_report_template.create_user_key IS '생성자';
+COMMENT ON COLUMN awf_report_template.create_dt IS '생성일시';
+COMMENT ON COLUMN awf_report_template.update_user_key IS '수정자';
+COMMENT ON COLUMN awf_report_template.update_dt IS '수정일시';
---
+++ erd/sql/68.awf_report_template_map.sql
@@ -0,0 +1,13 @@
+/**
+ * 보고서 설정 차트 매핑 테이블
+ */
+create table awf_report_template_map (
+    template_id varchar(128) NOT NULL,
+    chart_id varchar(128) NOT NULL,
+    display_order int,
+    CONSTRAINT awf_report_template_map_pk PRIMARY KEY (template_id, chart_id)
+);
+
+COMMENT ON TABLE awf_report_template_map IS '보고서 템플릿 차트 정보';
+COMMENT ON COLUMN awf_report_template_map.template_id IS '템플릿아이디';
+COMMENT ON COLUMN awf_report_template_map.chart_id IS '차트아이디';
