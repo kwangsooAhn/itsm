fbb83c4faf484707e060cb02ed73f0ea3ed97fb4
모형난 <hn.mo@brainz.co.kr>
2021-10-18 10:54:10 +0900

    3, 4 제외 수정 완료

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -4342,7 +4342,7 @@ DROP TABLE IF EXISTS awf_code_lang cascade;
 CREATE TABLE awf_code_lang
 (
     code varchar(100) NOT NULL,
-    code_value varchar(256),
+    code_name varchar(256),
     lang varchar(100) NOT NULL,
     CONSTRAINT awf_code_lang_pk PRIMARY KEY (code, lang),
     CONSTRAINT awf_code_lang_fk FOREIGN KEY (code) REFERENCES awf_code (code)
@@ -4350,7 +4350,7 @@ CREATE TABLE awf_code_lang
 
 COMMENT ON TABLE awf_code_lang IS '다국어 코드 정보';
 COMMENT ON COLUMN awf_code_lang.code IS '코드';
-COMMENT ON COLUMN awf_code_lang.code_name IS '코드 값';
+COMMENT ON COLUMN awf_code_lang.code_name IS '코드 이름';
 COMMENT ON COLUMN awf_code_lang.lang IS '언어';
 
 insert into awf_code_lang values ('document.status.temporary', 'temporary', 'en');
