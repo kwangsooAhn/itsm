56e461fb023cbc4a6e21228e662ab38c1ad4e103
모형난 <hn.mo@brainz.co.kr>
2021-10-19 10:07:36 +0900

    code_name(128)로 수정

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -4342,7 +4342,7 @@ DROP TABLE IF EXISTS awf_code_lang cascade;
 CREATE TABLE awf_code_lang
 (
     code varchar(100) NOT NULL,
-    code_name varchar(256),
+    code_name varchar(128),
     lang varchar(100) NOT NULL,
     CONSTRAINT awf_code_lang_pk PRIMARY KEY (code, lang),
     CONSTRAINT awf_code_lang_fk FOREIGN KEY (code) REFERENCES awf_code (code)
