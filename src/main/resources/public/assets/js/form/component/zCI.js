def886e728cec4d0075c070dabf927c508805398
모형난 <hn.mo@brainz.co.kr>
2021-11-04 10:36:46 +0900

    코드 수정 및 한 줄로

--- src/main/resources/public/assets/js/form/component/zCI.js
+++ src/main/resources/public/assets/js/form/component/zCI.js
@@ -66,8 +66,7 @@ export const ciMixin = {
         if (this.parent?.parent?.displayType === FORM.DISPLAY_TYPE.READONLY) {
             // 모든 버튼을 disabled 처리
             this.UIElement.UIComponent.UIElement.domElement.querySelectorAll('button').forEach((elem) => {
-                const searchButton = this.UIElement.UIComponent.UIElement.domElement.querySelector('span.i-search').closest('button')
-                elem === searchButton ? searchButton.disabled = false : elem.disabled = true;
+                elem.disabled = !elem.querySelector('span.i-search');
             });
         }
     },
