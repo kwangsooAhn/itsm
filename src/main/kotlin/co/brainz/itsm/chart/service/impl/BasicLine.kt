73ef5653dfa54618862773ceac8ba5987d729886
hg.jung <hg.jung@brainz.co.kr>
2021-11-13 02:16:14 +0900

    #11509 [사용자 정의차트] 연산방법 추가 -> 수정 진행

--- src/main/kotlin/co/brainz/itsm/chart/service/impl/BasicLine.kt
+++ src/main/kotlin/co/brainz/itsm/chart/service/impl/BasicLine.kt
@@ -14,6 +14,7 @@ import co.brainz.itsm.document.service.DocumentService
 import co.brainz.itsm.form.service.FormService
 import co.brainz.itsm.instance.service.InstanceService
 import co.brainz.itsm.token.service.TokenService
+import co.brainz.workflow.engine.manager.service.WfTokenManagerService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -24,8 +25,16 @@ class BasicLine(
     instanceService: InstanceService,
     documentService: DocumentService,
     formService: FormService,
-    tokenService: TokenService
-) : ChartManager(chartManagerService, instanceService, documentService, formService, tokenService) {
+    tokenService: TokenService,
+    wfTokenManagerService: WfTokenManagerService
+) : ChartManager(
+    chartManagerService,
+    instanceService,
+    documentService,
+    formService,
+    tokenService,
+    wfTokenManagerService
+) {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
