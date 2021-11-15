1870d0f63044aa4d7d0bcf17edf066b075cf7768
woodajung <wdj@brainz.co.kr>
2021-11-15 13:27:31 +0900

    #11526 [폼 디자이너] CI 컴포넌트 필수 체크 시 이동하지 않는다.

--- src/main/resources/public/assets/js/form/component/zCheckBox.js
+++ src/main/resources/public/assets/js/form/component/zCheckBox.js
@@ -56,7 +56,7 @@ export const checkBoxMixin = {
     },
     afterEvent() {
         // 신청서 양식 편집 화면에 따른 처리
-        if (this.parent?.parent?.displayType === FORM.DISPLAY_TYPE.READONLY) {
+        if (this.displayType === FORM.DISPLAY_TYPE.READONLY) {
             for (let i = 0; i < this.element.options.length; i++) {
                 this.UIElement.UIComponent.UIElement['UILabel' + i].addUIClass('readonly');
                 this.UIElement.UIComponent.UIElement['UILabel' + i].UICheckbox.addUIClass('readonly');
