a0995d74fa97fa7d784e3d6ae7c995349b5281f8
안광수 <ks.ahn@brainz.co.kr>
2021-12-08 10:08:17 +0900

    #11634 [신청서] 이관 이후 토픽이 추가로 생성된다.

--- src/main/kotlin/co/brainz/workflow/token/repository/WfTokenDataRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/workflow/token/repository/WfTokenDataRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package co.brainz.workflow.token.repository
 
 import co.brainz.workflow.component.entity.QWfComponentEntity
+import co.brainz.workflow.document.entity.QWfDocumentEntity
+import co.brainz.workflow.form.entity.QWfFormEntity
 import co.brainz.workflow.instance.dto.WfInstanceListComponentDto
 import co.brainz.workflow.instance.dto.WfInstanceListTokenDataDto
+import co.brainz.workflow.instance.entity.QWfInstanceEntity
 import co.brainz.workflow.token.entity.QWfTokenDataEntity
+import co.brainz.workflow.token.entity.QWfTokenEntity
 import co.brainz.workflow.token.entity.WfTokenDataEntity
 import com.querydsl.core.types.Projections
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
@@ -15,6 +19,11 @@ class WfTokenDataRepositoryImpl : QuerydslRepositorySupport(WfTokenDataEntity::c
     override fun findTokenDataByTokenIds(tokenIds: Set<String>): List<WfInstanceListTokenDataDto> {
         val tokenData = QWfTokenDataEntity.wfTokenDataEntity
         val component = QWfComponentEntity.wfComponentEntity
+        val instance = QWfInstanceEntity.wfInstanceEntity
+        val token = QWfTokenEntity.wfTokenEntity
+        val document = QWfDocumentEntity.wfDocumentEntity
+        val form = QWfFormEntity.wfFormEntity
+
         return from(tokenData)
             .select(
                 Projections.constructor(
@@ -31,6 +40,10 @@ class WfTokenDataRepositoryImpl : QuerydslRepositorySupport(WfTokenDataEntity::c
                 )
             )
             .innerJoin(tokenData.component, component)
+            .innerJoin(tokenData.token, token)
+            .innerJoin(token.instance, instance)
+            .innerJoin(instance.document, document)
+            .innerJoin(document.form, form).on(form.formId.eq(component.form.formId))
             .where(tokenData.token.tokenId.`in`(tokenIds))
             .fetch()
     }
