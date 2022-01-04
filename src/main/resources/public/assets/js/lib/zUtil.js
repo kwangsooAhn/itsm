00224e966ddf405f62b2fc301e2858a84b20ca51
woodajung <wdj@brainz.co.kr>
2022-01-04 09:26:28 +0900

    response 에러시 전달받은 메세지로 경고창 표시 / throw Error 제거

--- src/main/resources/public/assets/js/lib/zUtil.js
+++ src/main/resources/public/assets/js/lib/zUtil.js
@@ -1054,7 +1054,6 @@ aliceJs.fetchJson = function(url, option) {
             const jsonData = data ? JSON.parse(data) : {};
             // 상태 코드가 200이 아닐 경우 경고 표시
             if (jsonData.hasOwnProperty('status') && jsonData.status !== 200) {
-                console.log(jsonData);
                 zAlert.danger(jsonData.message);
             }
             return jsonData;
