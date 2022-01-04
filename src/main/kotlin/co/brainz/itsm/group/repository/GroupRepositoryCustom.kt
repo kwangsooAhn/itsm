ccfd260e5f2450267e7f8fbb1c664723f51cd9b9
lizeelf <lizeelf@gmail.com>
2022-01-04 17:13:32 +0900

    1. error 수정

--- src/main/kotlin/co/brainz/itsm/group/repository/GroupRepositoryCustom.kt
+++ src/main/kotlin/co/brainz/itsm/group/repository/GroupRepositoryCustom.kt
@@ -6,10 +6,14 @@
 package co.brainz.itsm.group.repository
 
 import co.brainz.framework.querydsl.AliceRepositoryCustom
+import co.brainz.itsm.group.dto.GroupSearchCondition
+import co.brainz.itsm.group.dto.PGroupDto
 import co.brainz.itsm.group.entity.GroupEntity
 import com.querydsl.core.QueryResults
 
 interface GroupRepositoryCustom : AliceRepositoryCustom {
 
     fun findByGroupSearchList(searchValue: String): QueryResults<GroupEntity>
+
+    fun findGroupList(groupSearchCondition: GroupSearchCondition): QueryResults<PGroupDto>
 }
