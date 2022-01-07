4edd07fa4aded1ba885a96cd88289493786322ee
jy.lim <jy.lim@brainz.co.kr>
2022-01-07 17:12:01 +0900

    #12067 [사용자 역할 및 권한 기능 변경] 권한 설정 목록 코드화

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -41,15 +41,15 @@ COMMENT ON COLUMN awf_auth.create_dt IS '등록일';
 COMMENT ON COLUMN awf_auth.update_user_key IS '수정자';
 COMMENT ON COLUMN awf_auth.update_dt IS '수정일';
 
-insert into awf_auth values ('general', '일반 사용', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('workflow.manage', '업무흐름 관리', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('workflow.expire', '업무 취소', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('cmdb.manage', 'CMDB 관리', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('cmdb.view', 'CMDB 조회', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('report.manage', '보고서 관리', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('report.view', '보고서 조회', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('system.manage', '시스템 관리', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
-insert into awf_auth values ('portal.manage', '포털 관리', '', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('general', '일반 사용', '신청서 작성 및 FAQ/자료실/공지사항을 조회할 수 있고, 게시판에 글을 등록하거나 조회할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('workflow.manage', '업무흐름 관리', '문서양식, 프로세스를 편집할 수 있으며, 관련된 업무흐름을 설정할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('workflow.expire', '업무 취소', '관련된 업무흐름을 처리할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('cmdb.manage', 'CMDB 관리', 'CI 속성항목, 유형 등 CMDB 관련 설정을 할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('cmdb.view', 'CMDB 조회', 'CMDB 내용을 검색하여 조회할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('report.manage', '보고서 관리', '사용자 정의 차트를 편집하고, 보고서 템플릿을 생성할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('report.view', '보고서 조회', '보고서 내용을 검색하여 조회할 수 있습니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('system.manage', '시스템 관리', '"사용자, 역할, 스케줄러, 게시판, 코드 관리 권한을 가집니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
+insert into awf_auth values ('portal.manage', '포털 관리', 'FAQ 등록, 게시판 생성, 공지사항 등록 등 포털과 관련된 설정이 가능합니다.', '0509e09412534a6e98f04ca79abb6424', now(), null, null);
 
 /**
  * 아바타
