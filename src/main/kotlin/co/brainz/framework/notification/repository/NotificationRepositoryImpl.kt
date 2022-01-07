eb5594c5d3b9398b2fa002c385f7311011594832
안광수 <ks.ahn@brainz.co.kr>
2022-01-07 15:44:29 +0900

    #11982 알람 메세지를 눌렀을때 신청서가 열렸으면 좋겠다

--- src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryImpl.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
 package co.brainz.framework.notification.repository
 
 import co.brainz.framework.auth.entity.QAliceUserEntity
@@ -5,6 +10,8 @@ import co.brainz.framework.constants.AliceConstants
 import co.brainz.framework.notification.dto.NotificationDto
 import co.brainz.framework.notification.entity.NotificationEntity
 import co.brainz.framework.notification.entity.QNotificationEntity
+import co.brainz.workflow.instance.entity.QWfInstanceEntity
+import co.brainz.workflow.token.entity.QWfTokenEntity
 import com.querydsl.core.types.Projections
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
@@ -14,6 +21,10 @@ class NotificationRepositoryImpl : QuerydslRepositorySupport(NotificationEntity:
     override fun findNotificationList(receivedUser: String): List<NotificationDto> {
         val notification = QNotificationEntity.notificationEntity
         val user = QAliceUserEntity.aliceUserEntity
+        val instance = QWfInstanceEntity.wfInstanceEntity
+        val token = QWfTokenEntity.wfTokenEntity
+        val subToken = QWfTokenEntity.wfTokenEntity
+
         return from(notification)
             .select(
                 Projections.constructor(
@@ -23,13 +34,24 @@ class NotificationRepositoryImpl : QuerydslRepositorySupport(NotificationEntity:
                     notification.title,
                     notification.message,
                     notification.instanceId,
+                    token.tokenId,
+                    instance.documentNo,
                     notification.confirmYn,
                     notification.displayYn,
                     notification.createDt
                 )
             )
             .innerJoin(notification.receivedUser, user)
-            .where(notification.receivedUser.userId.eq(receivedUser))
+            .innerJoin(instance).on(notification.instanceId.eq(instance.instanceId))
+            .innerJoin(token).on(token.instance.instanceId.eq(instance.instanceId))
+            .where(notification.receivedUser.userId.eq(receivedUser)
+                .and(token.tokenId.eq(
+                    from(subToken)
+                        .select(subToken.tokenId.max())
+                        .where(subToken.instance.instanceId.eq(instance.instanceId))
+                    )
+                )
+            )
             .orderBy(notification.confirmYn.asc(), notification.createDt.desc())
             .limit(AliceConstants.NOTIFICATION_SIZE)
             .fetch()
