543106116f3d727266d11539db71dbcc405f5790
woodajung <wdj@brainz.co.kr>
2022-01-10 09:11:11 +0900

    포탈 관리자 게시판 관리 메뉴 추가

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -638,6 +638,7 @@ insert into awf_menu_auth_map values ('cmdb.type', 'cmdb.manage');
 insert into awf_menu_auth_map values ('cmdb', 'cmdb.view');
 insert into awf_menu_auth_map values ('cmdb.ci', 'cmdb.view');
 insert into awf_menu_auth_map values ('config', 'system.manage');
+insert into awf_menu_auth_map values ('config', 'portal.manage');
 insert into awf_menu_auth_map values ('config.boardAdmin', 'system.manage');
 insert into awf_menu_auth_map values ('config.boardAdmin', 'portal.manage');
 insert into awf_menu_auth_map values ('config.code', 'system.manage');
