d6e9bb16697e040e94be5b767b093b6987d09280
hg.jung <hg.jung@brainz.co.kr>
2022-01-11 09:34:39 +0900

    #12061 [사용자 정의 차트] 조건식 구현 -> 구현 진행

---
+++ src/main/kotlin/co/brainz/itsm/chart/constants/ChartConditionConstants.kt
@@ -0,0 +1,16 @@
+package co.brainz.itsm.chart.constants
+
+object ChartConditionConstants {
+    enum class Arithmetic(val operator: String) {
+        ADDITION("+"),
+        SUBTRACTION("-"),
+        DIVISION("/"),
+        MULTIPLICATION("*")
+    }
+    enum class Logical(val operator: String) {
+
+    }
+    enum class Comparison(val operator: String) {
+
+    }
+}
