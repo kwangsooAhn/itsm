93a313e0d47e2ef74fdeb71a2c2797a4acffabe0
lizeelf <lizeelf@gmail.com>
2022-01-14 16:57:11 +0900

    #12100 [참조인 관리] ERD 작성 1. ERD 작성 2. 초기스크립트 작성 3. Entity 생성

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -8769,3 +8769,33 @@ COMMENT ON TABLE awf_organization_role_map IS '조직역할매핑';
 COMMENT ON COLUMN awf_organization_role_map.organization_id IS '그룹아이디';
 COMMENT ON COLUMN awf_organization_role_map.role_id IS '역할아이디';
 
+/**
+  참조인 관리
+ */
+DROP TABLE IF EXISTS wf_instance_reference cascade;
+
+CREATE TABLE wf_instance_reference
+(
+    instance_id varchar(128) NOT NULL,
+    reference_user_key varchar(128) NOT NULL,
+    review_yn boolean default false,
+    display_yn boolean default false,
+    create_user_key varchar(128),
+    create_dt timestamp,
+    update_user_key varchar(128),
+    update_dt timestamp,
+    CONSTRAINT wf_instance_reference_pk PRIMARY KEY (instance_id, reference_user_key),
+    CONSTRAINT wf_instance_reference_fk1 FOREIGN KEY (instance_id) REFERENCES wf_instance (instance_id),
+    CONSTRAINT wf_instance_reference_fk2 FOREIGN KEY (reference_user_key) REFERENCES awf_user (user_key)
+);
+
+COMMENT ON TABLE wf_instance_reference IS '참조인관리';
+COMMENT ON COLUMN wf_instance_reference.instance_id IS '인스턴스아이디';
+COMMENT ON COLUMN wf_instance_reference.reference_user_key IS '참조인아이디';
+COMMENT ON COLUMN wf_instance_reference.review_yn IS '읽음여부';
+COMMENT ON COLUMN wf_instance_reference.display_yn IS '표시여부';
+COMMENT ON COLUMN wf_instance_reference.create_user_key IS '생성자';
+COMMENT ON COLUMN wf_instance_reference.create_dt IS '생성일시';
+COMMENT ON COLUMN wf_instance_reference.create_user_key IS '수정자';
+COMMENT ON COLUMN wf_instance_reference.create_dt IS '수정일시';
+
