4d859ce956086524d4c95daf275704cc2dace4bd
lizeelf <lizeelf@gmail.com>
2022-01-17 11:32:35 +0900

    1. Merge 후 충돌 해결 2. 기타 오류 수정 3. FrontEnd와 연결 및 메뉴 추가 및 처리

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -8777,21 +8777,21 @@ DROP TABLE IF EXISTS wf_instance_viewer cascade;
 CREATE TABLE wf_instance_viewer
 (
     instance_id varchar(128) NOT NULL,
-    viewer varchar(128) NOT NULL,
+    viewer_key varchar(128) NOT NULL,
     review_yn boolean default false,
     display_yn boolean default false,
     create_user_key varchar(128),
     create_dt timestamp,
     update_user_key varchar(128),
     update_dt timestamp,
-    CONSTRAINT wf_instance_viewer_pk PRIMARY KEY (instance_id, viewer),
+    CONSTRAINT wf_instance_viewer_pk PRIMARY KEY (instance_id, viewer_key),
     CONSTRAINT wf_instance_viewer_fk1 FOREIGN KEY (instance_id) REFERENCES wf_instance (instance_id),
-    CONSTRAINT wf_instance_viewer_fk2 FOREIGN KEY (viewer) REFERENCES awf_user (user_key)
+    CONSTRAINT wf_instance_viewer_fk2 FOREIGN KEY (viewer_key) REFERENCES awf_user (user_key)
 );
 
 COMMENT ON TABLE wf_instance_viewer IS '참조인관리';
 COMMENT ON COLUMN wf_instance_viewer.instance_id IS '인스턴스아이디';
-COMMENT ON COLUMN wf_instance_viewer.viewer IS '참조인아이디';
+COMMENT ON COLUMN wf_instance_viewer.viewer_key IS '참조인';
 COMMENT ON COLUMN wf_instance_viewer.review_yn IS '읽음여부';
 COMMENT ON COLUMN wf_instance_viewer.display_yn IS '표시여부';
 COMMENT ON COLUMN wf_instance_viewer.create_user_key IS '생성자';
