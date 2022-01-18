ca40e80ba8fdd27894b8d81d0f2f6dc8282b7dea
Park Joohyun <jh.park@brainz.co.kr>
2022-01-18 09:47:28 +0900

    #12064 - entity 수정

--- src/main/kotlin/co/brainz/workflow/instance/entity/WfInstanceViewerEntity.kt
+++ src/main/kotlin/co/brainz/workflow/instance/entity/WfInstanceViewerEntity.kt
@@ -6,32 +6,31 @@
 package co.brainz.workflow.instance.entity
 
 import co.brainz.framework.auth.entity.AliceUserEntity
-import co.brainz.itsm.folder.entity.WfFolderEntity
-import co.brainz.itsm.instance.entity.WfCommentEntity
-import co.brainz.workflow.document.entity.WfDocumentEntity
-import co.brainz.workflow.token.entity.WfTokenEntity
 import java.io.Serializable
 import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.Id
+import javax.persistence.IdClass
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
 @Table(name = "wf_instance_viewer")
+@IdClass(WfInstanceViewerEntityPk::class)
 data class WfInstanceViewerEntity(
 
     @Id
-    @Column(name = "instance_id", length = 128)
-    val instanceId: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instance_id")
+    val instance: WfInstanceEntity,
 
     @Id
-    @Column(name = "viewer_key", length = 128)
-    var viewerKey: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "viewer_key")
+    var viewer: AliceUserEntity,
 
     @Column(name = "review_yn")
     val reviewYn: Boolean? = false,
@@ -51,4 +50,9 @@ data class WfInstanceViewerEntity(
     @Column(name = "update_dt", insertable = false)
     var updateDt: LocalDateTime? = null
 
+) : Serializable
+
+data class WfInstanceViewerEntityPk(
+    val instance: String = "",
+    val viewer: String = ""
 ) : Serializable
