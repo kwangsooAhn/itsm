6f5808e2480f55a6a84011c9d0bfd0eda6f913a8
hg.jung <hg.jung@brainz.co.kr>
2022-01-21 10:21:53 +0900

    #12061 [사용자 정의 차트] 조건식 구현 -> 테스트 expression Tree

--- src/main/kotlin/co/brainz/itsm/chart/service/ChartExpressionTree.kt
+++ src/main/kotlin/co/brainz/itsm/chart/service/ChartExpressionTree.kt
@@ -5,104 +5,212 @@ import org.springframework.stereotype.Service
 
 @Service
 class ChartExpressionTree {
-    var overallRoot: ChartConditionNode? = null
+    var root: ChartConditionNode? = null
+    var condition = ""
+    var index = 0
+    var target = ""
 
-    fun execute(condition: String) {
-        var str2 = ""
-        var i = 0
+    fun execute(condition: String): Int {
+        // create root node
+        this.initCondition(condition)
+        this.initRoot()
+        this.createExpressionTree()
 
-        if (condition[0] == '-') {
-            this.init(ChartConditionNode(data = "0"))
-            i = 1
-            this.add("-")
-        } else {
-            while (i < condition.length) {
-                str2 += if (condition[i] in '0'..'9') condition[i] - '0' else break
-                i++
+        // create expression tree
+
+        return this.calculateNode()
+    }
+
+    /**
+     * 조건문 초기화
+     */
+    private fun initCondition(condition: String) {
+        this.condition = condition
+    }
+
+    /**
+     * 최상단 루트 초기화 및 설정
+     */
+    private fun initRoot() {
+        this.index = 0
+        this.target = ""
+
+
+
+        // 첫 문자에 음수가 오는 경우
+        when (condition[0]) {
+            '-' -> {
+                root = ChartConditionNode(data = "0")
+                index = 1
+                this.addNode("-")
             }
-            this.init(ChartConditionNode(data = str2))
-        }
-
-        while (i < condition.length) {
-            if (condition[i] in '0'..'9') {
-                str2 = ""
-                while (i < condition.length) {
-                    str2 += if (condition[i] in '0'..'9') condition[i] - '0' else {
-                        i--
+            else -> {
+                while (index < condition.length) {
+                    if (condition[index] in '0'..'9') {
+                        target += condition[index]
+                    } else if (condition[index] == '[') {
+                        for (innerIndex in index..condition.indices.last) {
+                            if (condition[innerIndex] == ']') {
+                                target = condition.substring(index, innerIndex + 1)
+                                index = innerIndex
+                                break;
+                            }
+                        }
+                    } else {
                         break
                     }
-                    i++
+                    index++
                 }
-                this.add(str2)
-            } else if (condition[i] == '(')
-                this.add("(")
-            else if (condition[i] == ')')
-                this.add(")")
-            else if (condition[i] == '+')
-                this.add("+")
-            else if (condition[i] == '-')
-                this.add("-")
-            else if (condition[i] == '*')
-                this.add("*")
-            else if (condition[i] == '/')
-                this.add("/")
-            i++
+                root = ChartConditionNode(data = target)
+            }
         }
-
-        println(this.print())
     }
 
-    fun init(root: ChartConditionNode?) {
-        overallRoot = root
+    /**
+     * 수식 트리 (Expression Tree) 생성 진행
+     */
+    private fun createExpressionTree() {
+        while (index < condition.length) {
+            when (condition[index]) {
+                in '0'..'9' -> {
+                    target = ""
+                    while (index < condition.length) {
+                        if (condition[index] in '0'..'9') {
+                            target += condition[index]
+                        } else {
+                            index--
+                            break
+                        }
+                        index++
+                    }
+                    this.addNode(target)
+                }
+                '(' -> {
+                    this.addNode("(")
+                }
+                ')' -> {
+                    this.addNode(")")
+                }
+                '+' -> {
+                    this.addNode("+")
+                }
+                '-' -> {
+                    this.addNode("-")
+                }
+                '*' -> {
+                    this.addNode("*")
+                }
+                '/' -> {
+                    this.addNode("/")
+                }
+                '>' -> {
+                    if (condition[index + 1] == '=') {
+                        this.addNode(">=")
+                        index++
+                    } else {
+                        this.addNode(">")
+                    }
+                }
+                '<' -> {
+                    if (condition[index + 1] == '=') {
+                        this.addNode("<=")
+                        index++
+                    } else {
+                        this.addNode("<")
+                    }
+                }
+                '!' -> {
+                    if (condition[index + 1] == '=') {
+                        this.addNode("!=")
+                        index++
+                    }
+                }
+                '=' -> {
+                    if (condition[index + 1] == '=') {
+                        this.addNode("==")
+                        index++
+                    }
+                }
+                '&' -> {
+                    if (condition[index + 1] == '&') {
+                        this.addNode("&&")
+                        index++
+                    }
+                }
+                '|' -> {
+                    if (condition[index + 1] == '|') {
+                        this.addNode("||")
+                        index++
+                    }
+                }
+                '[' -> {
+                    for (innerIndex in index .. condition.indices.last) {
+                        if (condition[innerIndex] == ']') {
+                            val target = condition.substring(index..innerIndex)
+                            this.addNode(target)
+                            index = innerIndex
+                            break
+                        }
+                    }
+                }
+            }
+            index++
+        }
     }
 
-    private fun hasBracket(): Boolean {
-        return hasBracket(overallRoot)
+    private fun hasParentheses(targetNode: ChartConditionNode?): Boolean {
+        if (targetNode == null || targetNode.data.isEmpty()) return false
+        if (targetNode.data == "(") return true
+        return hasParentheses(targetNode.leftNode) || hasParentheses(targetNode.rightNode)
     }
 
-    private fun hasBracket(root: ChartConditionNode?): Boolean {
-        if (root == null || root.data.isEmpty()) return false
-        if (root.data[0] == '(') return true
-        return hasBracket(root.leftNode) || hasBracket(root.rightNode)
+    fun calculateNode(): Int {
+        return calculateNode(root)
     }
 
-    fun print(): Int {
-        return print(overallRoot)
-    }
+    fun calculateNode(targetNode: ChartConditionNode?): Int {
+        if (targetNode == null) return 0
 
-    fun print(root: ChartConditionNode?): Int {
-        if (root == null) return 0
-        if (root.data[0] in '0'..'9') {
-            return root.data.toInt()
+        if (targetNode.data[0] in '0'..'9') {
+            return targetNode.data.toInt()
         }
-        if (root.data[0] == '+') {
-            return print(root.leftNode) + print(root.rightNode)
+        if (targetNode.data == "+") {
+            return calculateNode(targetNode.leftNode) + calculateNode(targetNode.rightNode)
         }
-        if (root.data[0] == '-') {
-            return print(root.leftNode) - print(root.rightNode)
+        if (targetNode.data == "-") {
+            return calculateNode(targetNode.leftNode) - calculateNode(targetNode.rightNode)
         }
-        if (root.data[0] == '*') {
-            return print(root.leftNode) * print(root.rightNode)
+        if (targetNode.data == "*") {
+            return calculateNode(targetNode.leftNode) * calculateNode(targetNode.rightNode)
         }
-        if (root.data[0] == '/') {
-            return print(root.leftNode) / print(root.rightNode)
+        if (targetNode.data == "/") {
+            return calculateNode(targetNode.leftNode) / calculateNode(targetNode.rightNode)
         }
+/*
+        if (targetNode.data == ">") {
+
+        }
+        if (targetNode.data == "<") {
+
+        }
+*/
+
         return 0
     }
 
-    fun add(value: String) {
-        overallRoot = add(overallRoot, value)
+    fun addNode(target: String) {
+        root = addNode(root, target)
     }
 
-    private fun add(overallRoot: ChartConditionNode?, value: String): ChartConditionNode? {
+    private fun addNode(overallRoot: ChartConditionNode?, target: String): ChartConditionNode? {
         var root: ChartConditionNode? = overallRoot
         if (root == null || root.data.isEmpty()) {
-            root = ChartConditionNode(data = value)
-        } else if (root.data[0] == '(' && value[0] != ')') {
-            root.leftNode = add(root.leftNode, value)
-        } else if (root.data[0] == '(' && value[0] == ')') {
-            if (!hasBracket(root.leftNode)) {
-                var data = print(root.leftNode)
+            root = ChartConditionNode(data = target)
+        } else if (root.data == "(" && target != ")") {
+            root.leftNode = addNode(root.leftNode, target)
+        } else if (root.data == "(" && target == ")") {
+            if (!hasParentheses(root.leftNode)) {
+                var data = calculateNode(root.leftNode)
                 if (data < 0) {
                     data *= -1
                     root.rightNode = ChartConditionNode(data = data.toString())
@@ -113,29 +221,41 @@ class ChartExpressionTree {
                     root.leftNode = null
                 }
             } else {
-                root.leftNode = add(root.leftNode, value)
+                root.leftNode = addNode(root.leftNode, target)
             }
-        } else if (hasBracket(root)) {
-            root.rightNode = add(root.rightNode, value)
-        } else if (category(root.data[0]) < category(value[0])) {
-            val node = ChartConditionNode(data = value)
+        } else if (hasParentheses(root)) {
+            root.rightNode = addNode(root.rightNode, target)
+        } else if (getOperatorPrecedence(root.data) < getOperatorPrecedence(target)) {
+            val node = ChartConditionNode(data = target)
             node.leftNode = root
             return node
-        } else if (root.data[0] != '(' && category(root.data[0]) >= category(value[0]) && root.leftNode == null) {
-            root.leftNode = add(root.leftNode, value)
-        } else if (root.data[0] != '(' && category(root.data[0]) >= category(value[0]) && root.leftNode != null) {
-            root.rightNode = add(root.rightNode, value)
+        } else if (root.data != "(" && getOperatorPrecedence(root.data) >= getOperatorPrecedence(target) && root.leftNode == null) {
+            root.leftNode = addNode(root.leftNode, target)
+        } else if (root.data != "(" && getOperatorPrecedence(root.data) >= getOperatorPrecedence(target) && root.leftNode != null) {
+            root.rightNode = addNode(root.rightNode, target)
         }
+
         return root
     }
 
-    private fun category(c: Char): Int {
-        if (c == '+') return 6
-        if (c == '-') return 5
-        if (c == '*') return 4
-        if (c == '/') return 3
-        if (c == '(' || c == ')') return 2
-        if (c in '0'..'9') return 1
+    /**
+     * 연산자 우선순쉬
+     */
+    private fun getOperatorPrecedence(target: String): Int {
+        if (target == "&&") return 14
+        if (target == "||") return 13
+        if (target == "!=") return 12
+        if (target == "==") return 11
+        if (target == ">=") return 10
+        if (target == "<=") return 9
+        if (target == ">") return 8
+        if (target == "<") return 7
+        if (target == "+") return 6
+        if (target == "-") return 5
+        if (target == "*") return 4
+        if (target == "/") return 3
+        if (target == "(" || target == ")") return 2
+        if (target in "0".."9") return 1
         return 0;
     }
 }
