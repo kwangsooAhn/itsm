91e2e2052c91cde1dcf5ef08a8c2bd9275ea21c0
lizeelf <lizeelf@gmail.com>
2022-01-24 10:50:13 +0900

    1. copyright 추가

--- src/main/kotlin/co/brainz/itsm/file/controller/FileController.kt
+++ src/main/kotlin/co/brainz/itsm/file/controller/FileController.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
 package co.brainz.itsm.file.controller
 
 import co.brainz.framework.fileTransaction.dto.AliceFileNameExtensionDto
