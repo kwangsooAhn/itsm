023f4c8031cd11a23467ad58b76983f6a301d35f
hg.jung <hg.jung@brainz.co.kr>
2022-01-25 15:45:20 +0900

    #12061 [사용자 정의 차트] 조건식 구현 -> 구현 진행

--- src/main/kotlin/co/brainz/itsm/statistic/customChart/service/impl/Pie.kt
+++ src/main/kotlin/co/brainz/itsm/statistic/customChart/service/impl/Pie.kt
@@ -10,6 +10,7 @@ import co.brainz.framework.tag.dto.AliceTagDto
 import co.brainz.itsm.statistic.customChart.dto.ChartConfig
 import co.brainz.itsm.statistic.customChart.dto.ChartData
 import co.brainz.itsm.statistic.customChart.dto.ChartTagInstanceDto
+import co.brainz.itsm.statistic.customChart.service.ChartConditionService
 import co.brainz.itsm.statistic.customChart.service.ChartManager
 import co.brainz.itsm.statistic.customChart.service.ChartManagerService
 import org.slf4j.Logger
