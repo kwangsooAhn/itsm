b800aab615f4bf79dbaed54a9207b0ed915199e1
Lee Jaeyong <jy.lee@brainz.co.kr>
2022-02-15 14:12:50 +0900

    #12346 - [개인현황판] 부서별 요청현황 구현
    임포트 정렬

--- src/main/kotlin/co/brainz/itsm/dashboard/dto/OrganizationChartDto.kt
+++ src/main/kotlin/co/brainz/itsm/dashboard/dto/OrganizationChartDto.kt
@@ -1,9 +1,14 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
 package co.brainz.itsm.dashboard.dto
 
-import java.io.Serializable
 import co.brainz.framework.tag.dto.AliceTagDto
 import co.brainz.itsm.chart.dto.ChartConfig
 import co.brainz.itsm.chart.dto.ChartData
+import java.io.Serializable
 
 data class OrganizationChartDto(
     val chartId: String,
