76c4c5c42d53e6b77dbe15f24ea2ab8424bd650f
hg.jung <hg.jung@brainz.co.kr>
2022-02-25 10:16:26 +0900

    #12308 [방화벽 연동] Plugin 관리 기능 구현 -> 기능 구현 진행

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -8896,7 +8896,7 @@ COMMENT ON COLUMN awf_plugin_history.enddt IS '종료시간';
 COMMENT ON COLUMN awf_plugin_history.plugin_param IS '파라미터';
 COMMENT ON COLUMN awf_plugin_history.plugin_result IS '결과';
 COMMENT ON COLUMN awf_plugin_history.message IS '결과 메시지';
-COMMENT ON COLUMN awf_plugin_hiosory.plugin_data IS 'JAR 사용 데이터';
+COMMENT ON COLUMN awf_plugin_hiosory.plugin_data IS '플러그인 데이터';
 
 -- public.awf_plugin_history foreign keys
 ALTER TABLE awf_plugin_history ADD CONSTRAINT awf_plugin_history_fk FOREIGN KEY (plugin_id) REFERENCES awf_plugin(plugin_id);
