2b214b1b0e7130ec785a33f0968474032a2fb73e
lizeelf <lizeelf@gmail.com>
2022-02-25 18:19:03 +0900

    #12359 [업무흐름] 문서양식과 프로세스가 동일한 경우 등록버튼을 누르면 어떠한 이벤트도 발생하지 않는다. 1. 등록 시 validation 체크 이슈 수정 2. 기타 소스 개선 3. 신청서 링크 이름 중복 체크 추가

--- src/main/kotlin/co/brainz/workflow/document/repository/WfDocumentLinkRepository.kt
+++ src/main/kotlin/co/brainz/workflow/document/repository/WfDocumentLinkRepository.kt
@@ -5,17 +5,13 @@
 
 package co.brainz.workflow.document.repository
 
-import co.brainz.framework.querydsl.AliceRepositoryCustom
-import co.brainz.itsm.document.dto.DocumentDto
-import co.brainz.itsm.document.dto.DocumentSearchCondition
 import co.brainz.workflow.document.entity.WfDocumentLinkEntity
-import com.querydsl.core.QueryResults
+import co.brainz.workflow.document.repository.querydsl.WfDocumentLinkRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface WfDocumentLinkRepository : JpaRepository<WfDocumentLinkEntity, String>, AliceRepositoryCustom {
-    fun findByDocumentLink(documentSearchCondition: DocumentSearchCondition): QueryResults<DocumentDto>
+interface WfDocumentLinkRepository : JpaRepository<WfDocumentLinkEntity, String>, WfDocumentLinkRepositoryCustom {
     fun findByDocumentLinkId(documentLinkId: String): WfDocumentLinkEntity
     fun deleteByDocumentLinkId(documentLinkId: String): Int
 }
