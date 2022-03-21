a2b3b032ee59b2fc49940e15db267a8261e26a4f
안광수 <ks.ahn@brainz.co.kr>
2022-03-21 11:54:05 +0900

    #12580 [CMDB] CI 유형(TYPE) 등록 시, 결함 (유효성 검증함수 수정)

--- src/main/kotlin/co/brainz/itsm/cmdb/ciType/service/CITypeService.kt
+++ src/main/kotlin/co/brainz/itsm/cmdb/ciType/service/CITypeService.kt
@@ -6,12 +6,10 @@
 
 package co.brainz.itsm.cmdb.ciType.service
 
-import co.brainz.cmdb.ciType.repository.CITypeRepository
 import co.brainz.cmdb.ciType.service.CITypeService
 import co.brainz.cmdb.dto.CITypeDto
 import co.brainz.framework.util.CurrentSessionUser
 import co.brainz.itsm.cmdb.ciType.constants.CITypeConstants
-import co.brainz.itsm.cmdb.ciType.constants.CITypeConstants.Validation.*
 import co.brainz.itsm.cmdb.ciType.dto.CITypeTreeReturnDto
 import java.time.LocalDateTime
 import org.slf4j.LoggerFactory
@@ -20,8 +18,7 @@ import org.springframework.stereotype.Service
 @Service
 class CITypeService(
     private val ciTypeService: CITypeService,
-    private val currentSessionUser: CurrentSessionUser,
-    private val cITypeRepository: CITypeRepository
+    private val currentSessionUser: CurrentSessionUser
 ) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -44,49 +41,36 @@ class CITypeService(
      * CI Type 생성
      */
     fun createCIType(ciTypeDto: CITypeDto): String {
-        var resultCode = ""
-        val validationCode = ciTypeService.duplicationCheck(ciTypeDto)
+        var returnCode = CITypeConstants.Status.STATUS_FAIL.code
+        val validationCode = ciTypeService.checkValidation(ciTypeDto)
 
-        when (validationCode) {
-            VALIDATION_OK.code -> {
-                ciTypeDto.createDt = LocalDateTime.now()
-                ciTypeDto.createUserKey = currentSessionUser.getUserKey()
-                if (ciTypeService.createCIType(ciTypeDto))
-                    resultCode = CITypeConstants.Status.STATUS_SUCCESS.code
-            }
-            VALIDATION_DUPLICATION_TYPE_ALIAS.code -> {
-                resultCode = CITypeConstants.Status.STATUS_FAIL_TYPE_ALIAS_DUPLICATION.code
-            }
-            VALIDATION_DUPLICATION_PTYPE_AND_TYPENAME.code -> {
-                resultCode = CITypeConstants.Status.STATUS_FAIL_PTYPE_AND_TYPENAME_DUPLICATION.code
-            }
+        if (validationCode == CITypeConstants.Status.STATUS_SUCCESS.code) {
+            ciTypeDto.createDt = LocalDateTime.now()
+            ciTypeDto.createUserKey = currentSessionUser.getUserKey()
+            if (ciTypeService.createCIType(ciTypeDto))
+                returnCode = CITypeConstants.Status.STATUS_SUCCESS.code
+        } else {
+            returnCode = validationCode
         }
-
-        return resultCode
+        return returnCode
     }
 
     /**
      * CI Type 수정
      */
     fun updateCIType(ciTypeDto: CITypeDto, typeId: String): String {
-        var returnValue = ""
-        val validationCode = ciTypeService.duplicationCheck(ciTypeDto)
+        var returnCode = CITypeConstants.Status.STATUS_FAIL.code
+        val validationCode = ciTypeService.checkValidation(ciTypeDto)
 
-        when (validationCode) {
-            VALIDATION_OK.code -> {
-                ciTypeDto.createDt = LocalDateTime.now()
-                ciTypeDto.createUserKey = currentSessionUser.getUserKey()
-                if (ciTypeService.updateCIType(typeId, ciTypeDto))
-                    returnValue = CITypeConstants.Status.STATUS_SUCCESS.code
-            }
-            VALIDATION_DUPLICATION_TYPE_ALIAS.code -> {
-                returnValue = CITypeConstants.Status.STATUS_FAIL_TYPE_ALIAS_DUPLICATION.code
-            }
-            VALIDATION_DUPLICATION_PTYPE_AND_TYPENAME.code -> {
-                returnValue = CITypeConstants.Status.STATUS_FAIL_PTYPE_AND_TYPENAME_DUPLICATION.code
-            }
+        if (validationCode == CITypeConstants.Status.STATUS_SUCCESS.code) {
+            ciTypeDto.createDt = LocalDateTime.now()
+            ciTypeDto.createUserKey = currentSessionUser.getUserKey()
+            if (ciTypeService.updateCIType(typeId, ciTypeDto))
+                returnCode = CITypeConstants.Status.STATUS_SUCCESS_EDIT_TYPE.code
+        } else {
+            returnCode = validationCode
         }
-        return returnValue
+        return returnCode
     }
 
     /**
