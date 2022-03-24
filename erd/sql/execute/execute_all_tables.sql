fc626b040239fb8ba4b440f4394729bdba0209f1
hg.jung <hg.jung@brainz.co.kr>
2022-03-24 09:59:19 +0900

    #12637 [ KB 저축은행 외부연동 - 메신저 연동 ] 기능 구현 -> 일감 분석 및 수정 진행

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -696,7 +696,8 @@ CREATE TABLE awf_notification
 	instance_id varchar(128),
 	confirm_yn boolean DEFAULT 'false',
 	display_yn boolean DEFAULT 'false',
-	create_user_key varchar(128),
+    target varchar(100),
+    create_user_key varchar(128),
 	create_dt timestamp,
 	update_user_key varchar(128),
 	update_dt timestamp,
@@ -711,6 +712,7 @@ COMMENT ON COLUMN awf_notification.message IS '메시지';
 COMMENT ON COLUMN awf_notification.instance_id IS '인스턴스아이디';
 COMMENT ON COLUMN awf_notification.confirm_yn IS '확인여부';
 COMMENT ON COLUMN awf_notification.display_yn IS '표시여부';
+COMMENT ON COLUMN awf_notification.target IS '대상 시스템';
 COMMENT ON COLUMN awf_notification.create_user_key IS '등록자';
 COMMENT ON COLUMN awf_notification.create_dt IS '등록일';
 COMMENT ON COLUMN awf_notification.update_user_key IS '수정자';
