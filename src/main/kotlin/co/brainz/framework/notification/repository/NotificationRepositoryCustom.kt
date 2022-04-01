a14f8e76d19b2e1670a349afb6c2dda5bb4a20eb
woodajung <wdj@brainz.co.kr>
2022-04-01 09:19:13 +0900

    #12747 [CI 라이센스 관리 기능] Toast 알림

--- src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryCustom.kt
+++ src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryCustom.kt
@@ -3,5 +3,6 @@ package co.brainz.framework.notification.repository
 import co.brainz.framework.notification.dto.NotificationDto
 
 interface NotificationRepositoryCustom {
-    fun findNotificationList(receivedUser: String): List<NotificationDto>
+    fun findNotificationListExceptDocument(receivedUser: String): List<NotificationDto>
+    fun findNotificationListForDocument(receivedUser: String): List<NotificationDto>
 }
--- src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryImpl.kt
+++ src/main/kotlin/co/brainz/framework/notification/repository/NotificationRepositoryImpl.kt
@@ -11,42 +11,39 @@ import co.brainz.framework.notification.constants.NotificationConstants
 import co.brainz.framework.notification.dto.NotificationDto
 import co.brainz.framework.notification.entity.NotificationEntity
 import co.brainz.framework.notification.entity.QNotificationEntity
+import co.brainz.itsm.notice.entity.QNoticeEntity
 import co.brainz.workflow.instance.entity.QWfInstanceEntity
 import co.brainz.workflow.token.entity.QWfTokenEntity
+import com.querydsl.core.types.ExpressionUtils
 import com.querydsl.core.types.Projections
+import com.querydsl.jpa.JPAExpressions
+import com.querydsl.jpa.JPQLQuery
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 class NotificationRepositoryImpl : QuerydslRepositorySupport(NotificationEntity::class.java),
     NotificationRepositoryCustom {
 
-    override fun findNotificationList(receivedUser: String): List<NotificationDto> {
-        val notification = QNotificationEntity.notificationEntity
-        val user = QAliceUserEntity.aliceUserEntity
-        val instance = QWfInstanceEntity.wfInstanceEntity
-        val token = QWfTokenEntity.wfTokenEntity
+    val notification: QNotificationEntity = QNotificationEntity.notificationEntity
+    val user: QAliceUserEntity = QAliceUserEntity.aliceUserEntity
+    val instance: QWfInstanceEntity = QWfInstanceEntity.wfInstanceEntity
+    val token: QWfTokenEntity = QWfTokenEntity.wfTokenEntity
+
+    override fun findNotificationListExceptDocument(receivedUser: String): List<NotificationDto> {
+        return getNotificationListQuery(receivedUser)
+            .where(notification.type.eq(NotificationConstants.Type.CMDB.code))
+            .orderBy(notification.confirmYn.asc(), notification.createDt.desc())
+            .limit(AliceConstants.NOTIFICATION_SIZE)
+            .fetch()
+    }
+
+    override fun findNotificationListForDocument(receivedUser: String): List<NotificationDto> {
         val subToken = QWfTokenEntity.wfTokenEntity
         val startDtSubToken = QWfTokenEntity.wfTokenEntity
 
-        return from(notification)
-            .select(
-                Projections.constructor(
-                    NotificationDto::class.java,
-                    notification.notificationId,
-                    notification.receivedUser.userKey,
-                    notification.title,
-                    notification.message,
-                    notification.instanceId,
-                    token.tokenId,
-                    instance.documentNo,
-                    notification.confirmYn,
-                    notification.displayYn,
-                    notification.createDt
-                )
-            )
-            .innerJoin(notification.receivedUser, user)
+        return getNotificationListQuery(receivedUser)
             .innerJoin(instance).on(notification.instanceId.eq(instance.instanceId))
             .innerJoin(token).on(token.instance.instanceId.eq(instance.instanceId))
-            .where(notification.receivedUser.userId.eq(receivedUser)
+            .where(notification.type.eq(NotificationConstants.Type.DOCUMENT.code)
                 // 최신 토큰값 조회를 위해 tokenId.max() 대신 tokenStartDt.max()로 수정 (#12080 참고)
                 .and(token.tokenId.eq(
                     from(subToken)
@@ -56,13 +53,47 @@ class NotificationRepositoryImpl : QuerydslRepositorySupport(NotificationEntity:
                                 .select(startDtSubToken.tokenStartDt.max())
                                 .where(startDtSubToken.instance.instanceId.eq(instance.instanceId))
                         ))
-                    )
-                ).and(
-                    notification.target.eq(NotificationConstants.ITSM_IDENTIFIER)
-                )
+                ))
             )
             .orderBy(notification.confirmYn.asc(), notification.createDt.desc())
             .limit(AliceConstants.NOTIFICATION_SIZE)
             .fetch()
+    }
+
+    /**
+     * 알림 공통 목록 조회
+     */
+    private fun getNotificationListQuery(receivedUser: String): JPQLQuery<NotificationDto> {
+        return from(notification)
+            .select(
+                Projections.constructor(
+                    NotificationDto::class.java,
+                    notification.notificationId,
+                    notification.receivedUser.userKey,
+                    notification.title,
+                    notification.message,
+                    notification.instanceId,
+                    ExpressionUtils.`as`(
+                        JPAExpressions.select(token.tokenId).from(token)
+                            .where(token.instance.instanceId.eq(notification.instanceId)), "tokenId"
+                    ),
+                    ExpressionUtils.`as`(
+                        JPAExpressions.select(instance.documentNo).from(instance)
+                            .where(instance.instanceId.eq(notification.instanceId)), "documentNo"
+                    ),
+                    //token.tokenId,
+                    //instance.documentNo,
+                    notification.confirmYn,
+                    notification.displayYn,
+                    notification.createDt,
+                    notification.target
+                )
+            )
+            .innerJoin(notification.receivedUser, user)
+            //.leftJoin(instance).on(notification.instanceId.eq(instance.instanceId))
+            //.leftJoin(token).on(token.instance.instanceId.eq(instance.instanceId))
+            .where(notification.receivedUser.userId.eq(receivedUser)
+                .and(notification.target.eq(NotificationConstants.ITSM_IDENTIFIER))
+            )
     }
 }
