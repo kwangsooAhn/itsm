b9792568435486ea51ecc890822c449aacd53b15
hg.jung <hg.jung@brainz.co.kr>
2022-04-06 13:58:21 +0900

    #12798 [플러그인] IF 테이블 모니터링 -> sql 파일 수정

--- erd/sql/76.if_monitor.sql
+++ erd/sql/76.if_monitor.sql
@@ -5,8 +5,10 @@ DROP TABLE IF EXISTS if_monitor cascade;
 
 CREATE TABLE if_monitor
 (
-    document_no varchar(128) NOT NULL
+    document_no varchar(128) NOT NULL,
+    check_flag boolean DEFAULT 'false'
 );
 
 COMMENT ON TABLE if_monitor IS 'IF 모니터';
 COMMENT ON COLUMN if_monitor.document_no IS '문서번호';
+COMMNET ON COLUMN if_monitor.check_flag IS '처리여부';
