4c269fa5898fd3649bdc9aa83cc6063fd01f5c36
안광수 <ks.ahn@brainz.co.kr>
2022-04-18 11:21:24 +0900

    #12856 [통합테스트][부서] 부서이름 중복체크시 에러가 난다. #12837 [통합테스트][게시판관리] view 화면에서 첨부파일 input이 disabled가 아니다.

--- src/main/kotlin/co/brainz/framework/organization/repository/OrganizationRepositoryCustom.kt
+++ src/main/kotlin/co/brainz/framework/organization/repository/OrganizationRepositoryCustom.kt
@@ -12,7 +12,6 @@ import com.querydsl.core.QueryResults
 
 interface OrganizationRepositoryCustom : AliceRepositoryCustom {
     fun findByOrganizationSearchList(organizationSearchCondition: OrganizationSearchCondition): QueryResults<OrganizationEntity>
-    fun existsByOrganizationName(organizationName: String, organizationId: String?): Long
     fun getOrganizationListByIds(organizationIds: Set<String>): List<OrganizationEntity>
     fun existsByPOrganizationId(organizationId: String): Boolean
     fun findOrganizationsByUseYn(): QueryResults<OrganizationEntity>
