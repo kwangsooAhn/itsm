02fc99d59e3012d43dece19988e78e1b9de0562c
Lee Jaeyong <jy.lee@brainz.co.kr>
2022-04-28 19:08:18 +0900

    #12354 - 커스텀템플릿 목록 강제형변환시 오류나서 주석처리 한부분 제거

--- src/main/kotlin/co/brainz/itsm/statistic/customReport/service/CustomReportService.kt
+++ src/main/kotlin/co/brainz/itsm/statistic/customReport/service/CustomReportService.kt
@@ -48,14 +48,14 @@ class CustomReportService(
     private val mapper = ObjectMapper().registerModules(KotlinModule(), JavaTimeModule())
 
     fun getReportList(reportSearchCondition: ReportSearchCondition): CustomReportListReturnDto {
-        val paginResult = customReportRepository.getReportList(reportSearchCondition)
+        val pagingResult = customReportRepository.getReportList(reportSearchCondition)
         return CustomReportListReturnDto(
-            data = mapper.convertValue(paginResult.dataList, object : TypeReference<List<CustomReportListDto>>(){}),
+            data = mapper.convertValue(pagingResult.dataList, object : TypeReference<List<CustomReportListDto>>(){}),
             paging = AlicePagingData(
-                totalCount = paginResult.totalCount,
+                totalCount = pagingResult.totalCount,
                 totalCountWithoutCondition = customReportRepository.count(),
                 currentPageNum = reportSearchCondition.pageNum,
-                totalPageNum = Math.ceil(paginResult.totalCount.toDouble() / reportSearchCondition.contentNumPerPage.toDouble())
+                totalPageNum = Math.ceil(pagingResult.totalCount.toDouble() / reportSearchCondition.contentNumPerPage.toDouble())
                     .toLong(),
                 orderType = PagingConstants.ListOrderTypeCode.CREATE_DESC.code
             )
