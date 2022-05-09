c9381a75d7a4c3cea80b46d40ee40baeb509eb60
Park Joohyun <jh.park@brainz.co.kr>
2022-05-09 12:51:26 +0900

    #12008 [feature] : 범위에서 제외된 테이블 제거

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -9257,22 +9257,3 @@ CREATE TABLE sla_zql
 COMMENT ON TABLE sla_zql IS 'SLA ZQL';
 COMMENT ON COLUMN sla_zql.metric_id IS '지표아이디';
 COMMENT ON COLUMN sla_zql.zql_string IS 'zql';
-
-/**
-  SLA 지표 - 문서 매핑
- */
-DROP TABLE IF EXISTS sla_metric_document_map cascade;
-
-CREATE TABLE sla_metric_document_map
-(
-    metric_id varchar(128) NOT NULL,
-    document_id varchar(128) NOT NULL,
-    CONSTRAINT sla_metric_document_map_pk PRIMARY KEY (metric_id, document_id),
-    CONSTRAINT sla_metric_document_map_fk1 FOREIGN KEY (metric_id) REFERENCES sla_metric (metric_id),
-    CONSTRAINT sla_metric_document_map_fk2 FOREIGN KEY (document_id) REFERENCES wf_document (document_id)
-);
-
-COMMENT ON TABLE sla_metric_document_map IS 'SLA 지표 문서 매핑';
-COMMENT ON COLUMN sla_metric_document_map.metric_id IS '지표아이디';
-COMMENT ON COLUMN sla_metric_document_map.document_id IS '신청서아이디';
-
