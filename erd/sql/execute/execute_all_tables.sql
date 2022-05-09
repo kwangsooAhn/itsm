0468e8081cfa1f198e6b96ea3a538df66461926e
lizeelf <lizeelf@gmail.com>
2022-05-09 17:28:08 +0900

    #13051 [feature] 신청서기능강화 1. IF 테이블 추가 2. Numbering API URL 추가 3. CMDB_CI 의 mappingId 등록 기능 추가

--- erd/sql/execute/execute_all_tables.sql
+++ erd/sql/execute/execute_all_tables.sql
@@ -9124,3 +9124,100 @@ COMMENT ON COLUMN cmdb_class_notification.attribute_order IS '순서';
 COMMENT ON COLUMN cmdb_class_notification.condition IS '조건';
 COMMENT ON COLUMN cmdb_class_notification.target_attribute_id IS '담당자';
 
+/**
+  IF CMDB 테이블
+ */
+DROP TABLE IF EXISTS if_cmdb_ci cascade;
+
+CREATE TABLE if_cmdb_ci
+(
+    ci_id character varying(128) NOT NULL,
+    ci_no character varying(128),
+    ci_name character varying(128) NOT NULL,
+    ci_status character varying(100) NOT NULL,
+    type_id character varying(128) NOT NULL,
+    ci_desc character varying(512),
+    interlink boolean DEFAULT 'true',
+    instance_id character varying(128),
+    create_user_key character varying(128),
+    create_dt timestamp,
+    update_user_key character varying(128),
+    update_dt timestamp,
+    mapping_id character varying(128),
+    CONSTRAINT if_cmdb_ci_pk PRIMARY KEY (ci_id),
+    CONSTRAINT if_cmdb_ci_uk UNIQUE (ci_id),
+    CONSTRAINT if_cmdb_ci_fk1 FOREIGN KEY (type_id)
+        REFERENCES cmdb_type (type_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT if_cmdb_ci_fk2 FOREIGN KEY (instance_id)
+        REFERENCES wf_instance (instance_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+COMMENT ON TABLE if_cmdb_ci IS 'CMDB CI IF 테이블';
+COMMENT ON COLUMN if_cmdb_ci.ci_id IS 'CI아이디';
+COMMENT ON COLUMN if_cmdb_ci.ci_no IS '시퀀스';
+COMMENT ON COLUMN if_cmdb_ci.ci_name IS 'CI이름';
+COMMENT ON COLUMN if_cmdb_ci.ci_status IS 'CI상태';
+COMMENT ON COLUMN if_cmdb_ci.type_id IS '타입아이디';
+COMMENT ON COLUMN if_cmdb_ci.ci_desc IS 'CI설명';
+COMMENT ON COLUMN if_cmdb_ci.interlink IS '연동 여부';
+COMMENT ON COLUMN if_cmdb_ci.instance_id IS '인스턴스ID';
+COMMENT ON COLUMN if_cmdb_ci.create_user_key IS '등록자';
+COMMENT ON COLUMN if_cmdb_ci.create_dt IS '등록일시';
+COMMENT ON COLUMN if_cmdb_ci.update_user_key IS '수정자';
+COMMENT ON COLUMN if_cmdb_ci.update_dt IS '수정일시';
+COMMENT ON COLUMN if_cmdb_ci.mapping_id IS '매핑아이디';
+
+/**
+  IF CMDB 데이터 테이블
+ */
+DROP TABLE IF EXISTS if_cmdb_ci_data cascade;
+
+CREATE TABLE if_cmdb_ci_data
+(
+    ci_id character varying(128) NOT NULL,
+    attribute_id character varying(128) NOT NULL,
+    value text,
+    CONSTRAINT if_cmdb_ci_data_pk PRIMARY KEY (ci_id, attribute_id),
+    CONSTRAINT if_cmdb_ci_data_fk1 FOREIGN KEY (ci_id)
+        REFERENCES if_cmdb_ci (ci_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT if_cmdb_ci_data_fk2 FOREIGN KEY (attribute_id)
+        REFERENCES cmdb_attribute (attribute_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+COMMENT ON TABLE if_cmdb_ci_data IS 'CMDB CI IF 데이터';
+COMMENT ON COLUMN if_cmdb_ci_data.ci_id IS 'CI아이디';
+COMMENT ON COLUMN if_cmdb_ci_data.attribute_id IS '속성아이디';
+COMMENT ON COLUMN if_cmdb_ci_data.value IS '속성값';
+
+/**
+  IF CMDB 그룹 데이터 테이블
+ */
+DROP TABLE IF EXISTS if_cmdb_ci_group_list_data cascade;
+
+CREATE TABLE if_cmdb_ci_group_list_data
+(
+    ci_id character varying(128) NOT NULL,
+    attribute_id character varying(128) NOT NULL,
+    c_attribute_id character varying(128) NOT NULL,
+    c_attribute_seq int NOT NULL,
+    c_value text,
+    CONSTRAINT if_cmdb_ci_group_list_data_pk PRIMARY KEY (ci_id, attribute_id, c_attribute_id, c_attribute_seq),
+    CONSTRAINT if_cmdb_ci_group_list_data_fk1 FOREIGN KEY (ci_id)
+        REFERENCES if_cmdb_ci (ci_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT if_cmdb_ci_group_list_data_fk2 FOREIGN KEY (attribute_id)
+        REFERENCES cmdb_attribute (attribute_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+COMMENT ON TABLE if_cmdb_ci_group_list_data IS 'CMDB CI IF 그룹 리스트 데이터';
+COMMENT ON COLUMN if_cmdb_ci_group_list_data.ci_id IS 'CI아이디';
+COMMENT ON COLUMN if_cmdb_ci_group_list_data.attribute_id IS '속성아이디';
+COMMENT ON COLUMN if_cmdb_ci_group_list_data.c_attribute_id IS '자식속성아이디';
+COMMENT ON COLUMN if_cmdb_ci_group_list_data.c_attribute_seq IS '자식속성순서';
+COMMENT ON COLUMN if_cmdb_ci_group_list_data.c_value IS '자식속성값';
+
