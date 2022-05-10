604cc45ef12ab42be9c0f652eac5981506f270f5
Park Joohyun <jh.park@brainz.co.kr>
2022-05-10 18:02:18 +0900

    #12008 [feature] : SLA 지표 관리 - 목록 가져오기 순서 변경

--- src/main/kotlin/co/brainz/itsm/sla/metricPool/dto/MetricPoolDto.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricPool/dto/MetricPoolDto.kt
@@ -12,8 +12,8 @@ data class MetricPoolDto(
     var metricId: String = "",
     var metricGroupName: String = "",
     var metricName: String = "",
-    var metricDesc: String? = "",
     var metricTypeName: String = "",
     var metricUnitName: String = "",
-    var calculationTypeName: String = ""
+    var calculationTypeName: String = "",
+    var metricDesc: String? = ""
 ) : Serializable
