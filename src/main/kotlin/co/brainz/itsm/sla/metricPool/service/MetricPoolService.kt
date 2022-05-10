0ffdba3b25dbe86062f9e28699076ba82af5a530
Park Joohyun <jh.park@brainz.co.kr>
2022-05-10 12:57:45 +0900

    #12008 [feature] : SLA 지표 관리 - Entity, Repository 이름 변경

--- src/main/kotlin/co/brainz/itsm/sla/metricPool/service/MetricPoolService.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricPool/service/MetricPoolService.kt
@@ -9,7 +9,7 @@ import co.brainz.framework.constants.PagingConstants
 import co.brainz.framework.util.AlicePagingData
 import co.brainz.itsm.sla.metricPool.dto.MetricPoolListReturnDto
 import co.brainz.itsm.sla.metricPool.dto.MetricPoolSearchCondition
-import co.brainz.itsm.sla.metricPool.repository.SlaMetricPoolRepository
+import co.brainz.itsm.sla.metricPool.repository.MetricPoolRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class MetricPoolService(
-    private val slaMetricPoolRepository: SlaMetricPoolRepository
+    private val metricPoolRepository: MetricPoolRepository
 ) {
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
     private val mapper = ObjectMapper().registerModules(KotlinModule(), JavaTimeModule())
@@ -30,13 +30,13 @@ class MetricPoolService(
      * SLA 지표 목록 조회
      */
     fun getMetricPools(metricPoolSearchCondition: MetricPoolSearchCondition): MetricPoolListReturnDto {
-        val pagingResult = slaMetricPoolRepository.findMetricPools(metricPoolSearchCondition)
+        val pagingResult = metricPoolRepository.findMetricPools(metricPoolSearchCondition)
 
         return MetricPoolListReturnDto(
             data = mapper.convertValue(pagingResult.dataList),
             paging = AlicePagingData(
                 totalCount = pagingResult.totalCount,
-                totalCountWithoutCondition = slaMetricPoolRepository.count(),
+                totalCountWithoutCondition = metricPoolRepository.count(),
                 currentPageNum = metricPoolSearchCondition.pageNum,
                 totalPageNum = ceil(pagingResult.totalCount.toDouble() / metricPoolSearchCondition.contentNumPerPage.toDouble()).toLong(),
                 orderType = PagingConstants.ListOrderTypeCode.CREATE_DESC.code
