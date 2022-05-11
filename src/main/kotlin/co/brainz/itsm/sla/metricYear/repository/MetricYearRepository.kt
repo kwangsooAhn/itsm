012c4b3a0a34ed7817df42b830d600529771aa06
Park Joohyun <jh.park@brainz.co.kr>
2022-05-11 19:32:55 +0900

    copyright 추가

--- src/main/kotlin/co/brainz/itsm/sla/metricYear/repository/MetricYearRepository.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricYear/repository/MetricYearRepository.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
 package co.brainz.itsm.sla.metricYear.repository
 
 import co.brainz.itsm.sla.metricYear.entity.MetricYearEntity
