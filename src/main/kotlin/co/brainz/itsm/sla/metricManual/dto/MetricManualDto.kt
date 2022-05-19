9d2236cad8f91d01efa243929b655f4c36f2849c
Lee Jaeyong <jy.lee@brainz.co.kr>
2022-05-19 10:24:53 +0900

    #12008 [fix] 수동지표 삭제를 위한 metricId -> metricManualId로 데이터 값 변경

--- src/main/kotlin/co/brainz/itsm/sla/metricManual/dto/MetricManualDto.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricManual/dto/MetricManualDto.kt
@@ -11,7 +11,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class MetricManualDto(
-    val metricId: String = "",
+    val metricManualId: String = "",
     val metricName: String? = "",
     val referenceDate: LocalDate? = null,
     val metricValue: Double? = null,
