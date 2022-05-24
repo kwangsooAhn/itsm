34fc2be7e0aeb8bc535c67e5624dffb41ea790b3
Lee Jaeyong <jy.lee@brainz.co.kr>
2022-05-24 14:54:01 +0900

    #12008 [feature] 지표별 SLA 현황 최초 올해 metric selectBox 조회

--- src/main/kotlin/co/brainz/itsm/sla/metricStatus/controller/MetricStatusController.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricStatus/controller/MetricStatusController.kt
@@ -5,9 +5,23 @@
 
 package co.brainz.itsm.sla.metricStatus.controller
 
+import co.brainz.itsm.sla.metricStatus.service.MetricStatusService
+import javax.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/sla")
-class MetricStatusController
+@RequestMapping("/sla/metrics/status")
+class MetricStatusController(
+    private val metricStatusService: MetricStatusService
+) {
+    private val metricStatusSearchPage: String = "sla/metricStatus/statusMetricSearch"
+
+    @GetMapping("/search")
+    fun getMetricStatusSearch(request: HttpServletRequest, model: Model): String {
+        model.addAttribute("metricList", metricStatusService.getMetricList())
+        return metricStatusSearchPage
+    }
+}
--- src/main/kotlin/co/brainz/itsm/sla/metricStatus/service/MetricStatusService.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricStatus/service/MetricStatusService.kt
@@ -5,8 +5,21 @@
 
 package co.brainz.itsm.sla.metricStatus.service
 
+import co.brainz.itsm.sla.metricYear.dto.MetricLoadCondition
+import co.brainz.itsm.sla.metricYear.dto.MetricLoadDto
+import co.brainz.itsm.sla.metricYear.repository.MetricYearRepository
+import java.time.Year
 import org.springframework.stereotype.Service
 
 @Service
-class MetricStatusService{
+class MetricStatusService(
+    private val metricYearRepository: MetricYearRepository
+){
+
+    fun getMetricList(): List<MetricLoadDto> {
+        val metricLoadCondition = MetricLoadCondition(
+            source = Year.now().toString()
+        )
+        return metricYearRepository.findMetricListByLoadCondition(metricLoadCondition)
+    }
 }
