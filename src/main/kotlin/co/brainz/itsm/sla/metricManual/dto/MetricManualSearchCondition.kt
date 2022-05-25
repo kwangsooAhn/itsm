f2dca8bc66d8ce75e4d18665b7512c100e185fb1
Lee Jaeyong <jy.lee@brainz.co.kr>
2022-05-25 18:24:43 +0900

    #12008 [fix] 시간값까지 화면에서 전달 받는 걸로 수정

--- src/main/kotlin/co/brainz/itsm/sla/metricManual/dto/MetricManualSearchCondition.kt
+++ src/main/kotlin/co/brainz/itsm/sla/metricManual/dto/MetricManualSearchCondition.kt
@@ -8,10 +8,11 @@ package co.brainz.itsm.sla.metricManual.dto
 import co.brainz.framework.constants.PagingConstants
 import java.io.Serializable
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 /**
- * @param searchValue : 사용자가 입력한 검색어
+ * @param metricId : 사용자가 선택한 지표 ID
  * @param fromDt : 생성일자 시작일 조건
  * @param toDt : 생성일자 종료일 조건
  * @param pageNum : 검색결과중에서 요청받은 페이지
@@ -25,7 +26,7 @@ data class MetricManualSearchCondition(
     val contentNumPerPage: Long = PagingConstants.COUNT_PER_PAGE
 ) : Serializable {
     val formattedFromDt: LocalDate? = LocalDate.parse(
-        fromDt, DateTimeFormatter.ISO_DATE)
+        fromDt, DateTimeFormatter.ISO_DATE_TIME)
     val formattedToDt: LocalDate? = LocalDate.parse(
-        toDt, DateTimeFormatter.ISO_DATE)
+        toDt, DateTimeFormatter.ISO_DATE_TIME)
 }
