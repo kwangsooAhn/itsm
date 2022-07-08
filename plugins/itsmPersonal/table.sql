51e88df4153df59a3b6280a62495a86477e2f9c3
hg.jung <hg.jung@brainz.co.kr>
2022-07-08 09:04:39 +0900

    :feature: #13306 : query.xml 및 table.sql 수정

--- plugins/itsmPersonal/table.sql
+++ plugins/itsmPersonal/table.sql
@@ -7,19 +7,15 @@ DROP SEQUENCE IF EXISTS if_awf_user_history_seq cascade;
 CREATE SEQUENCE if_awf_user_history_seq INCREMENT 1 MINVALUE 1 START 1;
 CREATE TABLE if_awf_user_history
 (
-    seq bigint NOT NULL PRIMARY KEY,
-    user_key varchar(128) NOT NULL,
-    user_id varchar(128) NOT NULL,
-    user_name varchar(128) NOT NULL,
-    create_dt timestamp
+    seq           bigint       NOT NULL PRIMARY KEY,
+    user_key      varchar(128) NOT NULL,
+    user_id       varchar(128) NOT NULL,
+    user_name     varchar(128) NOT NULL,
+    mobile_number varchar(128),
+    organization  varchar(100),
+    use_yn        boolean,
+    create_dt     timestamp
 );
-
-COMMENT ON TABLE if_awf_user_history IS '인사 DB 이력 테이블';
-COMMENT ON COLUMN if_awf_user_history.seq IS '이력 번호';
-COMMENT ON COLUMN if_awf_user_history.user_key IS '사용자 키';
-COMMENT ON COLUMN if_awf_user_history.user_id  IS '사용자 아이디';
-COMMENT ON COLUMN if_awf_user_history.user_name  IS '사용자 명';
-COMMENT ON COLUMN if_awf_user_history.create_dt IS '등록일';
 
 /**
  * 테스트용 CLIENT 사용자 테이블
@@ -27,13 +23,25 @@ COMMENT ON COLUMN if_awf_user_history.create_dt IS '등록일';
 DROP TABLE IF EXISTS awf_user_client cascade;
 CREATE TABLE awf_user_client
 (
-    user_key_client varchar(128) NOT NULL PRIMARY KEY,
-    user_id_client varchar(128) NOT NULL,
-    user_name_client varchar(128) NOT NULL
+    user_key_client      varchar(128) NOT NULL PRIMARY KEY,
+    user_id_client       varchar(128) NOT NULL,
+    user_name_client     varchar(128) NOT NULL,
+    mobile_number_client varchar(128),
+    organization_client  varchar(100),
+    use_yn_client        boolean
 );
-insert into awf_user_client values ('0509e09412534a6e98f04ca79abb6424', 'test1', '테스터1');
-insert into awf_user_client values ('0509e09412534a6e98f04ca79abb6425', 'test2', '테스터2');
-insert into awf_user_client values ('0509e09412534a6e98f04ca79abb6426', 'test3', '테스터3');
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6424', 'test1', '테스터1', '010-1111-1111', '001', true);
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6425', 'test2', '테스터2', '010-2222-2222', '003', true);
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6426', 'test3', '테스터3', '010-3333-3333', '004', true);
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6427', 'test4', '테스터4', '010-4444-5555', '006', true);
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6428', 'test5', '테스터5', '010-6666-6666', '009', true);
+insert into awf_user_client
+values ('0509e09412534a6e98f04ca79abb6429', 'test6', '테스터6', '010-7777-7777', '017', true);
 
 /**
  * 테스트용 ITSM IF 사용자 테이블
@@ -41,13 +49,13 @@ insert into awf_user_client values ('0509e09412534a6e98f04ca79abb6426', 'test3',
 DROP TABLE IF EXISTS if_awf_user_test cascade;
 CREATE TABLE if_awf_user_test
 (
-    user_key varchar(128) NOT NULL PRIMARY KEY,
-    user_id varchar(128) NOT NULL,
-    user_name varchar(128) NOT NULL
+    user_key      varchar(128) NOT NULL PRIMARY KEY,
+    user_id       varchar(128) NOT NULL,
+    user_name     varchar(128) NOT NULL,
+    mobile_number varchar(128),
+    organization  varchar(100),
+    use_yn        boolean
 );
-insert into if_awf_user_test values ('0509e09412534a6e98f04ca79abb6424', 'test1', '테스터1');
-insert into if_awf_user_test values ('0509e09412534a6e98f04ca79abb6425', 'test2', '테스터2');
-insert into if_awf_user_test values ('0509e09412534a6e98f04ca79abb6426', 'test3', '테스터3');
 
 /**
  * 테스트용 ITSM 사용자 테이블
@@ -55,14 +63,13 @@ insert into if_awf_user_test values ('0509e09412534a6e98f04ca79abb6426', 'test3'
 DROP TABLE IF EXISTS awf_user_test cascade;
 CREATE TABLE awf_user_test
 (
-    user_key varchar(128) NOT NULL PRIMARY KEY,
-    user_id varchar(128) NOT NULL,
-    user_name varchar(128) NOT NULL
+    user_key      varchar(128) NOT NULL PRIMARY KEY,
+    user_id       varchar(128) NOT NULL,
+    user_name     varchar(128) NOT NULL,
+    mobile_number varchar(128),
+    organization  varchar(100),
+    use_yn        boolean
 );
-
-insert into awf_user_test values ('0509e09412534a6e98f04ca79abb6424', 'test1', '테스터1');
-insert into awf_user_test values ('0509e09412534a6e98f04ca79abb6425', 'test2', '테스터2');
-insert into awf_user_test values ('0509e09412534a6e98f04ca79abb6426', 'test3', '테스터3');
 
 /**
  * 테스트용 ITSM 사용자 역할 테이블
@@ -71,5 +78,5 @@ DROP TABLE IF EXISTS awf_user_role_test cascade;
 CREATE TABLE awf_user_role_test
 (
     user_key varchar(128) NOT NULL PRIMARY KEY,
-    role_id varchar(128) NOT NULL
+    role_id  varchar(128) NOT NULL
 );
