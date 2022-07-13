8924f92abdc0bd440f90c216e6c29fe624d07758
Park Joohyun <jh.park@brainz.co.kr>
2022-07-13 11:26:53 +0900

    :sparkles:  #13368 : 서비스카테고리 관리 화면 이동 구현

---
+++ src/main/kotlin/co/brainz/itsm/serviceCategory/controller/ServiceCategoryController.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 Brainzcompany Co., Ltd.
+ * https://www.brainz.co.kr
+ */
+
+package co.brainz.itsm.serviceCategory.controller
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/services")
+class ServiceCategoryController {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    private val serviceEditPage: String = "service-category/serviceEdit"
+
+    /**
+     * 서비스 카테고리 관리 화면 호출
+     */
+    @GetMapping("/edit")
+    fun getServiceEdit(): String {
+        return serviceEditPage
+    }
+}
