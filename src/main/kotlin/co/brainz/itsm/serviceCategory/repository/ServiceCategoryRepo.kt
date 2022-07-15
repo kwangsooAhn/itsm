34ec1516c9e10fcf80b996d4ceb77e404202f25a
Park Joohyun <jh.park@brainz.co.kr>
2022-07-15 15:38:27 +0900

    :sparkles: #13368 : 안 쓰는 함수 제거

--- src/main/kotlin/co/brainz/itsm/serviceCategory/repository/ServiceCategoryRepo.kt
+++ src/main/kotlin/co/brainz/itsm/serviceCategory/repository/ServiceCategoryRepo.kt
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface ServiceCategoryRepo : JpaRepository<ServiceCategoryEntity, String>, ServiceCategoryRepoCustom {
 
-    fun existsByServiceCodeOrServiceName(serviceCode: String, serviceName: String): Boolean
-
     fun existsByServiceName(serviceName: String): Boolean
 
     fun findByServiceCode(serviceCode: String): ServiceCategoryEntity
