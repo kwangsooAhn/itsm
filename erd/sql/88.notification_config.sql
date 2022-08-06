5bdfbde3c773351dd679e927a877ec51bd3f9206
안광수 <ks.ahn@brainz.co.kr>
2022-08-06 22:20:03 +0900

    #133#13333 notification모듈화 초기 DB스크립트 작성

---
+++ erd/sql/88.notification_config.sql
@@ -0,0 +1,19 @@
+/**
+ * 알람 발송관리 설정
+ */
+
+DROP TABLE IF EXISTS notification_config cascade;
+
+CREATE TABLE notification_config
+(
+    notification_code varchar(128) NOT NULL,
+    notification_name varchar(128) NOT NULL,
+    CONSTRAINT notification_config_pk PRIMARY KEY (notification_code)
+);
+
+COMMENT ON TABLE notification_config IS '알람 발송관리 설정';
+COMMENT ON COLUMN notification_config.notification_code IS '알람 발송관리 코드';
+COMMENT ON COLUMN notification_config.notification_name IS '알람 발송관리 명';
+
+INSERT INTO notification_config VALUES('document', '신청서');
+INSERT INTO notification_config VALUES('cmdbLicense', 'CMDB 라이센스');
---
+++ erd/sql/89.notification_config_detail.sql
@@ -0,0 +1,43 @@
+/**
+ * 알람 발송관리 설정
+ */
+
+DROP TABLE IF EXISTS notification_config_detail cascade;
+
+CREATE TABLE notification_config_detail
+(
+    channel             varchar(128) NOT NULL,
+    use_yn              boolean NOT NULL,
+    title_format        varchar(512) NOT NULL,
+    message_format      text NOT NULL,
+    template            varchar(128),
+    url                 varchar(256),
+    notification_code   varchar(128) NOT NULL,
+    create_user_key     varchar(128),
+    create_dt           timestamp,
+    update_user_key     varchar(128),
+    update_dt           timestamp,
+
+    CONSTRAINT notification_config_detail_pk PRIMARY KEY (notification_code, channel),
+    CONSTRAINT notification_config_detail_fk FOREIGN KEY (notification_code) REFERENCES notification_config (notification_code)
+);
+
+COMMENT ON TABLE notification_config_detail IS 'notification_config_detail';
+COMMENT ON COLUMN notification_config_detail.channel IS '발송 채널';
+COMMENT ON COLUMN notification_config_detail.use_yn IS '사용 여부';
+COMMENT ON COLUMN notification_config_detail.title_format IS '제목 양식';
+COMMENT ON COLUMN notification_config_detail.message_format IS '메세지 양식';
+COMMENT ON COLUMN notification_config_detail.template IS '템플릿 파일명';
+COMMENT ON COLUMN notification_config_detail.url IS '링크 URL';
+COMMENT ON COLUMN notification_config_detail.notification_code IS '알람 발송관리 코드';
+COMMENT ON COLUMN notification_config_detail.create_user_key IS '생성자';
+COMMENT ON COLUMN notification_config_detail.create_dt IS '생성 일시';
+COMMENT ON COLUMN notification_config_detail.update_user_key IS '수정자';
+COMMENT ON COLUMN notification_config_detail.update_dt IS '수정 일시';
+
+INSERT INTO notification_config_detail VALUES('toast', true, '${doc_type} ${doc_no}', '${doc_step}', null, null, 'document', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
+INSERT INTO notification_config_detail VALUES('sms', true, '[ITSM] ${doc_type} ${doc_no} 처리안내', '${doc_no} 처리바랍니다', null, null, 'document', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
+INSERT INTO notification_config_detail VALUES('mail', true, '[ITSM] ${doc_type} ${doc_no} 처리안내', '${doc_no} 처리바랍니다', 'document_mail_template.html', null, 'document', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
+INSERT INTO notification_config_detail VALUES('toast', true, '${ci_name} ${doc_no}', '${ci_name} ${monitoring_field}가 ${due_date}', null, null, 'cmdbLicense', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
+INSERT INTO notification_config_detail VALUES('sms', true, '[ITSM]  ${ci_no} ${ci_name} 만료 안내', '${ci_no} ${ci_name}가 ${due_date}', null, null, 'cmdbLicense', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
+INSERT INTO notification_config_detail VALUES('mail', true, '[ITSM]  ${ci_no} ${ci_name} 만료 안내', '${ci_no} ${ci_name}가 ${due_date}', 'cmdb_mail_template.html', null, 'cmdbLicense', '0509e09412534a6e98f04ca79abb6424',now(),null,null);
